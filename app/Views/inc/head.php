493c3974d68c39b141c54b76a666352f8341433b
tannv6 <tannv6@gmail.com>
2024-06-07 08:12:29 +0700

    add lib

--- app/Views/inc/head.php
+++ app/Views/inc/head.php
@@ -9,7 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/vendor/swiper-11.1.4/package/swiper-bundle.min.css" />
-    <script src="/vendor/swiper-11.1.4/package/swiper-bundle.min.js"></script>
+    <link rel="stylesheet" href="/lib/swiper-11.1.4/package/swiper-bundle.min.css" />
+    <script src="/lib/swiper-11.1.4/package/swiper-bundle.min.js"></script>
     <title><?= $setting['site_name'] ?></title>
 </head>
