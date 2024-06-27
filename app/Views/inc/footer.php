fa8c8f784fae857042f8878ba86c0363c56f9146
haitn0628 <haitn0628@gmail.com>
2024-06-27 13:17:25 +0700

    'spa-mobile'

--- app/Views/inc/footer.php
+++ app/Views/inc/footer.php
@@ -1,7 +1,10 @@
+
 <section class="main_section_notice">
+<div class="inner">
         <div class="body_inner">
-            <div class="main_section_notice__body">
+            <div class="main_section_notice__body only_web_flex">
                 <div class="notice__ttl">공지사항</div>
+
                 <div class="notice_list notice_swiper swiper">
                     <div class="swiper-wrapper">
                         <div class="swiper-slide">
@@ -36,8 +39,61 @@
                     </button>
                 </div>
             </div>
+
+
+
+            <div class="main_section_notice__body only_mo">
+
+                <div class="flex_mo_notice__body">
+                <div class="notice__ttl">공지사항</div>
+                <div class="swiper-button-box">
+                    <button class="notice_swiper_btn_prev notice_swiper_btn">
+                        <img src="/images/ico/ico_prev_slide.svg" alt="">
+                    </button>
+                    <button class="notice_swiper_btn_next notice_swiper_btn">
+                        <img src="/images/ico/ico_next_slide.svg" alt="">
+                    </button>
+                </div>
+                </div>
+
+
+                <div class="notice_list notice_swiper swiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="notice_item">
+                                <div class="notice_item__left">
+                                    <div class="notice_item__icon">공지</div>
+                                    <div class="notice_item__title">(여행소식) 수완나폼 공항내 그랩 택시 서비스 개시 </div>
+                                </div>
+                                <!-- <div class="notice_item__date">2022.05.23</div> -->
+                            </div>
+                            <!-- <div class="notice_item__date">2022.05.23</div> -->
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="notice_item">
+                                <div class="notice_item__left">
+                                    <div class="notice_item__icon">공지</div>
+                                    <div class="notice_item__title">(여행소식) 수완나폼 공항내 그랩 택시 서비스 개시 </div>
+                                </div>
+                               
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
+                <div class="flex_mobile_notice_item">
+                <div class="notice_item__date">2022.05.23</div>
+                <div> <a href="#!" class="main_section_notice__more">
+                    <img class="ico_plus" src="/images/ico/ico_plus.svg" alt="">
+                </a></div>
+                </div>
+               
+       
+            </div>
+        </div>
         </div>
     </section>
+
 <footer id="footer">
     <div class="inner">
         <div>
@@ -55,7 +111,8 @@
         <div class="flex_footer_bot">
             <div class="footer_l">
                 <div class="ft_logo">
-                    <img src="/images/sub/logo_footer.png" alt="">
+                    <img src="/images/sub/logo_footer.png" alt="" class="only_web">
+                    <img src="/uploads/sub/logo_footer_m.png" alt="" class="only_mo">
                 </div>
                 <div class="add_footer">
                     <p>대표이사 : 홍길동<br>
@@ -79,21 +136,23 @@
             <div class="footer_r">
                 <div>
                     <ul class="footer_icon">
-                        <li><a href=""><img src="/images/ico/ig_footer.png" alt=""></a></li>
-                        <li><a href=""><img src="/images/ico/bl_footer.png" alt=""></a></li>
-                        <li><a href=""><img src="/images/ico/n_footer.png" alt=""></a></li>
-                        <li><a href=""><img src="/images/ico/ytb_footer.png" alt=""></a></li>
+                        <li><a href=""><img class="only_web" src="/images/ico/ig_footer.png" alt=""> <img class="only_mo" src="/uploads/icons/ig_footer_m.png" alt=""></a></li>
+                        <li><a href=""><img class="only_web" src="/images/ico/bl_footer.png" alt=""> <img class="only_mo" src="/uploads/icons/bl_footer_m.png" alt=""></a></li>
+                        <li><a href=""><img class="only_web" src="/images/ico/n_footer.png" alt=""><img class="only_mo" src="/uploads/icons/n_footer_m.png" alt=""></a></li>
+                        <li><a href=""><img class="only_web" src="/images/ico/ytb_footer.png" alt=""><img class="only_mo" src="/uploads/icons/ytb_footer_m.png" alt=""></a></li>
+
+
 
                     </ul>
                 </div>
                 <div class="lh-1-6">
                     <p class="text-w text-18">고객센터</p>
-                    <p>한국에서 걸 때 <span class="text-w">070-7010-8266</span> (시내통화요금) (호텔/골프/투어/차량 상담)</p>
-                    <p>태국에서 걸 때 <span class="text-w">(0)2-730-5690</span> (방콕) 로밍폰, 태국 유심폰 모두 02-730-5690 번호만 누르면 됩니다.
+                    <p class="text-25">한국에서 걸 때 <span class="text-w"> 070-7010-8256</span> (시내통화요금) (호텔/골프/투어/차량 상담)</p>
+                    <p class="text-25">태국에서 걸 때 <span class="text-w"> (0)2-730-5690</span> (방콕) 로밍폰, 태국 유심폰 <br class="only_mo"> 모두 02-730-5690 번호만 누르면 됩니다.
                     </p>
                     <p class="text-18 p_bot_f">업무시간 :</p>
-                    <p>10:30 사무실 오픈 / 업무종료시간 - <span class="text-w">월~금 19:20, 토,일,공휴일 19:00</span> </p>
-                    <p>긴급예약처리 - <span class="text-w">매일 19:30~21:50 (한국시간)</span></p>
+                    <p class="text-25 no-w">10:30 사무실 오픈 / 업무종료시간 - <span class="text-w">월~금 19:20,토,일,공휴일 19:00</span> </p>
+                    <p class="text-25 no-w">긴급예약처리 - <span class="text-w">매일 19:30~21:50 (한국시간)</span></p>
                 </div>
                 <div class="btn_cus_f">
                     <button class="btn_custom_f">실시간 채팅형 간단 문의</button>
