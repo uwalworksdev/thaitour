a62cc7ea06c3e961eab884b3c0f75fc996808a58
tannv6 <tannv6@gmail.com>
2024-07-02 11:18:36 +0700

    tour mobile

--- app/Views/product/index.php
+++ app/Views/product/index.php
@@ -25,9 +25,9 @@
                     <div class="sub_tour__left__bottom">
                         <div class="sub_tour__slide__scroll"></div>
                         <div class="sub_tour__slide__paging">
-                            <img class="sub_tour__slide__paging__prev" src="/images/ico/ico_prev_slide.svg" alt="">
+                            <img class="sub_tour__slide__paging__prev" src="/images/ico/ico_prev_slide_1.svg" alt="">
                             <span class="sub_tour__slide__paging__divider"></span>
-                            <img class="sub_tour__slide__paging__next" src="/images/ico/ico_next_slide.svg" alt="">
+                            <img class="sub_tour__slide__paging__next" src="/images/ico/ico_next_slide_1.svg" alt="">
                         </div>
                     </div>
                 </div>
@@ -255,8 +255,27 @@
     <section class="sub_tour_section4">
         <div class="body_inner">
             <div class="sub_tour_section4_banner">
+                <div class="sub_tour_section4_banner__des">
+                    <p class="ico_block">
+                    <img src="/images/ico/ico_block_1.svg" alt="">    
+                    </p>
+                    <div class="sub_tour_section4_banner__des__text">
+                        <div class="sub_tour_section4_banner__des__ttl">
+                            여름휴가쿠폰대잔치
+                        </div>
+                        <p class="sub_tour_section4_banner__des__bottom">
+                            다운로드 기간 : 2024. 05. 22 ~ 07. 31
+                        </p>
+                    </div>
+                    <p class="ico_block">
+                    <img src="/images/ico/ico_block_1.svg" alt="">
+                    </p>
+                </div>
                 <div class="img_box img_box_11">
-                    <img src="/uploads/sub/banner_tour_1.png" alt="">
+                    <picture>
+                        <source media="(max-width: 850px)" srcset="/uploads/sub/banner_tour_1_m.png">
+                        <img class="img_box__img" src="/uploads/sub/banner_tour_1.png" alt="main">
+                    </picture>
                 </div>
             </div>
         </div>
@@ -293,7 +312,7 @@
                         <a href="#!" class="tour__head__tabs2__tab">
                             푸켓
                         </a>
-                        <a href="#!" class="tour__head__tabs2__tab">
+                        <!-- <a href="#!" class="tour__head__tabs2__tab">
                             치앙마이
                         </a>
                         <a href="#!" class="tour__head__tabs2__tab">
@@ -319,7 +338,7 @@
                         </a>
                         <a href="#!" class="tour__head__tabs2__tab">
                             기타지역
-                        </a>
+                        </a> -->
                     </div>
                     <select name="" id="">
                         <option value="">메뉴더보기</option>
@@ -443,7 +462,7 @@
                             d="M2.00001 10C2.00001 10.2589 1.89465 10.5073 1.70712 10.6904C1.51958 10.8735 1.26523 10.9764 1.00001 10.9764C0.734797 10.9764 0.480443 10.8735 0.292907 10.6904C0.105371 10.5073 1.41207e-05 10.2589 1.41207e-05 10C-0.00315467 8.16016 0.527064 6.35702 1.52937 4.79904C2.53167 3.24106 3.96513 1.99188 5.66401 1.1959C7.36289 0.399924 9.25782 0.08967 11.1297 0.301007C13.0016 0.512343 14.774 1.23664 16.242 2.39016V0.976372C16.242 0.717422 16.3473 0.469078 16.5349 0.285973C16.7224 0.102868 16.9768 0 17.242 0C17.5072 0 17.7616 0.102868 17.9491 0.285973C18.1366 0.469078 18.242 0.717422 18.242 0.976372V4.88186C18.242 5.14081 18.1366 5.38915 17.9491 5.57226C17.7616 5.75536 17.5072 5.85823 17.242 5.85823H13.242C12.9768 5.85823 12.7224 5.75536 12.5349 5.57226C12.3474 5.38915 12.242 5.14081 12.242 4.88186C12.242 4.62291 12.3474 4.37457 12.5349 4.19146C12.7224 4.00835 12.9768 3.90549 13.242 3.90549H14.985C13.8101 2.98468 12.3923 2.40715 10.8955 2.23961C9.39874 2.07207 7.88392 2.32136 6.52606 2.95867C5.16819 3.59599 4.02267 4.59534 3.22179 5.84129C2.42091 7.08725 1.99734 8.52899 2.00001 10ZM19 9.02363C18.7348 9.02363 18.4804 9.1265 18.2929 9.3096C18.1053 9.49271 18 9.74105 18 10C18.0027 11.471 17.5791 12.9127 16.7782 14.1587C15.9773 15.4047 14.8318 16.404 13.4739 17.0413C12.1161 17.6786 10.6013 17.9279 9.10446 17.7604C7.60766 17.5928 6.18993 17.0153 5.01501 16.0945H6.75701C7.02222 16.0945 7.27657 15.9916 7.46411 15.8085C7.65165 15.6254 7.757 15.3771 7.757 15.1181C7.757 14.8592 7.65165 14.6108 7.46411 14.4277C7.27657 14.2446 7.02222 14.1418 6.75701 14.1418H2.75701C2.49179 14.1418 2.23744 14.2446 2.0499 14.4277C1.86237 14.6108 1.75701 14.8592 1.75701 15.1181V19.0236C1.75701 19.2826 1.86237 19.5309 2.0499 19.714C2.23744 19.8971 2.49179 20 2.75701 20C3.02223 20 3.27658 19.8971 3.46412 19.714C3.65165 19.5309 3.75701 19.2826 3.75701 19.0236V17.6098C5.22511 18.7633 6.99756 19.4875 8.86946 19.6987C10.7414 19.91 12.6363 19.5998 14.3352 18.8038C16.0341 18.0079 17.4676 16.7588 18.4701 15.2009C19.4725 13.6429 20.0029 11.8398 20 10C20 9.74105 19.8946 9.49271 19.7071 9.3096C19.5196 9.1265 19.2652 9.02363 19 9.02363Z"
                             fill="black" />
                     </svg>
-                    <span>다음상품</span>
+                    <span class="prd_list_pagination__btn__text">다음상품</span>
                     <div class="prd_list_pagination__btn__pages">
                         <span class="prd_list_pagination__btn__current">1</span>
                         /
@@ -771,10 +790,13 @@
             },
         });
         const swiper12 = new Swiper(".sub_swiper2", {
-            loop: true,
-            slidesPerView: 6,
+            loop: false,
+            slidesPerView: 3,
+            slidesPerGroup: 3,
             spaceBetween: 20,
-            navigation: {
+            grid: {
+                rows: 2,
+                fill: 'row'
             },
             navigation: {
                 nextEl: ".sub_swiper2_btn_next",
@@ -783,21 +805,59 @@
             pagination: {
                 el: ".sub_swiper2_pagination",
             },
+            breakpoints: {
+                851: {
+                    loop: true,
+                    slidesPerView: 6,
+                    slidesPerGroup: 1,
+                    grid: {
+                        rows: 1,
+                        fill: 'column'
+                    },
+                },
+            },
+            on: {
+                beforeResize: function(){
+                    this.update();
+                    if (this.pagination && this.pagination.render && this.pagination.init) {
+                        this.pagination.render();
+                        this.pagination.init();
+                    }
+                    if (this.navigation && this.navigation.update) {
+                        this.navigation.update();
+                    }
+                },
+            },
         });
-
-        const swiper13 = new Swiper(".sub_section3_swiper", {
-            loop: true,
-            slidesPerView: 3,
-            spaceBetween: 20,
-            navigation: {
-            },
-            navigation: {
-                nextEl: ".sub_section3_swiper_btn_next",
-                prevEl: ".sub_section3_swiper_btn_prev",
-            },
-            pagination: {
-                el: ".sub_section3_swiper_pagination",
-            },
+        let swiper13 = undefined;
+        function initSwiper13() {
+            swiper13 = new Swiper(".sub_section3_swiper", {
+                loop: true,
+                slidesPerView: 1,
+                spaceBetween: 10,
+                breakpoints: {
+                    851: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                },
+                navigation: {
+                    nextEl: ".sub_section3_swiper_btn_next",
+                    prevEl: ".sub_section3_swiper_btn_prev",
+                },
+                pagination: {
+                    el: ".sub_section3_swiper_pagination",
+                },
+            });
+        }
+        initSwiper13();
+        $(window).resize(function () {
+            if (swiper1.navigation && swiper1.navigation.update) {
+                swiper1.navigation.update();
+            }
+            if (swiper1.scrollbar && swiper1.scrollbar.updateSize) {
+                swiper1.scrollbar.updateSize();
+            }
         });
     });
 </script>
