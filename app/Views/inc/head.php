4014bbdf607154a3d077c9333503e32572764bf7
dainqd <ngodaix5tp@gmail.com>
2024-07-10 08:18:23 +0700

    Custom ui in product-list page.

--- app/Views/inc/head.php
+++ app/Views/inc/head.php
@@ -10,7 +10,9 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/lib/swiper-11.1.4/package/swiper-bundle.min.css" />
     <link rel="stylesheet" href="/lib/summernote/summernote-lite.css">
+    <link rel="stylesheet" type="text/css" href="/css/base.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/custom.css">
     <script src="/js/script.js"></script>
     <script src="/lib/swiper-11.1.4/package/swiper-bundle.min.js"></script>
     <script src="/lib/slick/slick.js"></script>
