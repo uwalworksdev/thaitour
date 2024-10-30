eee872d57ebc1a9fe16b569c53ef3729c916164f
dainqd <ngodaix5tp@gmail.com>
2024-10-30 11:21:22 +0700

    Fix bug.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -183,7 +183,15 @@ $routes->group("AdmMaster", static function ($routes) {
             $routes->post("prod_update", "Admin\AdminSpaController::prod_update", ['as' => "admin.api.spa_.prod_update"]);
             $routes->post("ajax_change", "Admin\AdminSpaController::ajax_change", ['as' => "admin.api.spa_.ajax_change"]);
             $routes->post("del", "Admin\AdminSpaController::del", ['as' => "admin.api.spa_.del"]);
-            $routes->get("get_code", "Admin\AdminTourStayApi::get_code", ['as' => "admin.api.spa_.get_code"]);
+            $routes->get("get_code", "Admin\AdminSpaController::get_code", ['as' => "admin.api.spa_.get_code"]);
+            $routes->post("change_manager", "Api\AdminSpaController::change_manager", ['as' => "admin.api.spa_.change_manager"]);
+            $routes->post("add_moption", "Api\AdminSpaController::add_moption", ['as' => "admin.api.spa_.add_moption"]);
+            $routes->post("upd_moption", "Api\AdminSpaController::upd_moption", ['as' => "admin.api.spa_.upd_moption"]);
+            $routes->post("del_moption", "Api\AdminSpaController::del_moption", ['as' => "admin.api.spa_.del_moption"]);
+            $routes->post("add_option", "Api\AdminSpaController::add_option", ['as' => "admin.api.spa_.add_option"]);
+            $routes->post("upd_option", "Api\AdminSpaController::upd_option", ['as' => "admin.api.spa_.upd_option"]);
+            $routes->post("del_option", "Api\AdminSpaController::del_option", ['as' => "admin.api.spa_.del_option"]);
+            $routes->post("img_remove", "Api\AdminSpaController::img_remove", ['as' => "admin.api.spa_.img_remove"]);
         });
     });
 
