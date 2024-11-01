dd4c28d971fa0e28940b9aa747759ee2a58a6aa4
truong20 <vuxuantruong080920@gmail.com>
2024-11-01 17:14:09 +0700

    fix banner

--- app/Views/product/product-spa.php
+++ app/Views/product/product-spa.php
@@ -46,21 +46,21 @@
 
                     <?php foreach ($products as $product) { ?>
                         <div class="swiper-slide"
-                             onclick="location.href='/product-spa/spa-details/<?= $product['product_idx'] ?>'">
+                            onclick="location.href='/product-spa/spa-details/<?= $product['product_idx'] ?>'">
                             <div class="img_box_re">
                                 <?php
                                 if ($product["ufile1"] != "") {
-                                    ?>
+                                ?>
                                     <img class="only_web"
-                                         src="<?= base_url('/data/hotel/') . $product['ufile1'] ?>" alt="">
+                                        src="<?= base_url('/data/hotel/') . $product['ufile1'] ?>" alt="">
                                     <img class="only_mo img_box_re_img"
-                                         src="<?= base_url('/data/hotel/') . $product['ufile1'] ?>" alt="">
+                                        src="<?= base_url('/data/hotel/') . $product['ufile1'] ?>" alt="">
                                 <?php } else {
-                                    ?>
+                                ?>
                                     <img class="only_web"
-                                         src="/data/product/noimg.png" alt="">
+                                        src="/data/product/noimg.png" alt="">
                                     <img class="only_mo img_box_re_img"
-                                         src="/data/product/noimg.png" alt="">
+                                        src="/data/product/noimg.png" alt="">
                                 <?php }
                                 ?>
                                 <?php if ($product['product_type'] === "MD 추천") { ?>
@@ -94,15 +94,15 @@
         <div class="swiper-main-tools">
             <div class="play_pause" id="autoplay-button">
                 <svg id="pause-button" class="pause" width="6" height="10" viewBox="0 0 6 10" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <rect width="2" height="10" fill="#757575"/>
-                    <rect x="4" width="2" height="10" fill="#757575"/>
+                    xmlns="http://www.w3.org/2000/svg">
+                    <rect width="2" height="10" fill="#757575" />
+                    <rect x="4" width="2" height="10" fill="#757575" />
                 </svg>
                 <svg id="play-button" style="display: none;" class="play" width="8" height="10" viewBox="0 0 8 10"
-                     fill="none" xmlns="http://www.w3.org/2000/svg">
+                    fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd"
-                          d="M7.71975 4.48357L0.935104 0.11106C0.532604 -0.105726 0.0715332 -0.0832222 0.0715332 0.694992V9.305C0.0715332 10.0164 0.566176 10.1286 0.935104 9.88894L7.71975 5.51642C7.99904 5.23106 7.99904 4.76893 7.71975 4.48357Z"
-                          fill="#757575"/>
+                        d="M7.71975 4.48357L0.935104 0.11106C0.532604 -0.105726 0.0715332 -0.0832222 0.0715332 0.694992V9.305C0.0715332 10.0164 0.566176 10.1286 0.935104 9.88894L7.71975 5.51642C7.99904 5.23106 7.99904 4.76893 7.71975 4.48357Z"
+                        fill="#757575" />
                 </svg>
             </div>
             <div class="swiper-pagination-main">
@@ -111,6 +111,10 @@
             </div>
         </div>
         <div class="mid-banner-ticket">
+            <div class="box-text">
+                <h3 class="title-box">여름휴가쿠폰대잔치</h3>
+                <p class="des-box">다운로드 기간 : 2024. 05. 22 ~ 07. 31</p>
+            </div>
             <img class="only_web" src="<?= base_url('/images/sub/banner__spa01.png') ?>" alt="">
             <img class="only_mo" src="<?= base_url('/uploads/products/ticket-mid-banner-m.png') ?>" alt="">
         </div>
@@ -152,17 +156,17 @@
 
                                 <?php
                                 if ($product["ufile1"] != "") {
-                                    ?>
+                                ?>
                                     <img class="only_web"
-                                         src="<?= base_url('/data/hotel/') . $product['ufile1'] ?>" alt="">
+                                        src="<?= base_url('/data/hotel/') . $product['ufile1'] ?>" alt="">
                                     <img class="only_mo img_box_re_img"
-                                         src="<?= base_url('/data/hotel/') . $product['ufile1'] ?>" alt="">
+                                        src="<?= base_url('/data/hotel/') . $product['ufile1'] ?>" alt="">
                                 <?php } else {
-                                    ?>
+                                ?>
                                     <img class="only_web"
-                                         src="/data/product/noimg.png" alt="">
+                                        src="/data/product/noimg.png" alt="">
                                     <img class="only_mo img_box_re_img"
-                                         src="/data/product/noimg.png" alt="">
+                                        src="/data/product/noimg.png" alt="">
                                 <?php }
                                 ?>
 
@@ -188,9 +192,9 @@
                             </div>
                             <div class="prd_price_ko prd_price_ko_m">
                                 <span class="prd_price"><?= number_format($product['product_price']) ?></span> <span
-                                        class="ko_m_price y_price">원</span>
+                                    class="ko_m_price y_price">원</span>
                                 <span class="prd_price_thai ko_m_price">(6,000 <span
-                                            class="ko_m_price">바트)</span></span>
+                                        class="ko_m_price">바트)</span></span>
                             </div>
                         </a>
                     <?php } ?>
@@ -202,7 +206,7 @@
                 <div class="prd_list_pagination__btn">
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M2.00001 10C2.00001 10.2589 1.89465 10.5073 1.70712 10.6904C1.51958 10.8735 1.26523 10.9764 1.00001 10.9764C0.734797 10.9764 0.480443 10.8735 0.292907 10.6904C0.105371 10.5073 1.41207e-05 10.2589 1.41207e-05 10C-0.00315467 8.16016 0.527064 6.35702 1.52937 4.79904C2.53167 3.24106 3.96513 1.99188 5.66401 1.1959C7.36289 0.399924 9.25782 0.08967 11.1297 0.301007C13.0016 0.512343 14.774 1.23664 16.242 2.39016V0.976372C16.242 0.717422 16.3473 0.469078 16.5349 0.285973C16.7224 0.102868 16.9768 0 17.242 0C17.5072 0 17.7616 0.102868 17.9491 0.285973C18.1366 0.469078 18.242 0.717422 18.242 0.976372V4.88186C18.242 5.14081 18.1366 5.38915 17.9491 5.57226C17.7616 5.75536 17.5072 5.85823 17.242 5.85823H13.242C12.9768 5.85823 12.7224 5.75536 12.5349 5.57226C12.3474 5.38915 12.242 5.14081 12.242 4.88186C12.242 4.62291 12.3474 4.37457 12.5349 4.19146C12.7224 4.00835 12.9768 3.90549 13.242 3.90549H14.985C13.8101 2.98468 12.3923 2.40715 10.8955 2.23961C9.39874 2.07207 7.88392 2.32136 6.52606 2.95867C5.16819 3.59599 4.02267 4.59534 3.22179 5.84129C2.42091 7.08725 1.99734 8.52899 2.00001 10ZM19 9.02363C18.7348 9.02363 18.4804 9.1265 18.2929 9.3096C18.1053 9.49271 18 9.74105 18 10C18.0027 11.471 17.5791 12.9127 16.7782 14.1587C15.9773 15.4047 14.8318 16.404 13.4739 17.0413C12.1161 17.6786 10.6013 17.9279 9.10446 17.7604C7.60766 17.5928 6.18993 17.0153 5.01501 16.0945H6.75701C7.02222 16.0945 7.27657 15.9916 7.46411 15.8085C7.65165 15.6254 7.757 15.3771 7.757 15.1181C7.757 14.8592 7.65165 14.6108 7.46411 14.4277C7.27657 14.2446 7.02222 14.1418 6.75701 14.1418H2.75701C2.49179 14.1418 2.23744 14.2446 2.0499 14.4277C1.86237 14.6108 1.75701 14.8592 1.75701 15.1181V19.0236C1.75701 19.2826 1.86237 19.5309 2.0499 19.714C2.23744 19.8971 2.49179 20 2.75701 20C3.02223 20 3.27658 19.8971 3.46412 19.714C3.65165 19.5309 3.75701 19.2826 3.75701 19.0236V17.6098C5.22511 18.7633 6.99756 19.4875 8.86946 19.6987C10.7414 19.91 12.6363 19.5998 14.3352 18.8038C16.0341 18.0079 17.4676 16.7588 18.4701 15.2009C19.4725 13.6429 20.0029 11.8398 20 10C20 9.74105 19.8946 9.49271 19.7071 9.3096C19.5196 9.1265 19.2652 9.02363 19 9.02363Z"
-                              fill="black"/>
+                            fill="black" />
                     </svg>
                     <span class="prd_list_pagination__btn__text">다음상품</span>
                     <div class="prd_list_pagination__btn__pages">
@@ -247,10 +251,10 @@
             }
         },
         on: {
-            init: function () {
+            init: function() {
                 updateSlideCounter(this);
             },
-            slideChange: function () {
+            slideChange: function() {
                 updateSlideCounter(this);
             }
         }
@@ -263,7 +267,7 @@
         document.querySelector('.main_total_slide').innerText = totalSlides;
     }
 
-    document.getElementById('autoplay-button').addEventListener('click', function () {
+    document.getElementById('autoplay-button').addEventListener('click', function() {
         var playButton = document.getElementById('play-button');
         var pauseButton = document.getElementById('pause-button');
         if (swiper.autoplay.running) {
