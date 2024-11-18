a8499462e8ce8dbce104b4c4afbb9508c71645c5
diana tgkim <diana001@naver.com>
2024-11-19 04:12:12 +0900

    modified:   app/Models/MainDispModel.php

--- app/Models/MainDispModel.php
+++ app/Models/MainDispModel.php
@@ -63,16 +63,17 @@ class MainDisp extends Model
 
     public function List($code,  $where = [])
     {
-        //$builder = $this;
-        //$builder->select("{$this->table}.*  ");
-        //$builder->where('code_no', $code);
-        //$builder->orderBy("{$this->table}.code_no", "desc");
-
         $builder = $this->db->table('tbl_main_disp as s1')
-            ->select('s1.*, s2.*')
-            ->join('tbl_product_mst as s2', 's1.product_idx = s2.product_idx', 'left')
-            ->where('s1.code_no =', '$code')
-            ->where('s1.status  =', 'Y');
+        $builder->select('s1.*, s2.*');
+        $builder->join('tbl_product_mst as s2', 's1.product_idx = s2.product_idx', 'left')
+        $builder->where('s1.code_no', $code);
+        $builder->orderBy("s1.code_no", "desc");
+
+        //$builder = $this->db->table('tbl_main_disp as s1')
+        //    ->select('s1.*, s2.*')
+        //    ->join('tbl_product_mst as s2', 's1.product_idx = s2.product_idx', 'left')
+        //    ->where('s1.code_no =', '$code')
+        //    ->where('s1.status  =', 'Y');
 
         return $builder;
     }
