f9ea6b8f2ffe36f72e0b7071c0e44a0d577699b7
diana tgkim <diana001@naver.com>
2024-11-19 15:52:47 +0900

    modified:   app/Controllers/Admin/AdminBbsBannerController.php

--- app/Controllers/Admin/AdminBbsBannerController.php
+++ app/Controllers/Admin/AdminBbsBannerController.php
@@ -90,4 +90,48 @@ class AdminBbsBannerController extends BaseController
         $row_cate = $result_cate->getRowArray();
         return $row_cate;
     }
+
+    public function banner_change()
+    {
+        try {
+            $code_idx = $_POST['code_idx'] ?? [];
+            $onum     = $_POST['onum'] ?? [];
+
+            if (empty($code_idx)) {
+                return $this->response
+                    ->setStatusCode(400)
+                    ->setJSON([
+                        'status'  => 'error',
+                        'message' => 'No code_idx provided'
+                    ]);
+            }
+
+            $tot = count($code_idx);
+            for ($j = 0; $j < $tot; $j++) {
+
+                $sql    = " update tbl_code set onum='" . $onum[$j] . "' where code_idx='" . $code_idx[$j] . "'";
+                $result = $this->connect->query($sql);
+            }
+
+            if (isset($result) && $result) {
+                $msg = "순위변경 완료";
+            } else {
+                $msg = "순위변경 오류";
+            }
+
+            return $this->response
+                ->setStatusCode(200)
+                ->setJSON([
+                    'status' => 'success',
+                    'message' => $msg
+                ]);
+        } catch (\Exception $e) {
+            return $this->response
+                ->setStatusCode(400)
+                ->setJSON([
+                    'status' => 'error',
+                    'message' => $e->getMessage()
+                ]);
+        }
+    }
 }
