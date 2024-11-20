b91ffab70540fda32fe92adfd942716b77896b37
diana tgkim <diana001@naver.com>
2024-11-21 02:30:07 +0900

    modified:   app/Views/main/main.php

--- app/Views/main/main.php
+++ app/Views/main/main.php
@@ -474,9 +474,7 @@ $list5 = $MainDisp->List("2905")->findAll();
 				});
 
 				$('.main_section3__type_btn').each(function (index) {
-					if ($(this).hasClass('active')) {
-						code = $(this).data('code');
-					}
+					code = $(this).data('code');
 				});
 
 				alert(list+' - '+code);
