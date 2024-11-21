55d8326982ef3335ba638f3a459094191abc2b89
diana tgkim <diana001@naver.com>
2024-11-21 22:38:05 +0900

    new file:   public/1.php

--- app/Views/admin/_hotel/write.php
+++ app/Views/admin/_hotel/write.php
@@ -609,7 +609,6 @@ $links = "list";
 											</table>
 										</div>
                                         
-										<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 										<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 										<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
 										<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui/1.13.2/i18n/datepicker-ko.min.js"></script>
