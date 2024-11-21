65cc156cd119bb5c0d4e32a391f1279739c928e9
diana tgkim <diana001@naver.com>
2024-11-22 01:59:05 +0900

    modified:   public/js/main.js

--- public/js/main.js
+++ public/js/main.js
@@ -124,9 +124,16 @@ $(document).ready(function () {
 
     $(this).addClass("active");
   });
-  $(".place_item").on("click", function (event) {
+
+  $(".place_item_golf").on("click", function (event) {
     event.preventDefault();
-    $(".place_item").removeClass("active");
+    $(".place_item_golf").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $(".place_item_hotel").on("click", function (event) {
+    event.preventDefault();
+    $(".place_item_hotel").removeClass("active");
     $(this).addClass("active");
   });
   
