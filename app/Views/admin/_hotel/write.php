b23634ae9893d26099e3b57852932574c101eb76
diana tgkim <diana001@naver.com>
2024-11-21 21:15:54 +0900

    modified:   app/Views/admin/_hotel/write.php

--- app/Views/admin/_hotel/write.php
+++ app/Views/admin/_hotel/write.php
@@ -965,10 +965,10 @@ $links = "list";
                                                             <td>
                                                                 <input type='text' readonly class='datepicker '
                                                                        name='o_sdate[]'
-                                                                       value='<?= $frow3['o_sdate'] ?>'/> ~
+                                                                       value='<?= $frow3['o_sdate'] ?>' style='width:50%' /> ~
                                                                 <input type='text' readonly class='datepicker '
                                                                        name='o_edate[]'
-                                                                       value='<?= $frow3['o_edate'] ?>'/>
+                                                                       value='<?= $frow3['o_edate'] ?>' style='width:50%' />
                                                             </td>
                                                             <td>
                                                                 <input type='text' class='onlynum' name='o_price1[]'
