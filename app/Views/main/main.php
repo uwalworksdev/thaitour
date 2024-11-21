16aea648daab7fc1f7efe0d0d782b2b888e4dd29
diana tgkim <diana001@naver.com>
2024-11-22 01:31:11 +0900

    modified:   app/Views/main/main.php

--- app/Views/main/main.php
+++ app/Views/main/main.php
@@ -396,10 +396,10 @@ $list5 = $MainDisp->List("2905")->findAll();
                         1주일간 예약순위 : <span>호텔</span>
                     </div>
                     <div class="main_hot__head__place only_web_flex">
-                        <a href="#!" class="place_item active">방콕</a>
-                        <a href="#!" class="place_item">파타야</a>
-                        <a href="#!" class="place_item">푸켓</a>
-                        <a href="#!" class="place_item">치앙마이</a>
+                        <a href="#!" class="place_item hotel active">방콕</a>
+                        <a href="#!" class="place_item hotel">파타야</a>
+                        <a href="#!" class="place_item hotel">푸켓</a>
+                        <a href="#!" class="place_item hotel">치앙마이</a>
                     </div>
                 </div>
                 <div class="main_hot__head__right">
@@ -552,10 +552,10 @@ $list5 = $MainDisp->List("2905")->findAll();
                         1주일간 예약순위 : <span>골프</span>
                     </div>
                     <div class="main_hot__head__place only_web_flex">
-                        <div class="place_item_golf active">방콕</div>
-                        <div class="place_item_golf">파타야</div>
-                        <div class="place_item_golf">푸켓</div>
-                        <div class="place_item_golf">치앙마이</div>
+                        <div class="place_item golf active">방콕</div>
+                        <div class="place_item golf">파타야</div>
+                        <div class="place_item golf">푸켓</div>
+                        <div class="place_item golf">치앙마이</div>
                     </div>
                 </div>
                 <div class="main_hot__head__right">
