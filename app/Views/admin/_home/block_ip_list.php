547e7783678def51b2b3393546e6befd600cfc23
diana tgkim <diana001@naver.com>
2024-11-24 19:23:54 +0900

    modified:   app/Views/admin/_home/block_ip_list.php

--- app/Views/admin/_home/block_ip_list.php
+++ app/Views/admin/_home/block_ip_list.php
@@ -145,17 +145,43 @@
 
         function SELECT_DELETE() {
 
-			var tmpChkCnt = $('.m_idx:checked').length;
+			var tmpChkCnt = 0;
 
 			if (tmpChkCnt == 0) {
                 alert_("삭제할 내용을 선택하셔야 합니다.");
-                return false;
+                return;
             }
 
 			if (confirm("삭제 하시겠습니까?\n삭제후에는 복구가 불가능합니다.") == false) {
                 return;
             }
 
+            $("#ajax_loader").removeClass("display-none");
+
+            $.ajax({
+                url: "ip_del.php",
+                type: "POST",
+                data: $("#frm").serialize(),
+                error: function (request, status, error) {
+                    //통신 에러 발생시 처리
+                    alert_("code : " + request.status + "\r\nmessage : " + request.reponseText);
+                    $("#ajax_loader").addClass("display-none");
+                }
+                , complete: function (request, status, error) {
+                    //				$("#ajax_loader").addClass("display-none");
+                }
+                , success: function (response, status, request) {
+                    if (response == "OK") {
+                        alert_("정상적으로 삭제되었습니다.");
+                        location.reload();
+                        return;
+                    } else {
+                        alert(response);
+                        alert_("오류가 발생하였습니다!!");
+                        return;
+                    }
+                }
+            });
 
         }
 
