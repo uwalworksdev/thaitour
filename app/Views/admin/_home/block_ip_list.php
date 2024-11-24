5e307f92b2e37ed3747a3193a31390015d326d4f
diana tgkim <diana001@naver.com>
2024-11-24 19:31:17 +0900

    modified:   app/Views/admin/_home/block_ip_list.php

--- app/Views/admin/_home/block_ip_list.php
+++ app/Views/admin/_home/block_ip_list.php
@@ -144,10 +144,10 @@
         }
 
         function SELECT_DELETE() {
-			var tmpChkCnt = $('.m_idx:checked').length *1;
+			var tmpChkCnt = $('.m_idx:checked').length;
 alert(tmpChkCnt);
 
-			if (tmpChkCnt == 0) {
+			if (tmpChkCnt == "0") {
                 alert_("삭제할 내용을 선택하셔야 합니다.");
                 return false;
             }
