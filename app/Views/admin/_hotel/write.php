1703e2749df38222852a9f835190d11888275309
diana tgkim <diana001@naver.com>
2024-11-25 10:53:35 +0900

    modified:   app/Views/admin/_hotel/write.php

--- app/Views/admin/_hotel/write.php
+++ app/Views/admin/_hotel/write.php
@@ -822,7 +822,7 @@ $links = "list";
                                                     <thead>
                                                     <tr>
                                                         <th>객실명</th>
-                                                        <th>기간</th>
+                                                        <th>기간.</th>
                                                         <th>가격</th>
                                                         <th>마감날짜</th>
                                                         <th>삭제</th>
