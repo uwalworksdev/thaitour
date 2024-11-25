7951558e2a5719bc1cd4e8781e48b4f524da632b
dainqd <ngodaix5tp@gmail.com>
2024-11-25 09:18:33 +0700

    Fix layout product hotel.

--- app/Views/product/product-hotel.php
+++ app/Views/product/product-hotel.php
@@ -33,19 +33,24 @@
     <script type="text/javascript" src="/lib/daterangepicker/daterangepicker.min.js"></script>
     <div class="main_page_01 page_share_ page_product_list_">
         <section class="sub_top_visual">
-            <img class="only_web" src="/data/cate_banner/<?=$bannerTop['ufile1']?>" alt="">
-            <img class="only_mo" src="/data/cate_banner/<?=$bannerTop['ufile2']?>" alt="">
+            <img class="only_web" src="/data/cate_banner/<?= $bannerTop['ufile1'] ?>" alt="">
+            <img class="only_mo" src="/data/cate_banner/<?= $bannerTop['ufile2'] ?>" alt="">
             <div class="main_visual_content_">
                 <div class="text_title">지금, 바로 떠날 수 있는 이유</div>
                 <div class="form_search">
                     <div class="form_element_">
+                        <div class="form_input_">
+                            <label for="input_day">여행지</label>
+                            <input type="text" id="" class="input_text_" placeholder="호텔 지역을 입력해주세요!">
+                        </div>
                         <div class="form_input_">
                             <label for="input_day">체크인/체크아웃 날짜</label>
                             <input type="text" id="input_day" class="input_custom_" placeholder="날짜를 선택해주세요." readonly>
                         </div>
                         <div class="form_input_">
                             <label for="input_hotel">호텔명(미입력 시 전체)</label>
-                            <input type="text" style="text-transform: none;" id="input_hotel" class="input_custom_" placeholder="호텔명을 입력해주세요.">
+                            <input type="text" style="text-transform: none;" id="input_hotel" class="input_custom_"
+                                   placeholder="호텔명을 입력해주세요.">
                         </div>
                         <button type="button" onclick="search_list();" class="btn_search_">
                             검색
@@ -204,11 +209,12 @@
                 </div>
             </div>
         </section>
-        <?php if($bannerMiddle) : ?>
+        <?php if ($bannerMiddle) : ?>
             <style>
                 .main_page_01 .banner_section_main_page .banner_section_image {
                     background-image: url('/data/cate_banner/<?=$bannerMiddle['ufile1']?>');
                 }
+
                 @media screen and (max-width: 850px) {
                     .main_page_01 .banner_section_main_page .banner_section_image {
                         background-image: url('/data/cate_banner/<?=$bannerMiddle['ufile2']?>');
@@ -217,10 +223,10 @@
             </style>
             <section class="banner_section_main_page">
                 <div class="body_inner">
-                    <a href="<?=$bannerMiddle['url']?>" class="banner_section_image" style="position: relative;">
+                    <a href="<?= $bannerMiddle['url'] ?>" class="banner_section_image" style="position: relative;">
                         <div class="box-text">
-                            <h3 class="title-box"><?=viewSQ($bannerMiddle['title'])?></h3>
-                            <p class="des-box"><?=viewSQ($bannerMiddle['subtitle'])?></p>
+                            <h3 class="title-box"><?= viewSQ($bannerMiddle['title']) ?></h3>
+                            <p class="des-box"><?= viewSQ($bannerMiddle['subtitle']) ?></p>
                         </div>
                     </a>
                 </div>
@@ -358,13 +364,13 @@
             <div class="body_inner">
                 <div class="sub_tour_section8__banners">
                     <div class="sub_tour_section8__banner">
-                        <a href="<?=$bannerBottom[0]['url']?>" target="_blank" class="img_box img_box_13 ">
-                            <img src="/data/cate_banner/<?=$bannerBottom[0]['ufile1']?>" alt="">
+                        <a href="<?= $bannerBottom[0]['url'] ?>" target="_blank" class="img_box img_box_13 ">
+                            <img src="/data/cate_banner/<?= $bannerBottom[0]['ufile1'] ?>" alt="">
                         </a>
                     </div>
                     <div class="sub_tour_section8__banner">
-                        <a href="<?=$bannerBottom[1]['url']?>" target="_blank" class="img_box img_box_13">
-                            <img src="/data/cate_banner/<?=$bannerBottom[1]['ufile1']?>" alt="">
+                        <a href="<?= $bannerBottom[1]['url'] ?>" target="_blank" class="img_box img_box_13">
+                            <img src="/data/cate_banner/<?= $bannerBottom[1]['ufile1'] ?>" alt="">
                         </a>
                     </div>
                 </div>
