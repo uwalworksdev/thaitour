3b7fc07aefc30d6159ef30977bbfd15e9102f49d
diana tgkim <diana001@naver.com>
2024-11-27 04:04:42 +0900

    modified:   app/Controllers/Admin/AdminHotelController.php

--- app/Controllers/Admin/AdminHotelController.php
+++ app/Controllers/Admin/AdminHotelController.php
@@ -282,7 +282,7 @@ class AdminHotelController extends BaseController
                                         ,o_soldout		= '" . $item_soldout . "'
                                 ";
                             $result_opt = $this->connect->query($sql_su);
-							$option_idx = $connect->insert_id;
+					        $option_idx = mysqli_insert_id($connect);
 
 							$dateRange = getDateRange($item_sdate, $item_edate);
 
@@ -410,7 +410,7 @@ class AdminHotelController extends BaseController
                                     ,o_soldout		= '" . $item_soldout . "'
                             ";
 					$result_opt = $this->connect->query($sql_su);
-					$option_idx = $connect->insert_id;
+					$option_idx = mysqli_insert_id($connect);
 
 					$dateRange = getDateRange($item_sdate, $item_edate);
 
