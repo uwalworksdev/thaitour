ae686ca21ea6661a825d3e5caa5d29764a1976a0
diana tgkim <diana001@naver.com>
2024-11-29 11:42:45 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -229,7 +229,7 @@
                         </p>
                         <p>
                             <span class="l-label">티오프시간</span>
-                            <span class="l-label2"><em class="final_hour">00</em> <em class="final_minute">00</em>분></span>
+                            <span class="l-label2"><em class="final_hour">00</em>시<em class="final_minute">00</em>분</span>
                         </p>
                         <p>
                             <span class="l-label">인원</span>
