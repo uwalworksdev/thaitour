432920758d587974b3fdb18a90877dcb7aad71a7
diana tgkim <diana001@naver.com>
2024-11-30 02:14:18 +0900

    modified:   app/Models/GolfOptionModel.php

--- app/Models/GolfOptionModel.php
+++ app/Models/GolfOptionModel.php
@@ -41,7 +41,7 @@ class GolfOptionModel extends Model
 
     public function getOptions($product_idx, $hole_cnt = null, $hour = null, $minute = null)
     {
-		$this->table = 'tbl_golf_price'; 
+		//$this->table = 'tbl_golf_price'; 
         $options = $this->where("product_idx", $product_idx);
         if ($hole_cnt) {
             $options = $options->where("hole_cnt", $hole_cnt);
