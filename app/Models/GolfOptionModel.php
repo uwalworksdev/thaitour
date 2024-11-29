8d7462062e83ebd8b525791abdca0722b6042191
diana tgkim <diana001@naver.com>
2024-11-30 01:42:10 +0900

    modified:   app/Models/GolfOptionModel.php

--- app/Models/GolfOptionModel.php
+++ app/Models/GolfOptionModel.php
@@ -39,7 +39,7 @@ class GolfOptionModel extends Model
 
     protected $useSoftDeletes = false;
 
-    public function getOptions($product_idx)
+    public function getOptions($product_idx, $hole_cnt = null, $hour = null, $minute = null)
     {
 		 
         $options = $this->where("product_idx", $product_idx);
