92ff0667a46c89d3cd48d2cb63c56393616e7361
diana tgkim <diana001@naver.com>
2024-11-29 20:29:44 +0900

    modified:   app/Views/product/golf/option_list.php

--- app/Views/product/golf/option_list.php
+++ app/Views/product/golf/option_list.php
@@ -1,18 +1,17 @@
 <?php foreach ($options as $option) { ?>
     <div class="card-item" 
-        data-hole="<?=$option['hole_cnt']?>" 
-        data-hour="<?=$option['hour']?>" 
-        data-minute="<?=$option['minute']?>"
-        data-idx="<?=$option['idx']?>"
-        data-option_price="<?=$option['option_price']?>"
-        data-option_price_baht="<?=$option['option_price_baht']?>"
-        data-caddy_fee="<?=$option['caddy_fee']?>"
-        data-cart_pie_fee="<?=$option['cart_pie_fee']?>"
-        onclick="selectOption(this)">
+        data-hole              = "<?=$option['hole_cnt']?>" 
+        data-hour              = "<?=$option['hour']?>" 
+        data-minute            = "<?=$option['minute']?>"
+        data-idx               = "<?=$option['idx']?>"
+        data-option_price      = "<?=$option['option_price']?>"
+        data-option_price_baht = "<?=$option['option_price_baht']?>"
+        data-caddy_fee         = "<?=$option['caddy_fee']?>"
+        data-cart_pie_fee      = "<?=$option['cart_pie_fee']?>" onclick="selectOption(this)">
         <div class="header">
             <div class="header-con">
-                <img class="only_web" src="/uploads/icons/timer_gray_icon.png" alt="timer_gray_icon">
-                <img class="only_mo" src="/uploads/icons/timer_gray_icon_mo.png" alt="timer_gray_icon_mo">
+                <img class="only_web" src="/uploads/icons/timer_gray_icon.png"    alt="timer_gray_icon">
+                <img class="only_mo"  src="/uploads/icons/timer_gray_icon_mo.png" alt="timer_gray_icon_mo">
                 <p><?=$option['hour']?>:<?=$option['minute']?></p>
             </div>
             <p class="cus-text">캐디피 : <?=$option['caddy_fee']?></p>
