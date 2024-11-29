261195bd427ec0e9afa228c15872966262bdd450
diana tgkim <diana001@naver.com>
2024-11-30 00:57:31 +0900

    modified:   app/Views/admin/_tourRegist/write_golf.php

--- app/Views/admin/_tourRegist/write_golf.php
+++ app/Views/admin/_tourRegist/write_golf.php
@@ -772,9 +772,9 @@
                                             <col width="8%"/>
                                             <col width="8%"/>
                                             <col width="8%"/>
-                                            <col width="12%"/>
-                                            <col width="12%"/>
-                                            <col width="10%"/>
+                                            <col width="11%"/>
+                                            <col width="11%"/>
+                                            <col width="9%"/>
                                         </colgroup>
                                         <thead>
                                             <tr>
