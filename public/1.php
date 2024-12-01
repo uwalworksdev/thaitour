73bd360b59016a7fbd8967ee06b15809a7271580
diana tgkim <diana001@naver.com>
2024-12-01 11:18:07 +0900

    new file:   app/Controllers/AjaxController.php.tmp 	new file:   app/Views/admin/_tourRegist/write_golf_price.php.tmp 	modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -1,42 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Datepicker Example</title>
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
-</head>
-<body>
-    <label for="start_date">Start Date:</label>
-    <input type="text" name="start_date[]" class="start_date">
-    <label for="end_date">End Date:</label>
-    <input type="text" name="end_date[]" class="end_date">
+<?php
+            $from_date = "2024-12-01";
+            $days = "10";
+			$date = new DateTime($from_date);
 
-    <label for="start_date">Start Date:</label>
-    <input type="text" name="start_date[]" class="start_date">
-    <label for="end_date">End Date:</label>
-    <input type="text" name="end_date[]" class="end_date">
+			// 일 추가
+			$date->modify('+ $days days');
 
-    <script>
-        $(function () {
-            // 시작 날짜
-            $(".start_date").datepicker({
-                dateFormat: "yy-mm-dd",
-                onClose: function (selectedDate) {
-                    $(".end_date").datepicker("option", "minDate", selectedDate);
-                }
-            });
-
-            // 종료 날짜
-            $(".end_date").datepicker({
-                dateFormat: "yy-mm-dd",
-                onClose: function (selectedDate) {
-                    $(".start_date").datepicker("option", "maxDate", selectedDate);
-                }
-            });
-        });
-    </script>
-</body>
-</html>
+			// 결과 출력
+			$to_date   = $date->format('Y-m-d'); // 2024-12-10
+            echo $to_date;
+?>
