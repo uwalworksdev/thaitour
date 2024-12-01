5e2d0ee7a70f8eb295f548dab7f35946b2e72d86
diana tgkim <diana001@naver.com>
2024-12-01 20:02:53 +0900

    modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -1,12 +1,6 @@
 <?php
-            $from_date = "2024-12-01";
-            $days = "10";
-			$date = new DateTime($from_date);
-
-			// 일 추가
-			$date->modify('+'. $days .'days');
-
-			// 결과 출력
-			$to_date   = $date->format('Y-m-d'); // 2024-12-10
-            echo $to_date;
+define('GOLF_HOLES', [18, 27, 36, 45]);
+foreach (GOLF_HOLES as $hole) {
+    echo $hole . PHP_EOL; // 각 값 출력
+}
 ?>
