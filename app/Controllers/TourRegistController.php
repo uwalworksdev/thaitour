98f5eada4fe1df05ca927b32ea6e0dbfa862b320
diana tgkim <diana001@naver.com>
2024-12-03 19:12:08 +0900

    modified:   app/Controllers/TourRegistController.php

--- app/Controllers/TourRegistController.php
+++ app/Controllers/TourRegistController.php
@@ -646,7 +646,7 @@ class TourRegistController extends BaseController
         return view("admin/_tourRegist/write_spas", $data);
     }
 
-    public function write_toursx()
+    public function write_tours()
     {
         $product_idx = updateSQ($_GET["product_idx"] ?? '');
         $data = $this->getWrite('', '', '1301', '', '');
