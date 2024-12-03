02e10c14930226508fb36014237e4f104c5ecb36
diana tgkim <diana001@naver.com>
2024-12-03 16:47:11 +0900

    modified:   app/Views/admin/_tourRegist/write_tours.php

--- app/Views/admin/_tourRegist/write_tours.php
+++ app/Views/admin/_tourRegist/write_tours.php
@@ -92,7 +92,7 @@
             }
         };
 		
-		var clareCalendar2 = {
+		var clareCalendar2 = {  
             dateFormat : 'yy-m-dd',
             monthNamesShort : [ '1월', '2월', '3월', '4월', '5월', '6월','7월','8월','9월','10월','11월','12월'],
             monthNames : [ '1월', '2월', '3월', '4월', '5월', '6월','7월','8월','9월','10월','11월','12월'],
