b8075b7543086928c777e352273f8907c1bfc1c6
diana tgkim <diana001@naver.com>
2024-12-05 23:29:49 +0900

    modified:   app/Views/payment_failed.php

--- app/Views/payment_failed.php
+++ app/Views/payment_failed.php
@@ -1,8 +1,4 @@
 <h1>결제가 실패했습니다.</h1>
 <p>오류 메시지: <?= esc($message) ?></p>
-<?php
-if (isset($response['status']) && $response['status'] !== 'SUCCESS') {
-    echo "오류 메시지: " . $response['message'];
-}
 
 ?>
