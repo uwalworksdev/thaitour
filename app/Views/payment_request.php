febdd26a85506903e472c84a7e7bf955927033ee
diana tgkim <diana001@naver.com>
2024-12-05 23:17:56 +0900

    new file:   app/Views/payment_failed.php 	new file:   app/Views/payment_request.php 	new file:   app/Views/payment_success.php

---
+++ app/Views/payment_request.php
@@ -0,0 +1,7 @@
+<form action="/payment/request" method="get">
+    <label>상품명:</label>
+    <input type="text" name="orderName" value="테스트 상품" readonly><br>
+    <label>결제 금액:</label>
+    <input type="number" name="amount" value="10000" readonly><br>
+    <button type="submit">결제하기</button>
+</form>
