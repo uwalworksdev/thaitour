762ff56cafc5bedfdff4c13b33d62178f3643fcc
diana tgkim <diana001@naver.com>
2024-12-06 02:29:56 +0900

    modified:   app/Views/payment_request.php

--- app/Views/payment_request.php
+++ app/Views/payment_request.php
@@ -3,5 +3,7 @@
     <input type="text" name="orderName" value="테스트 상품" readonly><br>
     <label>결제 금액:</label>
     <input type="number" name="amount" value="10000" readonly><br>
+    <label>주문번호</label>
+    <input type="text" name="orderId" value="<?=time();?>" readonly><br>
     <button type="submit">결제하기</button>
 </form>
