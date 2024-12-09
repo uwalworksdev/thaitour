feef722ef4d5d1347ab8d67828220068df4714c4
datdo <datvbhp123@gmail.com>
2024-12-09 10:53:05 +0700

    fix-images-admin

--- app/Views/admin/_hotel/write.php
+++ app/Views/admin/_hotel/write.php
@@ -1058,7 +1058,8 @@ $links = "list";
                                             <div class="img_add">
                                                 <?php 
                                                     for($i = 1; $i <= 1; $i++) : 
-                                                        $img = get_img(${"ufile" . $i}, "/public/data/product/", "200", "200");
+                                                        // $img = get_img(${"ufile" . $i}, "/data/product/", "200", "200");
+                                                        $img ="/data/product/" . ${"ufile" . $i};
                                                 ?>
                                                     <div class="file_input <?=empty(${"ufile" . $i}) ? "" : "applied"?>">
                                                         <input type="file" name='ufile<?=$i?>' id="ufile<?=$i?>" onchange="productImagePreview(this, '<?=$i?>')">
@@ -1081,7 +1082,8 @@ $links = "list";
                                             <div class="img_add">
                                             <?php 
                                                 for($i = 2; $i <= 7; $i++) : 
-                                                    $img = get_img(${"ufile" . $i}, "/public/data/product/", "200", "200");
+                                                    // $img = get_img(${"ufile" . $i}, "/data/product/", "200", "200");
+                                                    $img ="/data/product/" . ${"ufile" . $i};
                                             ?>
                                                 <div class="file_input <?=empty(${"ufile" . $i}) ? "" : "applied"?>">
                                                     <input type="file" name='ufile<?=$i?>' id="ufile<?=$i?>" onchange="productImagePreview(this, '<?=$i?>')">
