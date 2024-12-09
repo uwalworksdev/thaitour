b20033782dd219fedcdce6cc35435867fd3e3411
datdo <datvbhp123@gmail.com>
2024-12-09 16:29:39 +0700

    fix-images-admin

--- app/Views/admin/_tourStay/write.php
+++ app/Views/admin/_tourStay/write.php
@@ -892,7 +892,7 @@
                                         <?php 
                                             for($i = 1; $i <= 5; $i++) : 
                                                 // $img = get_img(${"ufile" . $i}, "/data/product/", "640", "480");
-                                                $img = WRITEPATH ."/public/uploads/product/" . ${"ufile" . $i};
+                                                $img =  base_url('writable/uploads/products' . ${"ufile" . $i});
                                         ?>
                                             <div class="file_input <?=empty(${"ufile" . $i}) ? "" : "applied"?>">
                                                 <input type="file" name='ufile<?=$i?>' id="ufile<?=$i?>" onchange="productImagePreview(this, '<?=$i?>')">
