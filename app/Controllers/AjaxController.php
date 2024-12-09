9d2afc98809319a20639aec16ad3cd3b8ef2a299
diana tgkim <diana001@naver.com>
2024-12-09 17:32:50 +0900

    modified:   app/Controllers/AjaxController.php

--- app/Controllers/AjaxController.php
+++ app/Controllers/AjaxController.php
@@ -679,17 +679,16 @@ class AjaxController extends BaseController {
 
 	public function memberSession()
     {
-        $data = [];
 
         $user_id    = "N_". time();
         setcookie("c_userId", $user_id, time() + 86000 * 365, '/');
 
-			return $this->response
-				->setStatusCode(200)
-				->setJSON([
-					'status'  => 'success',
-					'message' => '비회원 로그인'
-				]);
+        $output = [
+            "message"  => "비회원 로그인"
+        ];
+
+		return $this->response->setJSON($output);
+
     }
 	
 }
