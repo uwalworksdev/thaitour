95c20c4584834325bb11c036f167c2daeafc012c
diana tgkim <diana001@naver.com>
2024-12-10 17:50:01 +0900

    modified:   app/Controllers/CartController.php

--- app/Controllers/CartController.php
+++ app/Controllers/CartController.php
@@ -15,7 +15,9 @@ class CartController extends BaseController
     {
 		$db = \Config\Database::connect(); // 데이터베이스 연결
 		$m_idx  = session("member.idx");
-		$query  = $db->query("SELECT * FROM tbl_order_mst WHERE m_idx = '$m_idx' AND order_status = 'B' " );
+		$sql    = "SELECT * FROM tbl_order_mst WHERE m_idx = '$m_idx' AND order_status = 'B' ";
+		write_log($sql);
+		$query  = $db->query($sql);
 		$result = $query->getResult();
 
         return view("cart/item-list", [
