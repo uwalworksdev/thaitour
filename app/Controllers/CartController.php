06167ea0ccf318cb6d44f0dc172cabeaca68e01d
diana tgkim <diana001@naver.com>
2024-12-10 18:25:43 +0900

    modified:   app/Controllers/CartController.php

--- app/Controllers/CartController.php
+++ app/Controllers/CartController.php
@@ -14,10 +14,12 @@ class CartController extends BaseController
     public function itemList($code_no)
     {
 		$db     = \Config\Database::connect(); // 데이터베이스 연결
-
 		$m_idx  = session("member.idx");
-		$sql    = "SELECT a.*, b.* FROM tbl_order_mst a
-		                           LEFT JOIN tbl_product_mst b ON a.product_idx = b.product_idx WHERE a.m_idx = '$m_idx' AND a.order_status = 'B' ";
+        
+		// 골프
+		$sql    = "SELECT a.*, b.*, c.* FROM tbl_order_mst a
+		                                LEFT JOIN tbl_product_mst b  ON a.product_idx = b.product_idx 
+										LEFT JOIN tbl_order_option C ON a.order_idx   = c.order_idx   WHERE a.m_idx = '$m_idx' AND a.order_status = 'B' ";
 		write_log($sql);
 		$query  = $db->query($sql);
 		$result = $query->getResultArray();
