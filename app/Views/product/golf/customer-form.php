32b17cd401dc89d2fda9bae0a39b7cd71a4a98cf
diana tgkim <diana001@naver.com>
2024-12-10 15:28:19 +0900

    modified:   app/Controllers/Product.php 	modified:   app/Views/product/golf/customer-form.php

--- app/Views/product/golf/customer-form.php
+++ app/Views/product/golf/customer-form.php
@@ -34,7 +34,7 @@
         <input type="hidden" name="product_code_2" value="<?= $product['product_code_2'] ?>">
         <input type="hidden" name="product_code_3" value=".">
         <input type="hidden" name="product_code_4" value="."> 
-        <input type="hidden" name="order_status" id="order_status" value="">
+        <input type="hidden" name="order_status" id="order_status" value="W">
         <input type="hidden" name="people_adult_cnt" value="<?= $people_adult_cnt ?>">
         <input type="hidden" name="order_date" id="order_date" value="<?= $order_date ?>">
         <input type="hidden" name="option_idx" id="option_idx" value="<?= $option_idx ?>">
