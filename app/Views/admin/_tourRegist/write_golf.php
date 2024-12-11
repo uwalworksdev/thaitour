174756792247a68dfb0f7a9c9dcb362684862ad9
diana tgkim <diana001@naver.com>
2024-12-11 17:00:57 +0900

    modified:   app/Views/admin/_tourRegist/write_golf.php

--- app/Views/admin/_tourRegist/write_golf.php
+++ app/Views/admin/_tourRegist/write_golf.php
@@ -1387,6 +1387,12 @@
                     return;
                 }
 
+				if (frm.email.value == "") {
+                    alert("이메일을 입력하셔야 합니다..");
+                    frm.email.focus();
+                    return;
+                }
+
 				if (frm.keyword.value == "") {
                     alert("검색키워드를 입력하셔야 합니다..");
                     frm.keyword.focus();
