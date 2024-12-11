8a0cbf2ced8650649626a44c2c6b5f96e9b9967d
diana tgkim <diana001@naver.com>
2024-12-12 08:28:53 +0900

    modified:   app/Views/product/inc/spa_ticket_restaurant/detail.php

--- app/Views/product/inc/spa_ticket_restaurant/detail.php
+++ app/Views/product/inc/spa_ticket_restaurant/detail.php
@@ -521,7 +521,7 @@
         $('.sel_date').removeClass('active_');
         $(this).addClass('active_');
         let day_ = $(this).data('date');
-		//alert(day_);
+		//alert(day_);  
         spaCharge(day_);
     });
 
