0fa1776bd0d5d180461c42c357afb2e2cf92aab1
diana tgkim <diana001@naver.com>
2024-12-11 18:57:23 +0900

    modified:   app/Controllers/CartController.php 	modified:   app/Views/cart/item-list.php

--- app/Views/cart/item-list.php
+++ app/Views/cart/item-list.php
@@ -266,7 +266,7 @@
                     <div class="main-cart">
                         <div class="checkbox-group-2 form-group">
                             <input type="checkbox" id="group_2">
-                            <label class="font-bold" for="group_2">투어.. :<span class="text-red"> <?=$tour_cnt?></span>
+                            <label class="font-bold" for="group_2">투어.. :<span class="text-red"> <?=$tours_cnt?></span>
                             </label>
                         </div>
                         <table class="table-container">
@@ -282,7 +282,7 @@
                             </thead>
                             <tbody>
 							<?php $currentOrderIdx = null;?>
-							<?php foreach ($tour_result as $item): ?>
+							<?php foreach ($tours_result as $item): ?>
                             <tr>
 					            <?php if ($currentOrderIdx !== $item['order_idx']) : ?>
                                 <td class="custom-td-product-info">
