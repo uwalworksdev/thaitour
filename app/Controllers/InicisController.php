8295f39f4ce0bb150f1fbffc6f7b9179b0862c43
diana tgkim <diana001@naver.com>
2024-12-13 02:11:16 +0900

    new file:   app/Controllers/InicisController.php

---
+++ app/Controllers/InicisController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class InicisController extends BaseController
+{
+    public function request()
+    {
+        // 로직 구현
+		echo "Inicis request";
+    }
+}
+
+?>
