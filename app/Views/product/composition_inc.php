28c0569cb09ca10165f66db1b6df65112f3b8f47
diana tgkim <diana001@naver.com>
2024-12-12 15:44:38 +0900

    modified:   app/Views/product/composition_inc.php

--- app/Views/product/composition_inc.php
+++ app/Views/product/composition_inc.php
@@ -302,6 +302,24 @@
         }
         ?>
 
+// 각각의 input 태그에 대해 data-* 값을 가져오기
+$('input[name="mem_cnt2[]"]').each(function () {
+    // 현재 input 태그
+    var $input = $(this);
+
+    // data- 값들 가져오기
+    var price = $input.data('price'); // data-price 값
+    var idx = $input.data('idx'); // data-idx 값
+    var station = $input.data('s_station'); // data-s_station 값
+    var type = $input.data('type'); // data-type 값
+
+    // 콘솔 출력
+    alert("Price:", price);
+    alert("Idx:", idx);
+    alert("Station:", station);
+    alert("Type:", type);
+});
+
         /* Form submission setup */
         let url = '<?= route_to('api.product.processBooking') ?>';
         let formData = new FormData($('#frm')[0]);
