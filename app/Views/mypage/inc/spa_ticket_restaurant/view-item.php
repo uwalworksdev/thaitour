6a3341d0d33fff9efc3610b57b100c156f963aaa
diana tgkim <diana001@naver.com>
2024-12-12 14:22:15 +0900

    modified:   app/Views/mypage/inc/spa_ticket_restaurant/view-item.php

--- app/Views/mypage/inc/spa_ticket_restaurant/view-item.php
+++ app/Views/mypage/inc/spa_ticket_restaurant/view-item.php
@@ -70,7 +70,8 @@
 
                     <td class="content">
                         <span><?= $row["people_adult_cnt"] + $row["people_kids_cnt"] ?>
-						(성인: <?=number_format($row["people_adult_price"]*$row["people_adult_cnt"])?>원 / 아동: <?=number_format($row["people_kids_price"]*$row["people_kids_cnt"])?>원)
+						(성인: <?=$row["people_adult_cnt"]?>명 <?=number_format($row["people_adult_price"]*$row["people_adult_cnt"])?>원 / 
+						 아동: <?=$row["people_kids_cnt"]?>명 <?=number_format($row["people_kids_price"]*$row["people_kids_cnt"])?>원)
 						</span>
                     </td>
 
