ac5df574cb564aad57e71076e5f0c02275bfdc13
diana tgkim <diana001@naver.com>
2024-12-12 10:03:22 +0900

    modified:   app/Controllers/MyPage.php

--- app/Controllers/MyPage.php
+++ app/Controllers/MyPage.php
@@ -444,7 +444,8 @@ class MyPage extends BaseController
         $_arr_additional_request = explode("|", $additional_request);
         $list__additional_request = rtrim(implode(',', $_arr_additional_request), ',');
 
-        $sql = "select * from tbl_code WHERE parent_code_no='53' AND status = 'Y' and code_no IN ($list__additional_request) order by onum desc, code_idx desc";
+        //$sql = "select * from tbl_code WHERE parent_code_no='53' AND status = 'Y' and code_no IN ($list__additional_request) order by onum desc, code_idx desc";
+        $sql = "select * from tbl_code WHERE parent_code_no='53' AND status = 'Y'  order by onum desc, code_idx desc";
 		write_log($sql);
         $fcodes = $this->db->query($sql)->getResultArray();
 
