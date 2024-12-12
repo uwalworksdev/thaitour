9f1287c5e050bf5e5249f6534430a0556bc188cc
diana tgkim <diana001@naver.com>
2024-12-13 02:49:37 +0900

    modified:   app/Controllers/InicisController.php

--- app/Controllers/InicisController.php
+++ app/Controllers/InicisController.php
@@ -7,7 +7,7 @@ class InicisController extends BaseController
     public function request()
     {
         // 로직 구현
-		echo "Inicis request";
+
 		$data[] = "";
 		return $this->renderView('inicis_request', $data);
     }
