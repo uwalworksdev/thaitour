a9fb8c5e88b3441aec3a0360edb303961c0a842c
diana tgkim <diana001@naver.com>
2024-12-14 07:13:43 +0900

    modified:   app/Views/admin/_tourRegist/write_golf.php

--- app/Views/admin/_tourRegist/write_golf.php
+++ app/Views/admin/_tourRegist/write_golf.php
@@ -837,7 +837,8 @@
 									<span style="color:red;">※ 옵션 삭제 시에 해당 옵션과 연동된 주문, 결제내역에 영향을 미치니 반드시 확인 후에 삭제바랍니다. /
 										마감날짜 예시) [ 2019-10-15||2019-10-17 ] Y-m-d 형식으로 || 를 구분자로 사용해주세요.</span>
 									<div id="mainGolf">
-											<!--table>
+									<?php foreach ($options as $option): ?>
+											<table>
 												<colgroup>
 													<col width="10%">
 													</col>
@@ -918,7 +919,8 @@
 														</td>
 													</tr>
 												</tbody>
-											</table-->
+											</table>
+                                            <?php endforeach; ?>
 									</div>
 								</td>
 								</tr>
