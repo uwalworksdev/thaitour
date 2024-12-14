4bb67931d04e11bd836a91eeaa37e72d4ec9c5ee
diana tgkim <diana001@naver.com>
2024-12-14 23:40:12 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -856,7 +856,8 @@
             const optionPrice  = setOptionArea();
             const optionPrice1 = setGolfOption();
 
-            
+            alert(optionPrice1.total_option_price);
+            alert(optionPrice1.total_option_price_baht);
             let last_price      = vehiclePrice.total_vehicle_price + optionPrice.final_price + optionPrice1.total_option_price;
             let last_price_baht = vehiclePrice.total_vehicle_price_baht + optionPrice.final_price_baht + optionPrice1.total_option_price_baht;
 
