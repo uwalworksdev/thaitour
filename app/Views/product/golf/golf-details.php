024e72255a1a6b077dfe2d22fdae71280d1b9539
diana tgkim <diana001@naver.com>
2024-12-15 00:32:15 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -760,7 +760,7 @@
                     .replace("[price_baht]", number_format(price_baht));
             }).get().join('');
 
-			if(let > 0) $("#option_list_result").html(html2);
+			if(cnt > 0) $("#option_list_result").html(html2);
             return {
                 total_option_price,
                 total_option_price_baht
