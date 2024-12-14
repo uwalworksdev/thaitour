77be51f3dc84a25fdacf8d3ec526f2a52e9d552d
diana tgkim <diana001@naver.com>
2024-12-14 21:15:26 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -1698,7 +1698,7 @@ class Product extends BaseController
         $data['product']['product_price_won'] = $data['product']['product_price'] * $baht_thai;
 
         // 예약가능한 일자 및 금액 데이터 조회
-        $sql_p = "SELECT * FROM tbl_golf_price WHERE product_idx = '$product_idx' AND goods_date >= CURDATE() AND use_yn != 'N' ORDER BY goods_date, hole_cnt ASC LIMIT 0,1 ";
+        $sql_p = "SELECT * FROM tbl_golf_price WHERE product_idx = '$product_idx' AND goods_date >= CURDATE() AND use_yn != 'N' ORDER BY goods_date, goods_name ASC LIMIT 0,1 ";
         $result_p = $this->db->query($sql_p);
         $data['golf_price'] = $result_p->getResultArray();
 
@@ -1773,16 +1773,16 @@ class Product extends BaseController
 
         $options = $this->golfOptionModel->getOptions($product_idx);
 
-        $hole_cnt_arr = array_column($options, 'hole_cnt');
-        $hour_arr = array_column($options, 'hour');
+        $hole_cnt_arr = array_column($options, 'goods_name');
+        //$hour_arr = array_column($options, 'hour');
 
         $data['hole_cnt_arr'] = array_filter(GOLF_HOLES, function ($value) use ($hole_cnt_arr) {
             return in_array($value, $hole_cnt_arr);
         });
 
-        $data['hour_arr'] = array_filter(GOLF_HOURS, function ($value) use ($hour_arr) {
-            return in_array($value, $hour_arr);
-        });
+        //$data['hour_arr'] = array_filter(GOLF_HOURS, function ($value) use ($hour_arr) {
+        //    return in_array($value, $hour_arr);
+        //});
 
         $data['reviewCategories'] = $this->getReviewCategories($product_idx) ?? [];
 
