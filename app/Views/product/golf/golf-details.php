79efa67063577ea530a597f036fc33d05e39abbf
diana tgkim <diana001@naver.com>
2024-12-14 23:04:49 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -310,8 +310,11 @@
                                 </div>
                                 <div class="item_right flex__c">
                                     <span class="pri" rel="<?=$option['goods_price1']?>">￦<?=number_format($option['goods_price1'])?></span>
-                                    <select data-name="<?=$option['goods_name']?>" data-price="<?=$option['goods_price1']?>" data-price_baht="<?=$baht_thai?>" class="option_select select_custom_ active_ cus-width" name="option_cnt[]">
-                                        <option value="0">선택</option>
+                                    <input type="hidden" name="option_idx[]" value="<?= $option['idx'] ?>">
+                                    <select data-name="<?=$option['goods_name']?>" 
+									        data-price="<?=$option['goods_price1']?>" 
+											data-price_baht="<?=$baht_thai?>" class="option_select select_custom_ active_ cus-width" name="option_cnt[]">
+                                        <option>선택</option>
 										<option value="1">1</option>
 										<option value="2">2</option>
 										<option value="3">3</option>
