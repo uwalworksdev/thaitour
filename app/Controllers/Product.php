ce5315608a06041af121a9e328d1c0de3d61e7e0
diana tgkim <diana001@naver.com>
2024-12-15 02:23:19 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -1699,6 +1699,7 @@ class Product extends BaseController
 
         // 예약가능한 일자 및 금액 데이터 조회
         $sql_p    = "SELECT * FROM tbl_golf_price WHERE product_idx = '$product_idx' AND goods_date >= CURDATE() AND use_yn != 'N' ORDER BY goods_date, goods_name ASC LIMIT 0,1 ";
+		write_log($sql_p);
         $result_p = $this->db->query($sql_p);
         $data['golf_price'] = $result_p->getResultArray();
 
