b10e52ebd8e06a725e773d6c666e11121c7d21c7
diana tgkim <diana001@naver.com>
2024-12-14 23:21:17 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -745,6 +745,7 @@
                     .replace("[price_baht]", number_format(price_baht));
             }).get().join('');
             $("#option_list_result").html(html2);
+            $("#option_list_result").show();
 
             return {
                 total_option_price,
@@ -857,8 +858,8 @@
             const optionPrice1 = setGolfOption();
 
             
-            let last_price = vehiclePrice.total_vehicle_price + optionPrice.final_price;
-            let last_price_baht = vehiclePrice.total_vehicle_price_baht + optionPrice.final_price_baht;
+            let last_price      = vehiclePrice.total_vehicle_price + optionPrice.final_price + optionPrice1.total_option_price;
+            let last_price_baht = vehiclePrice.total_vehicle_price_baht + optionPrice.final_price_baht + optionPrice1.total_option_price_baht;
 
             $("#total_price_popup").text(number_format(last_price) + "원");
             $("#total_price").val(last_price);
