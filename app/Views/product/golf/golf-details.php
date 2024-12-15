891b6176732750ac90294fc6529c07da33127f1d
diana tgkim <diana001@naver.com>
2024-12-16 01:59:41 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -1133,8 +1133,8 @@
             return {s_date: new Date(s_date), e_date: new Date(e_date)};
         });
 
-        var sel_Date = "2024-12-21|2024-12-30";
-		//alert(sel_Date);
+        var sel_Date = getAvailableDates(s_date, e_date, deadline_date_arr);
+		//alert(sel_Date); 마감일자 확인
         const arrDate = sel_Date.split("|");
         const arrPrice = arrDate.map(x => '<?=round($product['product_price_won'] / 10000, 1)?>');
 
