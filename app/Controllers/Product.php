f3d9ff6687529b1efbb7ec5a1164bbe5afa6241b
diana tgkim <diana001@naver.com>
2024-12-16 16:09:16 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -1865,6 +1865,7 @@ class Product extends BaseController
 
     private function golfPriceCalculate($option_idx, $hour, $people_adult_cnt, $vehicle_cnt, $vehicle_idx, $option_cnt, $opt_idx, $use_coupon_idx)
     {
+		write_log($option_idx ." - ". $hour ." - ". $people_adult_cnt ." - ". $vehicle_cnt ." - ". $vehicle_idx ." - ". $option_cnt ." - ". $opt_idx ." - ". $use_coupon_idx);
         //$data['option'] = $this->golfPriceModel->find($option_idx);
         $baht_thai = (float)($this->setting['baht_thai'] ?? 0);
 
