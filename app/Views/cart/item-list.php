b44021f132d66bebc14e097216a7d848401e16f2
diana tgkim <diana001@naver.com>
2024-12-17 00:23:13 +0900

    modified:   app/Views/cart/item-list.php

--- app/Views/cart/item-list.php
+++ app/Views/cart/item-list.php
@@ -200,6 +200,184 @@
                         </div>
                     </div>
                 </div>
+                    <div class="main-cart">
+                        <div class="checkbox-group-2 form-group only_web">
+                            <input type="checkbox" id="group_1" checked>
+                            <label class="font-bold" for="group_1">골프 :<span class="text-red"> <?=$golf_cnt?></span>
+                            </label>
+                        </div>
+                        <div class="checkbox-group-2 form-group only_mo">
+                            <input type="checkbox" id="group_1_mo" checked>
+                            <label class="font-bold" for="group_1_mo">골프 :<span class="text-red"> <?=$golf_cnt?></span>
+                            </label>
+                        </div>
+                        <table class="table-container only_web">
+                            <thead>
+                            <tr class="table-header">
+                                <th>
+                                    <span>상품</span>
+                                </th>
+                                <th>금액</th>
+                                <th>옵션금액</th>
+                                <th>결제예정금액</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+							<?php $currentOrderIdx = null;?>
+							<?php foreach ($golf_result as $item): ?>
+                            <tr>
+					            <?php if ($currentOrderIdx !== $item['order_idx']) : ?>
+                                <td class="custom-td-product-info">
+                                    <div class="product-info">
+                                        <img src="/data/product/<?=$item['ufile1']?>" alt="cart_test_img_01">
+                                        <div class="product-details">
+                                            <div class="product-name"><?=$item['product_name']?></div>
+                                            <div class="product-date"><?=$item['order_date']?></div>
+                                            <!--p class="product-desc text-gray"><?=$item['option_name']?></p-->
+                                        </div>
+                                        <div class="form-group-2 cus-checkbox-td">
+                                            <input type="checkbox" id="group_1_item1">
+                                            <label for="group_1_item1"></label>
+                                        </div>
+                                    </div>
+                                <td class="price"><?=number_format($item['order_price']-$item['option_amt'])?> 원</td>
+                                <td class="discount"><?=number_format($item['option_amt'])?> 원</td>
+                                <td class="total"><?=number_format($item['order_price'])?> 원</td>
+							</tr>
+								<?php 
+									$currentOrderIdx = $item['order_idx'];
+								  endif; 
+								?>
+							<tr>
+                                <td><?=$item['option_name']?> / <?=$item['option_cnt']?> EA / <?=number_format($item['option_tot'])?> 원</td> 
+                                <td class="price" colspan="3"></td>
+							</tr>
+		                    <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                        <div class="table-container custom-mo only_mo">
+						    <?php foreach ($result as $item): ?>
+                            <div class="item">
+                                <div class="con-up">
+                                    <div class="picture-con">
+                                        <img src="/uploads/sub/cart_test_img_01_mo.png" alt="">
+                                        <div class="checkbox-group-2 form-group form-table">
+                                            <input type="checkbox" id="group_1_mo_item_1" checked>
+                                            <label class="font-bold" for="group_1_mo_item_1"></label>
+                                        </div>
+                                    </div>
+                                    <div class="text-right-p">
+                                        <h3 class="title-p">
+                                            샹그릴라 호텔 방콕 (차오프라야 강)
+                                        </h3>
+                                        <div class="time-date-p">
+                                            2024.08.10(토)
+                                        </div>
+                                        <p class="des-p">
+                                            디럭스 연박 프로모션 더블(2룸) /조식포함 성인 4 / 아동 2
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="des-space-p">
+                                    <div class="des-item">
+                                        <span class="space-left">금액</span>
+                                        <span>1,467,360 원</span>
+                                    </div>
+                                    <div class="des-item">
+                                        <span class="space-left">할인금액</span>
+                                        <span>0원</span>
+                                    </div>
+                                    <div class="des-item">
+                                        <span class="space-left">결제예정금액</span>
+                                        <span>1,230,000 원</span>
+                                    </div>
+                                </div>
+                            </div>
+		                    <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart-left only_mo">
+                    <div class="main-cart">
+                        <div class="checkbox-group-2 form-group">
+                            <input type="checkbox" id="group_2_mo" checked>
+                            <label class="font-bold" for="group_2_mo">투어 :<span class="text-red"> <?=$tour_cnt?></span>
+                            </label>
+                        </div>
+                        <table class="table-container only_web">
+                            <thead>
+                            <tr class="table-header">
+                                <th>
+                                    <span>상품...</span>
+                                </th>
+                                <th>금액</th>
+                                <th>옵션금액</th>
+                                <th>결제예정금액</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="custom-td-product-info">
+                                    <div class="product-info">
+                                        <img src="/uploads/sub/cart_test_img_03.png" alt="cart_test_img_01">
+                                        <div class="product-details">
+                                            <div class="product-name">샹그릴라 호텔 방콕 (차오프라야 강)</div>
+                                            <div class="product-date">2024.08.10(토)</div>
+                                            <p class="product-desc text-gray">디럭스 연박 프로모션 더블(2룸) /조식포함<br>
+                                                성인 4 / 아동 2</p>
+                                        </div>
+                                        <div class="form-group-2 cus-checkbox-td">
+                                            <input type="checkbox" id="group_2_mo_item1">
+                                            <label for="group_2_mo_item1"></label>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="price">1,467,360 원</td>
+                                <td class="discount">0 원</td>
+                                <td class="total">1,230,000 원</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <div class="table-container custom-mo only_mo">
+                            <div class="item">
+                                <div class="con-up">
+                                    <div class="picture-con">
+                                        <img src="/uploads/sub/cart_test_img_03_mo.png" alt="">
+                                        <div class="checkbox-group-2 form-group form-table">
+                                            <input type="checkbox" id="group_2_mo_item2" checked>
+                                            <label class="font-bold" for="group_2_mo_item2"></label>
+                                        </div>
+                                    </div>
+                                    <div class="text-right-p">
+                                        <h3 class="title-p">
+                                            샹그릴라 호텔 방콕 (차오프라야 강)
+                                        </h3>
+                                        <div class="time-date-p">
+                                            2024.08.10(토)
+                                        </div>
+                                        <p class="des-p">
+                                            디럭스 연박 프로모션 더블(2룸) /조식포함 성인 4 / 아동 2
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="des-space-p">
+                                    <div class="des-item">
+                                        <span class="space-left">금액</span>
+                                        <span>1,467,360 원</span>
+                                    </div>
+                                    <div class="des-item">
+                                        <span class="space-left">할인금액</span>
+                                        <span>0원</span>
+                                    </div>
+                                    <div class="des-item">
+                                        <span class="space-left">결제예정금액</span>
+                                        <span>1,230,000 원</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="cart-right" id="cart-right" style="display:none;">
                     <h3 class="title-cr">선택상품 : 3건</h3>
                     <div class="item-info-cr">
