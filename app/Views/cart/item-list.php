67c70692e8df97dc01180a0e360d33d23ea4cf1c
diana tgkim <diana001@naver.com>
2024-12-17 02:20:41 +0900

    modified:   app/Views/cart/item-list.php

--- app/Views/cart/item-list.php
+++ app/Views/cart/item-list.php
@@ -412,8 +412,10 @@
 		$("#group_golf").on("change", function() {
 			if ($(this).prop("checked")) {
 				alert("골프 체크박스가 체크되었습니다.");
+				$(".chkGolf").prop("checked", isChecked); // 다른 체크박스 상태 변경
 			} else {
 				alert("골프 체크박스가 체크되지 않았습니다.");
+				$(".chkGolf").prop("checked", false);
 			}
 		});
 
