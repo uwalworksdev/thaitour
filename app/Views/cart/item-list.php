d508c44ac817efe6779f4fc829027c7a2d26aaf5
diana tgkim <diana001@naver.com>
2024-12-17 00:54:21 +0900

    modified:   app/Views/cart/item-list.php

--- app/Views/cart/item-list.php
+++ app/Views/cart/item-list.php
@@ -231,7 +231,7 @@
                     <div class="main-cart">
                         <div class="checkbox-group-2 form-group">
                             <input type="checkbox" id="group_2">
-                            <label class="font-bold" for="group_2">투어x :<span class="text-red"> <?=$tours_cnt?></span>
+                            <label class="font-bold" for="group_2">투어 :<span class="text-red"> <?=$tours_cnt?></span>
                             </label>
                         </div>
                         <table class="table-container">
