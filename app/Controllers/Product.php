02fed92a2105afe54622bb9b669b9adb8840d2e3
diana tgkim <diana001@naver.com>
2024-12-16 15:44:07 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -2138,7 +2138,7 @@ class Product extends BaseController
 
 			for($i=0;$i<count($optIdx);$i++)
             {
-				$row        = $this->GolfOptionModel->getByIdx($optIdx[$i]);
+				$row        = $this->golfOptionModel->getByIdx($optIdx[$i]);
                 $option_tot = $row['goods_price1'] * $optCnt[$i] * $this->setting['baht_thai'];
                 $sql_order  = "INSERT INTO tbl_order_option SET 
 														      option_type  = 'golf'	
