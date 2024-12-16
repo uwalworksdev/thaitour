ff3ad48d8f263583535890e7575e7632a1f1d435
diana tgkim <diana001@naver.com>
2024-12-17 00:27:44 +0900

    modified:   app/Views/cart/item-list.php

--- app/Views/cart/item-list.php
+++ app/Views/cart/item-list.php
@@ -287,7 +287,7 @@
                     <div class="main-cart">
                         <div class="checkbox-group-2 form-group">
                             <input type="checkbox" id="group_2">
-                            <label class="font-bold" for="group_2">스파 :<span class="text-red"> <?=$tours_cnt?></span>
+                            <label class="font-bold" for="group_2">스파 :<span class="text-red"> <?=$spa_cnt?></span>
                             </label>
                         </div>
                         <table class="table-container">
