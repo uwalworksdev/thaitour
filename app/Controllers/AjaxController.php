63591aa048762faf6ff08624465f900b81b24711
diana tgkim <diana001@naver.com>
2024-12-17 13:31:41 +0900

    modified:   app/Controllers/AjaxController.php

--- app/Controllers/AjaxController.php
+++ app/Controllers/AjaxController.php
@@ -759,7 +759,7 @@ class AjaxController extends BaseController {
 
 			$sql    = "SELECT SUM(order_price) AS tot_amt, COUNT(order_no) AS tot_cnt FROM tbl_order_mst WHERE order_no IN(". $output .") AND order_no != '' ";
 			write_log($sql);
-			$row    = $db->query($sql)->getRow();
+			//$row    = $db->query($sql)->getRow();
 
             $msg    = "확인";
 			
