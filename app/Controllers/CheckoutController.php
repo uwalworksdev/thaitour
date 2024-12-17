a70e957224b93f2a2cdf19bd8921893ded984cde
diana tgkim <diana001@naver.com>
2024-12-17 22:38:27 +0900

    modified:   app/Controllers/CheckoutController.php

--- app/Controllers/CheckoutController.php
+++ app/Controllers/CheckoutController.php
@@ -4,9 +4,50 @@ namespace App\Controllers;
 
 class CheckoutController extends BaseController
 {
+    private $db;
+    private $productModel;
+
+    public function __construct() {
+        $this->db = db_connect();
+        $this->orderModel = model("orderModel");
+
+    }
+
     public function show()
     {
-        return view('checkout/show');
+        $db         = \Config\Database::connect();
+
+		$array = explode(",", $_POST['dataValue']);
+
+		// 각 요소에 작은따옴표 추가
+		$quotedArray = array_map(function($item) {
+			return "'" . $item . "'";
+		}, $array);
+
+		// 배열을 다시 문자열로 변환
+		$output = implode(',', $quotedArray);
+
+		$sql = "SELECT 
+				tbl_order_mst.order_idx,
+				tbl_order_mst.product_name,
+				tbl_order_mst.order_price,
+				GROUP_CONCAT(CONCAT(tbl_order_option.option_name, ":", tbl_order_option.option_cnt) SEPARATOR "|") as options
+				FROM 
+					tbl_order_mst
+				LEFT JOIN 
+					tbl_order_option 
+				ON 
+					tbl_order_mst.order_idx = tbl_order_option.order_idx
+				WHERE tbl_order_mst.order_no IN(". $output .") AND order_no != '' 
+				GROUP BY 
+					tbl_order_mst.order_no ";
+        write_log($sql); 
+        $result = $db->connect->query($sql);
+        $row    = $result->getRowArray();
+
+        return view("checkout/show", [
+            "row" => $row 
+        ]);
     }
 
     public function confirm()
