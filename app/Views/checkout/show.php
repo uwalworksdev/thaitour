586afeeef32d2848811e80dc9b25d6634c483b30
diana tgkim <diana001@naver.com>
2024-12-17 13:58:17 +0900

    modified:   app/Views/checkout/show.php

--- app/Views/checkout/show.php
+++ app/Views/checkout/show.php
@@ -376,7 +376,7 @@
                             <input type="hidden" value="N" id="guidelines">
                         </div>
 
-                        <button class="btn-order btnOrder" onclick="completeOrder();" type="button">
+                        <button class="btn-order btnOrder" onclick="nicepayStart();" type="button">
                             결제하기
                         </button>
                         <button class="btn-cancel btnCancel" onclick="cancelOrder();" type="button">
