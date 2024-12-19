cc1ea9a625dcb7c11bc88272a1033c6b8c7e2dc0
dainqd <ngodaix5tp@gmail.com>
2024-12-19 13:48:13 +0700

    Update guide.

---
+++ app/Views/admin/_tourGuides/inc/createmap/createmap.php
@@ -0,0 +1,46 @@
+<table cellpadding="0" cellspacing="0" summary="" class="listTable mem_detail"
+       style="margin-top: 50px">
+    <caption>
+    </caption>
+    <colgroup>
+        <col width="10%"/>
+        <col width="90%"/>
+    </colgroup>
+    <tbody>
+    <tr>
+        <th>
+            옵션추가
+            <button style="margin: 0px;" type="button" class="btn_01"
+                    onclick="add_option();">추가
+            </button>
+        </th>
+        <td>
+            <table>
+                <colgroup>
+                    <col width="*%"/>
+                    <col width="10%"/>
+                    <col width="10%"/>
+                    <col width="10%"/>
+                    <col width="10%"/>
+                    <col width="8%"/>
+                    <col width="8%"/>
+                </colgroup>
+                <thead>
+                <tr>
+                    <th style="text-align: center">옵션명</th>
+                    <th style="text-align: center">최초가격</th>
+                    <th style="text-align: center">판매가격</th>
+                    <th style="text-align: center">예약가능여부</th>
+                    <th style="text-align: center">총인원</th>
+                    <th style="text-align: center">우선순위</th>
+                    <th style="text-align: center">관리</th>
+                </tr>
+                </thead>
+                <tbody id="list_option">
+
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
