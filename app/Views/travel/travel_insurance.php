d8c735d52852d92c7b1f45bd4e699eb77d305390
thanhthoai <thanhthoai1001@gmail.com>
2024-12-19 10:26:14 +0700

    fix iframe header

---
+++ app/Views/travel/travel_insurance.php
@@ -0,0 +1,5 @@
+<?php $this->section('content'); ?>
+
+<div id="iframeContainer">
+    <iframe id="myIframe" src="https://tourlab.toursafe.co.kr/" frameborder="0" width="100%" height="100%"></iframe>
+</div>
