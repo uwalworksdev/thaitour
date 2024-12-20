33052f7e5b770eeac7c2caab706737f126b0252a
diana tgkim <diana001@naver.com>
2024-12-20 16:34:14 +0900

    modified:   app/Controllers/InicisController.php

--- app/Controllers/InicisController.php
+++ app/Controllers/InicisController.php
@@ -39,26 +39,9 @@ class InicisController extends BaseController
 	public function inicisResult()
 	{
 
-		if (file_exists(APPPATH . 'Libraries/INIStdPayUtil.php')) {
-			require_once(APPPATH . 'Libraries/INIStdPayUtil.php');
-		} else {
-			echo "File not found.";
-		}
-
-		if (file_exists(APPPATH . 'Libraries/HttpClient.php')) {
-			require_once(APPPATH . 'Libraries/HttpClient.php');
-		} else {
-			echo "File not found.";
-		}
-
-		if (file_exists(APPPATH . 'Libraries/properties.php')) {
-			require_once(APPPATH . 'Libraries/properties.php');
-		} else {
-			echo "File not found.";
-		}
-		//require_once(APPPATH . 'Libraries/INIStdPayUtil.php');
-		//require_once(APPPATH . 'Libraries/HttpClient.php');
-		//require_once(APPPATH . 'Libraries/properties.php');
+		require_once(APPPATH . 'Libraries/INIStdPayUtil.php');
+		require_once(APPPATH . 'Libraries/HttpClient.php');
+		require_once(APPPATH . 'Libraries/properties.php');
 
         $util = new INIStdPayUtil();
         $prop = new properties();
