0d54db2d2d507d109910bc8816395692b839e2e3
diana tgkim <diana001@naver.com>
2024-12-22 22:45:35 +0900

    modified:   app/Controllers/CheckoutController.php

--- app/Controllers/CheckoutController.php
+++ app/Controllers/CheckoutController.php
@@ -75,7 +75,7 @@ class CheckoutController extends BaseController
              }
         }
           
-        if(count($array) > 0) {
+        if(count($array) > 1) {
 		   $product_name .= " 외 ". (count($array)-1) ."개";
 		}
 		
