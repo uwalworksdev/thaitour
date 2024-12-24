b39e9502ee503a403d25f2190dbd615130b02418
diana tgkim <diana001@naver.com>
2024-12-24 16:16:20 +0900

    modified:   app/Views/checkout/confirm.php

--- app/Views/checkout/confirm.php
+++ app/Views/checkout/confirm.php
@@ -695,7 +695,8 @@ function payment_acnt()
 		 var used_point   = $("#used_point").val()*1;
 
 		 if(coupon_pe > 0) {
-			var used_coupon_money = payment_tot * coupon_pe / 100;
+			var used_coupon_money = parseInt(payment_tot * coupon_pe / 100);
+			alert(used_coupon_money);
 		 } else {  
 			var used_coupon_money = coupon_price;
 		 }
