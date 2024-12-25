71fbaac80038347319c9ae4c80bb5ab48abf796a
diana tgkim <diana001@naver.com>
2024-12-25 12:25:16 +0900

    modified:   app/Views/cart/item-list.php

--- app/Views/cart/item-list.php
+++ app/Views/cart/item-list.php
@@ -20,7 +20,7 @@
                             <input type="checkbox" id="check_all">
                             <label class="text-gray" for="check_all">전체선택</label>
                         </div>
-						<button type="button" id="deleteBtn">삭제</button>
+						<button type="button" id="deleteBtn" class="btn_delete">삭제</button>
                     </div>
 
 					<?php if($golf_cnt > 0) { ?>
