ef114a553eb51837c1e0320e79a342e006cdc914
diana tgkim <diana001@naver.com>
2024-12-26 15:09:30 +0900

    new file:   app/Config/Routes.php.tmp 	modified:   app/Controllers/InicisController.php 	new file:   app/Views/nicepay/nicepay_web.php.tmp

--- app/Controllers/InicisController.php
+++ app/Controllers/InicisController.php
@@ -262,6 +262,81 @@ class InicisController extends BaseController
 
     }
 
+	public function inicisResult_m()
+	{
+	   $session = session();
+
+       $db  = \Config\Database::connect();
+
+       require_once(APPPATH . 'Libraries/properties.php');
+
+	   $prop = new properties();
+	 
+	   $P_STATUS    = $_REQUEST["P_STATUS"];
+	   $P_RMESG1    = $_REQUEST["P_RMESG1"];
+	   $P_TID       = $_REQUEST["P_TID"];
+	   $P_REQ_URL   = $_REQUEST["P_REQ_URL"];
+	   $P_NOTI      = $_REQUEST["P_NOTI"];
+	   $P_AMT       = $_REQUEST["P_AMT"];
+	  
+	   if ($_REQUEST["P_STATUS"] === "00") {             // 인증이 P_STATUS===00 일 경우만 승인 요청
+	 
+			$id_merchant = substr($P_TID,'10','10');     // P_TID 내 MID 구분
+			$data = array(
+			
+			 'P_MID' => $id_merchant,         // P_MID
+			 'P_TID' => $P_TID                // P_TID
+
+			);
+
+			//##########################################################################
+			// 승인요청 API url (authUrl) 리스트 는 properties 에 세팅하여 사용합니다.
+			// idc_name 으로 수신 받은 센터 네임을 properties 에서 include 하여 승인요청하시면 됩니다.
+			//##########################################################################
+			$idc_name 	= $_REQUEST["idc_name"];
+			$P_REQ_URL  = $prop->getAuthUrl($idc_name); 
+	 
+			if (strcmp($P_REQ_URL, $_REQUEST["P_REQ_URL"]) == 0) {
+			
+				// curl 통신 시작 
+			
+				$ch = curl_init();                                                //curl 초기화
+				curl_setopt($ch, CURLOPT_URL, $_REQUEST["P_REQ_URL"]);            //URL 지정하기
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                   //요청 결과를 문자열로 반환 
+				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);                     //connection timeout 10초 
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);                      //원격 서버의 인증서가 유효한지 검사 안함
+				curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));    //POST 로 $data 를 보냄
+				curl_setopt($ch, CURLOPT_POST, 1);                                //true시 post 전송 
+		
+		
+				$response = curl_exec($ch);
+				curl_close($ch);
+
+				parse_str($response, $out);
+				print_r($out);
+			}
+		}
+/*
+        $data = [];
+
+		$sql = " SELECT * from tbl_member WHERE m_idx = '" . $m_idx . "'";
+		$row = $db->query($sql)->getRowArray();
+
+        $data['id']    = $row['user_id'];
+        $data['idx']   = $row['m_idx'];
+        $data["mIdx"]  = $row['m_idx'];
+        $data['name']  = encryptField($row['user_name'], "decode");
+        $data['email'] = encryptField($row['user_email'], "decode");
+        $data['level'] = $row['user_level'];
+        $data['phone'] = encryptField($row['user_mobile'], "decode");
+
+        session()->set("member", $data);
+
+	    $data['ResultMsg'] = $resultMap['resultMsg'];
+
+	    return $this->renderView('inicis_result', $data);
+*/
+    }
 
 }
 
