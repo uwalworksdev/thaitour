6d0a59e9429b57f5a1f667db2c9c5b294895a7c9
diana tgkim <diana001@naver.com>
2024-12-26 12:42:19 +0900

    modified:   app/Views/checkout/confirm.php

--- app/Views/checkout/confirm.php
+++ app/Views/checkout/confirm.php
@@ -1,10 +1,7 @@
 <?php $this->extend('inc/layout_index'); ?>
 <?php $this->section('content'); ?>
 <?php $setting = homeSetInfo();?>
-<?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-?>
+
 <link rel="stylesheet" href="/css/contents/confirm.css">
 
 <div class="customer-form-page">
