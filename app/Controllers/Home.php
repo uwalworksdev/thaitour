44e51162093cb6d9282d8deae48865735372b60c
diana tgkim <diana001@naver.com>
2024-12-29 01:06:58 +0900

    modified:   app/Controllers/Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -60,15 +60,11 @@ class Home extends BaseController
         }, $best_reviews);
 
 
-		// 태국에서 즐기는 골프의 특별함
-		$list5 = $MainDisp->List("2905");
-
         $data = [
             'codes'        => $codes,
             'best_reviews' => $best_reviews,
             'codeBanners'  => $codeBanners,
             'popups'       => $this->cmsModel->getPaging(['r_code' => 'popup', 'sch_status' => 'Y'], 5, 1)['items'],
-            'list5'        => $list5,
         ];
 
         $data['magazines'] = $magazines;
