3783f470b487a82481273e251bd63c00e0d1a41d
diana tgkim <diana001@naver.com>
2025-01-05 01:47:41 +0900

    modified:   app/Views/product/tour/confirm-info.php

--- app/Views/product/tour/confirm-info.php
+++ app/Views/product/tour/confirm-info.php
@@ -524,6 +524,51 @@
                 return number.toLocaleString('ko-KR');
             }
 
+            function handlePayment(status) {
+
+				$("#order_status").val(status);
+
+				if(status == "B") {
+						if ($("#order_user_name").val() === "") {
+							alert("한국이름을 입력해주세요.");
+							$("#order_user_name").focus();
+							return false;
+						}
+						if ($("#order_user_first_name_en").val() === "") {
+							alert("영문 이름(First Name)을 입력해주세요.");
+							$("#order_user_first_name_en").focus();
+							return false;
+						}
+
+						if ($("#order_user_last_name_en").val() === "") {
+							alert("영문 성(Last Name)을 입력해주세요.");
+							$("#order_user_last_name_en").focus();
+							return false;
+						}
+
+						if ($("#email_1").val() === "" || $("#email_2").val() === "") {
+							alert("이메일 주소를 입력해주세요.");
+							$("#email_1").focus();
+							return false;
+						}
+
+						if ($("input[name='radio_phone']:checked").val() === "kor") {
+							if ($("#phone_1").val() === "" || $("#phone_2").val() === "" || $("#phone_3").val() === "") {
+								alert("한국번호를 입력해주세요.");
+								return false;
+							}
+						} else if ($("input[name='radio_phone']:checked").val() === "thai") {
+							if ($("#phone_thai").val() === "") {
+								alert("태국번호를 입력해주세요.");
+								return false;
+							}
+						}
+                }
+
+			    $('#order_frm').attr('action', '/product-golf/customer-payment-ok');
+                $("#order_frm").submit();
+            }
+			
             function handleSubmit(status) {
 
 				$("#order_status").val(status);
