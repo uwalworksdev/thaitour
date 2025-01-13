82a3a96c3c1bab5742e34e0d5fb0f7d7635dd34e
diana tgkim <diana001@naver.com>
2025-01-13 11:57:20 +0900

    modified:   app/Views/admin/_member/write.php

--- app/Views/admin/_member/write.php
+++ app/Views/admin/_member/write.php
@@ -104,7 +104,7 @@
                                 </select>
 
                                 &nbsp;&nbsp;&nbsp;
-                                <input type="radio" id="imfor_r03" name="user_email_yn" value="Y" <?php if($user_email_yn == "Y") echo "checked";?> > <label for="imfor_r03">수신동의</label>
+                                <input type="radio" id="imfor_r03" name="user_email_yn" value="Y" <?= $user_email_yn == "Y" ? 'checked' : '' ?> > <label for="imfor_r03">수신동의</label>
                                 &nbsp;&nbsp;&nbsp;
                                 <input type="radio" id="imfor_r04" class="mar_l" name="user_email_yn" value="N" <?= $user_email_yn == "N" ? 'checked' : '' ?>><label for="imfor_r04">수신동의 안함</label>
                             </td>
