92ffcacb87e95f1824c8d3fd02ed6fc9533efc2a
diana tgkim <diana001@naver.com>
2025-01-14 12:50:27 +0900

    modified:   app/Views/admin/inc/header.php

--- app/Views/admin/inc/header.php
+++ app/Views/admin/inc/header.php
@@ -193,7 +193,7 @@ $code = isset($_GET['code']) ? $_GET['code'] : null;
             <div class="top_box">
                 <!-- <a href="" class="logo"><img src="<?php //=_IT_LOGOS_ADM ?>" alt="로고"></a> -->
                 <a href="/" class="txt_admin" target="_blank"></a>
-                <a href="/" class="logo">
+                <a href="/AdmMaster/main" class="logo">
                     <!--                    <img src="/uploads/setting/--><?php //= $setting['logos'] ?><!--" alt="">-->
                     <img src="/images/sub/logo_w.png" alt="" style="height: 30px;">
                 </a>
