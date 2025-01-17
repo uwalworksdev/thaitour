49210b86d96152d886102ae00cff1cf73b725564
diana tgkim <diana001@naver.com>
2025-01-17 20:54:51 +0900

    modified:   app/Filters/ExcludePreviousUrl.php

--- app/Filters/ExcludePreviousUrl.php
+++ app/Filters/ExcludePreviousUrl.php
@@ -11,19 +11,17 @@ class ExcludePreviousUrl implements FilterInterface
     public function before(RequestInterface $request, $arguments = null)
     {
         $excludedUrls = [
-				'/^\/member\/login$/',
-				'/^\/member\/join_choice$/',
-				'/^\/member\/join_agree$/',        ];
+            '/member/login',
+			'/member/join_choice',
+            '/member/join_agree', 
+        ];
 
         $currentUrl = current_url(true)->getPath();
 
         // 현재 URL이 제외 목록에 있는 경우 _ci_previous_url 설정 제거
-		foreach ($excludedPatterns as $pattern) {
-			if (preg_match($pattern, $currentUrl)) {
-				session()->remove('_ci_previous_url');
-				break;
-			}
-		}    
+        if (in_array($currentUrl, $excludedUrls)) {
+            session()->remove('_ci_previous_url');
+        }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
