1d1bf9e5366bb0f70887bc8fc109005e26ea2aae
diana tgkim <diana001@naver.com>
2025-01-20 16:31:36 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -473,7 +473,7 @@
 		       <div class="item-select">
                     <span class="label">승용차</span>
                     <input type="hidden" name="vehicle_idx[]" value="2">
-                    <select data-name="승용차" data-price="6361500" data-price_baht="0" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
+                    <select data-name="승용차" data-price="<?=$product['vehicle_price1']?>" data-price_baht="<?=$product['vehicle_price1_baht']?>" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
                         <option value="">선택해주세요.</option>
 						<option value="1">1대</option>
 						<option value="2">2대</option>
@@ -485,7 +485,7 @@
             	<div class="item-select">
                     <span class="label">밴 (승합차) </span>
                     <input type="hidden" name="vehicle_idx[]" value="5">
-                    <select data-name="밴 (승합차) " data-price="11874800" data-price_baht="0" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
+                    <select data-name="밴 (승합차) " data-price="<?=$product['vehicle_price2']?>" data-price_baht="<?=$product['vehicle_price2_baht']?>" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
                         <option value="">선택해주세요.</option>
 								<option value="1">1대</option>
 								<option value="2">2대</option>
@@ -499,7 +499,7 @@
             	<div class="item-select">
                     <span class="label">SUV</span>
                     <input type="hidden" name="vehicle_idx[]" value="3">
-                    <select data-name="SUV" data-price="4241000" data-price_baht="0" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
+                    <select data-name="SUV" data-price="<?=$product['vehicle_price3']?>" data-price_baht="<?=$product['vehicle_price3_baht']?>" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
                         <option value="">선택해주세요.</option>
 						<option value="1">1대</option>
 						<option value="2">2대</option>
