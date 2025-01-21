7518953418ee2b45f37add75a5303b3805feface
diana tgkim <diana001@naver.com>
2025-01-22 02:33:48 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -490,7 +490,7 @@ $(document).ready(function() {
                 </div>
             	<div class="item-select">
                     <span class="label">밴 (승합차) </span>
-                    <input type="hidden" name="vehicle_idx[]" value="5">
+                    <input type="hidden" name="vehicle_idx[]" value="<?=$golf_price['idx']?>">
                     <select data-name="밴 (승합차) " data-price="<?=$vehicle_price2?>" data-price_baht="<?=$vehicle_price2_baht?>" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
                         <option value="">선택해주세요.</option>
 								<option value="1">1대</option>
@@ -504,7 +504,7 @@ $(document).ready(function() {
                 </div>
             	<div class="item-select">
                     <span class="label">SUV</span>
-                    <input type="hidden" name="vehicle_idx[]" value="3">
+                    <input type="hidden" name="vehicle_idx[]" value="<?=$golf_price['idx']?>">
                     <select data-name="SUV" data-price="<?=$vehicle_price3?>" data-price_baht="<?=$vehicle_price3_baht?>" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
                         <option value="">선택해주세요.</option>
 						<option value="1">1대</option>
@@ -517,7 +517,7 @@ $(document).ready(function() {
 
 			   <div class="item-select">
                     <span class="label">카트</span>
-                    <input type="hidden" name="vehicle_idx[]" value="2">
+                    <input type="hidden" name="vehicle_idx[]" value="<?=$golf_price['idx']?>">
                     <select data-name="카트" data-price="<?=$cart_price?>" data-price_baht="<?=$cart_price_baht?>" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
                         <option value="">선택해주세요.</option>
 						<option value="1">1대</option>
@@ -529,7 +529,7 @@ $(document).ready(function() {
                 </div>
             	<div class="item-select">
                     <span class="label">캐디피</span>
-                    <input type="hidden" name="vehicle_idx[]" value="5">
+                    <input type="hidden" name="vehicle_idx[]" value="<?=$golf_price['idx']?>">
                     <select data-name="캐디피" data-price="<?=$caddie_fee?>" data-price_baht="<?=$caddie_fee_baht?>" class="vehicle_select select_custom_ active_ cus-width" name="vehicle_cnt[]">
                         <option value="">선택해주세요.</option>
 								<option value="1">1명</option>
