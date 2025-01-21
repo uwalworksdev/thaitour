6353d8446e33c9a417d1da41ea428d6c70ae45a2
diana tgkim <diana001@naver.com>
2025-01-22 03:27:08 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -1983,7 +1983,7 @@ class Product extends BaseController
                 //$info = $this->golfVehicleModel->getCodeByIdx($vehicle_idx[$key]);
 				
 				$sql    = "SELECT * FROM tbl_golf_option WHERE idx = '$option_idx' ";
-				$result = $db->query($sql)->getResultArray();
+				$result = $this->$db->query($sql)->getResultArray();
 				foreach($result as $info)
 				{ 
 						$info['cnt'] = $value;
