7674ecf7c85c809e98000f93adc0c280e19e230e
diana tgkim <diana001@naver.com>
2025-01-22 11:52:13 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -1195,8 +1195,6 @@ $(document).ready(function() {
             $(this).addClass('active');
  			
 			var goods_name = $(this).data('tab') + '홀';
-			alert(goods_name);
-			alert($('input[name="product_idx"]').val());
  			
 			$.ajax({
 				url: "/ajax/get_golf_option",
