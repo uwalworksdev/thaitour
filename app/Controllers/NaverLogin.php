a1aa0b636279eb27c8bde086350ba0eaedfa2669
diana tgkim <diana001@naver.com>
2025-01-26 04:02:46 +0900

    modified:   app/Controllers/NaverLogin.php

--- app/Controllers/NaverLogin.php
+++ app/Controllers/NaverLogin.php
@@ -14,6 +14,39 @@ class NaverLogin extends BaseController
 
     public function callback()
     {
+/*		
+        try {
+            $code  = $this->request->getGet('code');
+            $state = $this->request->getGet('state');
+
+			if (!$code || !$state) {
+                throw new \Exception('코드 또는 상태 값이 없습니다.');
+            }
+
+            $naverOAuth = new \App\Libraries\NaverOAuth();
+            $tokenData = $naverOAuth->getAccessToken($code, $state);
+
+            if (!isset($tokenData['access_token'])) {
+                throw new \Exception('Access token을 가져올 수 없습니다.');
+				write_log('error', 'Access token을 가져올 수 없습니다.');
+            }
+
+            $userInfo = $naverOAuth->getUserInfo($tokenData['access_token']);
+            if (!isset($userInfo['response'])) {
+                throw new \Exception('사용자 정보를 가져올 수 없습니다.');
+				write_log('error', '사용자 정보를 가져올 수 없습니다.');
+            }
+
+            session()->set('user', $userInfo['response']);
+            return redirect()->to('/dashboard');
+        } catch (\Exception $e) {
+			log_message('debug', 'OAuth 요청 state: ' . session()->get('naver_oauth_state'));
+			log_message('debug', '콜백 state: ' . $this->request->getGet('state'));
+			log_message('debug', 'Access Token 데이터: ' . json_encode($tokenData));			
+            log_message('error', '네이버 로그인 콜백 오류: ' . $e->getMessage());
+            return redirect()->to('/login')->with('error', '로그인 중 문제가 발생했습니다.');
+        }
+*/		
 		  // 네이버 로그인 콜백 예제
 		  $client_id     = "thHkJbn94PdAfE38YW5r";
 		  $client_secret = "Y5V6L6ryPj";
@@ -35,7 +68,7 @@ class NaverLogin extends BaseController
 			echo $response;
 		  } else {
 			echo "Error 내용:".$response;
-		  }
+		  }		
     }
 
 
