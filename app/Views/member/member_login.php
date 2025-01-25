cafcb6c46c763cc11bf5591dd6b6619eecf148de
diana tgkim <diana001@naver.com>
2025-01-25 13:26:12 +0900

    modified:   app/Views/member/member_login.php

--- app/Views/member/member_login.php
+++ app/Views/member/member_login.php
@@ -101,7 +101,7 @@
                 $_url .= "&state=OK";
 				
                 // 네이버 로그인 접근토큰 요청 예제
-                $client_id   = "thHkJbn94PdAfE38YW5r";
+                $client_id   = env('NAVER_CLIENT_ID');
 				echo $client_id;
                 //$redirectURI = "https://thetourlab.com/member/member_login";
                 $redirectURI = $previousUrl;
