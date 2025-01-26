9b3ab6effc5c1fbae3f435c9879cae512052be11
diana tgkim <diana001@naver.com>
2025-01-26 15:00:42 +0900

    modified:   public/room_upd.php

--- public/room_upd.php
+++ public/room_upd.php
@@ -6,4 +6,22 @@
 echo "<pre>";
 print_r($_POST);
 echo "</pre>";
+
+// room_types 배열 순회
+if (isset($_POST['room_types']) && is_array($_POST['room_types'])) {
+    $roomTypes = $_POST['room_types'];
+
+    for ($i = 0; $i < count($roomTypes); $i++) {
+        // room_types의 각 타입 정보 출력
+        echo "Room Type " . ($i + 1) . ": " . $roomTypes[$i]['type_name'] . "<br>";
+
+        // 해당 room_type의 rooms 배열 순회
+        $rooms = $roomTypes[$i]['rooms'];
+        for ($j = 0; $j < count($rooms); $j++) {
+            echo "&nbsp;&nbsp;Room " . ($j + 1) . ": " . $rooms[$j] . "<br>";
+        }
+        echo "<br>";
+    }
+}
+
 ?>  
