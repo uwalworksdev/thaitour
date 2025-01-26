301d7b5b939293d57af164935575e94e9f9cfb3d
diana tgkim <diana001@naver.com>
2025-01-26 22:48:57 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -56,8 +56,8 @@ use App\Controllers\Admin\AdminHotelController;
 
         .popup_ {
             position: fixed;
-            top: 20%;
-            left: 20%;
+            top: 0;
+            left: 0;
             width: 100%;
             height: 100%;
             z-index: 9999;
