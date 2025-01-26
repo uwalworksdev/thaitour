9e9c2cfaf0f0958615668957309b0ea6d6289ea5
diana tgkim <diana001@naver.com>
2025-01-26 22:39:42 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -61,8 +61,7 @@ use App\Controllers\Admin\AdminHotelController;
             width: 100%;
             height: 100%;
             z-index: 9999;
-            /*background-color: rgba(0, 0, 0, 0.2);*/
-			background: #555151;
+            background-color: rgba(0, 0, 0, 0.2); 
             display: none;
             align-items: center;
             justify-content: center;
