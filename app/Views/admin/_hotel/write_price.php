8a6be8fae8e0320a1c68e90dcfe8b2db36c2e767
diana tgkim <diana001@naver.com>
2025-01-26 18:21:09 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -159,7 +159,7 @@ $links = "list";
 									
                                     <?php foreach ($roomresult as $row) : ?>
 										<div class="item_" style="margin-bottom: 10px;">
-											<input readonly="" type="text" value="123" style="width:50%">
+											<input readonly="" type="text" value="<?=$row['roomName']?>" style="width:50%">
 											<button class="btn_del" onclick="removeRoomSelect(this, 80)" type="button" style="width: 50px; background-color: #4f728a; color : #fff;">삭제</button>
 											<button class="btn_set" onclick="updateRoomSelect(this, 80)" type="button" style="width: 50px ; background-color: #d03a3e; color : #fff;">수정</button>
 										</div>
