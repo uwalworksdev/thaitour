47d62c265a5154f4ab999f155809fd4d0764f324
diana tgkim <diana001@naver.com>
2025-01-26 11:55:38 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -136,6 +136,63 @@ $links = "list";
                                 </tbody>
                             </table>
 
+  <table cellpadding="0" cellspacing="0" summary="" class="listTable mem_detail" style="margin-top:50px;">
+                                <caption>
+                                </caption>
+                                <colgroup>
+                                    <col width="10%">
+                                    <col width="40%">
+                                    <col width="10%">
+                                    <col width="40%">
+                                </colgroup>
+                                <tbody>
+                                <tr>
+                                    <td colspan="4">
+                                        룸정보
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <th>룸등록</th>
+                                    <td colspan="3">
+                                        <button type="button" class="btn_select_room_list" onclick="showOrHide();">
+                                            룸추가
+                                        </button>
+                                        <div class="room_list_render_" id="room_list_render_"><div class="item_">
+                            <input readonly="" type="text" value="123">
+                            <!--button class="update_" onclick="priceRoomProcess()"                    type="button">가격관리</button-->
+                            <button class="delete_" onclick="removeRoomSelect(this, 80)" type="button">삭제</button>
+                            <button class="update_" onclick="updateRoomSelect(this, 80)" type="button">수정</button>
+                        </div>
+
+                        <div class="item_">
+                            <input readonly="" type="text" value="Test123">
+                            <!--button class="update_" onclick="priceRoomProcess()"                    type="button">가격관리</button-->
+                            <button class="delete_" onclick="removeRoomSelect(this, 87)" type="button">삭제</button>
+                            <button class="update_" onclick="updateRoomSelect(this, 87)" type="button">수정</button>
+                        </div>
+
+
+                        <div class="item_">
+                            <input readonly="" type="text" value="테스트 상품 phong-룸-1">
+                            <!--button class="update_" onclick="priceRoomProcess()"                    type="button">가격관리</button-->
+                            <button class="delete_" onclick="removeRoomSelect(this, 89)" type="button">삭제</button>
+                            <button class="update_" onclick="updateRoomSelect(this, 89)" type="button">수정</button>
+                        </div>
+
+                        <div class="item_">
+                            <input readonly="" type="text" value="222222222222">
+                            <!--button class="update_" onclick="priceRoomProcess()"                    type="button">가격관리</button-->
+                            <button class="delete_" onclick="removeRoomSelect(this, 91)" type="button">삭제</button>
+                            <button class="update_" onclick="updateRoomSelect(this, 91)" type="button">수정</button>
+                        </div>
+
+                        </div>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+							
                             <!--table cellpadding="0" cellspacing="0" summary="" class="listTable mem_detail"
                                    style="margin-top:50px;">
                                 <caption>
