e9edfa4679dd458248009c48538fa62cba0babfa
diana tgkim <diana001@naver.com>
2025-01-28 02:45:34 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -328,9 +328,6 @@ $links = "list";
                                 <?php $roomIdx = 0; ?>
                                 <?php $comIdx  = ""; ?>
                                 <?php foreach ($roomresult as $row) : ?>
-								<?php $roomIdx++; ?>
-								<input type="text" name="product_idx[<?=$roomIdx?>]" value="<?=$product_idx?>" /> 
-								<input type="text" name="g_idx[]" id="g_idx" value="<?=$row['roomType_idx']?>" /> 
                                 <table cellpadding="0" cellspacing="0" summary="" class="listTable mem_detail" style="margin-top:50px;">
                                     <caption>
                                     </caption>
@@ -354,6 +351,10 @@ $links = "list";
 											
                                             <div id="table_child_<?=$roomIdx?>">
 											    <?php if($row['room_name']) { ?>
+												<?php $roomIdx++; ?>
+												<input type="text" name="product_idx[<?=$roomIdx?>]" value="<?=$product_idx?>" /> 
+												<input type="text" name="g_idx[]" id="g_idx" value="<?=$row['roomType_idx']?>" /> 
+												
                                                 <table>
                                                     <tbody>
                                                         <tr>
