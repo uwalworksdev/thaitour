9763b42cb0a943daec92d18b49a5de9aed6a0580
diana tgkim <diana001@naver.com>
2025-01-27 11:48:48 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -452,10 +452,8 @@ $links = "list";
                         <ul>
                             <li class="left"></li>
                             <li class="right_sub">
-                                <a href="/AdmMaster/_hotel/list" class="btn btn-default"><span
-                                            class="glyphicon glyphicon-th-list"></span><span class="txt">리스트</span></a>
-                                <a href="javascript:send_it_price()" class="btn btn-default"><span
-                                            class="glyphicon glyphicon-cog"></span><span class="txt">등록</span></a>
+                                <a href="/AdmMaster/_hotel/list" class="btn btn-default"><span class="glyphicon glyphicon-th-list"></span><span class="txt">리스트</span></a>
+                                <!--a href="javascript:send_it_price()" class="btn btn-default"><span class="glyphicon glyphicon-cog"></span><span class="txt">등록</span></a-->
                             </li>
                         </ul>
                     </div>
