5081c24d59e6e865fc042124e53d3acf7ed0107a
diana tgkim <diana001@naver.com>
2025-01-27 10:52:14 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -432,7 +432,6 @@ $links = "list";
                                                         </tr>
 														</table>
 														
-														<table id="option_chile_<?=$roomIdx?>">
                                                         <tr>
                                                             <td>
                                                                 <p style="margin-bottom: 3px;">옵션 내용을 추가 합니다. (html 태그 사용가능)</p>
@@ -440,7 +439,6 @@ $links = "list";
                                                                 <button type="button" style="width: 31px; height : 31px" value="<?=$roomIdx?>" class="addOptionBtn">+</button>
                                                             </td>
                                                         </tr>
-														</table>
 														
                                                     </tbody>
                                                 </table>
