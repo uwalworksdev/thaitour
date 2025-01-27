dcba6ac1822a0c264f22f107637e1498369846fe
diana tgkim <diana001@naver.com>
2025-01-28 03:16:04 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -304,7 +304,7 @@ $links = "list";
                                     <td colspan="4">
                                         룸정보
 										<button type="button" class="btn_select_room_list" onclick="showOrHide();" style="width : 90px ;background-color : #4f728a; color : #fff">룸타입 추가</button>
-										<button type="button" class="btn_select_room_list" onclick="showOrHide();" style="width : 90px ;background-color : #4f728a; color : #fff">전체저장</button>
+										<button type="button" class="btn_select_room_list" onclick="allUpdate();"  style="width : 90px ;background-color : #4f728a; color : #fff">전체저장</button>
                                     </td>
                                 </tr>
 
