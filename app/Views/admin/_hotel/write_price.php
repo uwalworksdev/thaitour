bd3478cb820c3f98a694f24608952e390ca42c63
diana tgkim <diana001@naver.com>
2025-01-28 16:59:22 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -353,12 +353,12 @@ $links = "list";
 											
                                             <div id="table_child_<?=$roomIdx?>">
 											    <?php //if($row['room_name']) { ?>
-												<input type="text" name="product_idx[<?=$roomIdx?>]" value="<?=$product_idx?>" /> 
-												<input type="text" name="g_idx[<?=$roomIdx?>]" id="g_idx" value="<?=$row['roomType_idx']?>" /> 
 												
                                                 <table>
                                                     <tbody>
                                                         <tr>
+															<input type="text" name="product_idx[<?=$roomIdx?>]"  value="<?=$product_idx?>" /> 
+															<input type="text" name="g_idx[<?=$roomIdx?>]"        value="<?=$row['roomType_idx']?>" /> 
                                                             <td style="background-color: #eee;">
                                                                 <span>룸 명칭</span>
                                                                 <input style="width: 30%;" type="text" name="room_name[<?=$roomIdx?>]">
