d15e74afedd45ee4ee5d69a0b3ffefc5ab3a95f1
diana tgkim <diana001@naver.com>
2025-01-28 16:36:36 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -715,6 +715,7 @@ $links = "list";
                 // 새로운 테이블 HTML 생성
 				var roomIdx  = $(this).val();
                 var prod_idx = $(this).data('prod');
+				alert(prod_idx);
                 var g_idx    = $(this).data('roomtype');
 				room_Idx++;
                 const newTable = `
