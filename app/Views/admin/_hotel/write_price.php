3313a46bd039ec49a71cded62591381ed2760eba
diana tgkim <diana001@naver.com>
2025-01-30 04:41:37 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -339,6 +339,18 @@ $links = "list";
                                     </colgroup>
                                     <tbody>
                                     <tr>
+                                        <th><?=$type['roomName']?></th>
+                                        <td>
+                                            <div class="head_table">
+                                                <div class="btn_more">
+                                                    <button type="button" class="addTableBtn" value="<?=$mainIdx?>" data-prod="<?=$product_idx?>" data-roomtype="<?=$type['g_idx']?>" style="width:70px;background-color:#4f728a;color:#fff">룸 추가</button>
+                                                    <!--span style="color : red" class="note">※ 옵션 삭제 시에 해당 옵션과 연동된 주문, 결제내역에 영향을 미치니 반드시 확인 후에 삭제바랍니다</span-->
+                                                </div>
+                                                <!--div class="btn_save_all">
+                                                    <button style = "background-color : #4f728a; color : #fff">전체저장</button>
+                                                </div-->
+                                            </div>
+
 											<?php
 												$target_g_idx  = $type['g_idx']; // 원하는 g_idx 값 (예: 1번 그룹만 표시)
 												$filteredRooms = array_filter($roomsByType, function($room) use ($target_g_idx) {
@@ -348,19 +360,6 @@ $links = "list";
 										    
 											<?php foreach ($filteredRooms as $row): ?>
 			                                <?php $roomIdx++; ?>
-									
-                                        <th><?=$type['roomName']?></th>
-                                        <td>
-                                            <div class="head_table">
-                                                <div class="btn_more">
-                                                    <button type="button" class="addTableBtn" value="<?=$roomIdx?>" data-prod="<?=$product_idx?>" data-roomtype="<?=$type['g_idx']?>" style="width:70px;background-color:#4f728a;color:#fff">룸 추가</button>
-                                                    <!--span style="color : red" class="note">※ 옵션 삭제 시에 해당 옵션과 연동된 주문, 결제내역에 영향을 미치니 반드시 확인 후에 삭제바랍니다</span-->
-                                                </div>
-                                                <!--div class="btn_save_all">
-                                                    <button style = "background-color : #4f728a; color : #fff">전체저장</button>
-                                                </div-->
-                                            </div>
-
                                             <div id="table_child_<?=$mainIdx?>">
 											    <?php //if($row['room_name']) { ?>
 												
