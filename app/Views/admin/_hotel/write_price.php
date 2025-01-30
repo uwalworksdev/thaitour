4ccb36987c8ad4b15f8d6e8e7e65cf82163b96b0
diana tgkim <diana001@naver.com>
2025-01-31 01:04:38 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -912,7 +912,6 @@ $links = "list";
 			// Extract the roomIdx from the button's value
 			const roomIdx    = $(this).val();
 			const currentRow = $(this).closest('tr');
-            let lastRow = $(".bed_child_"+roomIdx:last").clone();
 
 			// Define the new bed type row
 			const newBedRow = `
@@ -924,8 +923,7 @@ $links = "list";
 					</td>
 				</tr>`;
 			// Append the new row to the bed_child_<roomIdx> section
-			//currentRow.after(newBedRow);
-			$(".bed_child_"+roomIdx+":last").after(newBedRow);
+			currentRow.after(newBedRow);
 		});
 
 		// Remove a bed type row
