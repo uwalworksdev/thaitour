621319d32dcc6a5d3e855428b034627e8fe40549
diana tgkim <diana001@naver.com>
2025-01-31 01:32:23 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -843,7 +843,7 @@ $links = "list";
 								</td>
 							</tr>
 							
-							<tr> 
+							<tr class="bed_child_${room_Idx}"> 
 								<td>
 									<p style="margin-bottom: 3px;">침대타입추가 (침대타입의 가격은 추가되는 금액만 넣습니다. (제목/금액))
                                        <button style="width: 50px; background-color: #4f728a; color : #fff;" class="btn_set">참고</button>
@@ -853,7 +853,7 @@ $links = "list";
                                     <button type="button" style="width: 31px; height : 31px" value="${room_Idx}" class="addBedBtn" >+</button>
 								</td>
 							</tr>
-							<tr>
+							<tr class="option_child_${room_Idx}">
 								<td>
 									<p style="margin-bottom: 3px;">옵션 내용을 추가 합니다. (html 태그 사용가능)</p>
 									<input style="width: 20%;" type="text" name="option_val[${room_Idx}][]">
