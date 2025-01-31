9364cdea45881c33294ba35fbe1a63627eeddb77
diana tgkim <diana001@naver.com>
2025-01-31 13:02:26 +0900

    modified:   app/Views/product/hotel/hotel-details.php

--- app/Views/product/hotel/hotel-details.php
+++ app/Views/product/hotel/hotel-details.php
@@ -1151,7 +1151,7 @@
 									        <?php $real_bath  = $price_bath + $bed_price[$i];?>
 
                                             <div class="wrap_input">
-                                                <input type="radio" name="bed_type_<?=$room['rooms_idx']?>" id="<?=$room['g_idx']?><?=$room['rooms_idx']?>" value="<?=$i?>" checked="">
+                                                <input type="radio" name="bed_type_<?=$room['g_idx']?><?=$room['rooms_idx']?>" id="<?=$room['g_idx']?><?=$room['rooms_idx']?>" value="<?=$i?>" checked="">
                                                 <label for="bed_type_<?=$room['g_idx']?><?=$room['rooms_idx']?>"><?=$bed_type[$i]?>: <span style="color :coral"><?=number_format($real_won)?>원 (<?=number_format($real_bath)?>바트)</span></label>
                                             </div>
 											<?php } ?>
