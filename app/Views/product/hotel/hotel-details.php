18efb5abaac47b7b4298c571288c830a0ddd9028
diana tgkim <diana001@naver.com>
2025-01-31 11:24:57 +0900

    modified:   app/Views/product/hotel/hotel-details.php

--- app/Views/product/hotel/hotel-details.php
+++ app/Views/product/hotel/hotel-details.php
@@ -1077,7 +1077,7 @@
                                             <li><?=$option_val[$i]?></li>
 											<?php } ?>
                                             <!--li>온라인 사전결제</li-->
-                                            <li style="color : red">환불 불가</li>
+                                            <li><span style="color:red">환불 불가</span></li>
                                         </ul>
                                     </div>
                                 </td>
