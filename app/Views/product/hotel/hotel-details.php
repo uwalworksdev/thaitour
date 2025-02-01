d9b441b9b44cbccec5e9d61f62d96419da796a79
diana tgkim <diana001@naver.com>
2025-02-02 03:08:43 +0900

    modified:   app/Views/product/hotel/hotel-details.php

--- app/Views/product/hotel/hotel-details.php
+++ app/Views/product/hotel/hotel-details.php
@@ -1587,18 +1587,18 @@
             });
         </script>
     </div>
-    <input type="text" name="coupon_discount" id="coupon_discount" value="0">
-    <input type="text" name="coupon_name" id="coupon_name">
-    <input type="text" name="coupon_type" id="coupon_type">
-    <input type="text" name="total_last_price" id="total_last_price" value="0">
-    <input type="text" name="use_coupon_room" id="use_coupon_room">
-    <input type="text" name="use_op_type" id="use_op_type">
-    <input type="text" name="use_coupon_idx" id="use_coupon_idx">
-    <input type="text" name="number_room" id="number_room">
-    <input type="text" name="number_day" id="number_day">
-    <input type="text" name="product_idx" id="product_idx" value="<?= $hotel['product_idx'] ?>">
-    <input type="text" name="room_op_idx" id="room_op_idx" value="">
-    <input type="text" name="bed_type" id="bed_type" value="">
+    <input type="hidden" name="coupon_discount" id="coupon_discount" value="0">
+    <input type="hidden" name="coupon_name" id="coupon_name">
+    <input type="hidden" name="coupon_type" id="coupon_type">
+    <input type="hidden" name="total_last_price" id="total_last_price" value="0">
+    <input type="hidden" name="use_coupon_room" id="use_coupon_room">
+    <input type="hidden" name="use_op_type" id="use_op_type">
+    <input type="hidden" name="use_coupon_idx" id="use_coupon_idx">
+    <input type="hidden" name="number_room" id="number_room">
+    <input type="hidden" name="number_day" id="number_day">
+    <input type="hidden" name="product_idx" id="product_idx" value="<?= $hotel['product_idx'] ?>">
+    <input type="hidden" name="room_op_idx" id="room_op_idx" value="">
+    <input type="hidden" name="bed_type" id="bed_type" value="">
 	
 
     <div id="popup" class="popup" data-roop="" data-opId="" data-opType="" data-price="">
