9e39e902623b000eeb46c3ec274d8613295c979d
diana tgkim <diana001@naver.com>
2025-02-01 11:33:44 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -1463,11 +1463,11 @@ class Product extends BaseController
             $room_facil = $room_['room_facil'];
             $_arr_room_facil = explode("|", $room_facil);
             $list__room_facil = rtrim(implode(',', $_arr_room_facil), ',');
-*/
+
             $fsql = "SELECT * FROM tbl_code WHERE code_no IN ($list__room_facil) ORDER BY onum DESC, code_idx DESC";
             $fresult4 = $this->db->query($fsql);
             $fresult4 = $fresult4->getResultArray();
-
+*/
             //product_bedrooms
             $product_bedrooms = $hotel['product_bedrooms'];
             $_arr_product_bedrooms = explode("|", $product_bedrooms);
