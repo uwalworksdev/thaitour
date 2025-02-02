08c8533ae0c9ea61178f5de1a91587a30f4b2c42
diana tgkim <diana001@naver.com>
2025-02-02 10:45:34 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -1518,36 +1518,36 @@ class Product extends BaseController
     public function reservationFormInsert()
     {
         try {
-            $order_status = $this->request->getPost('order_status') ?? "W";
-            $product_idx = $this->request->getPost('product_idx') ?? 0;
-            $ho_idx = $this->request->getPost('ho_idx') ?? 0;
-            $room_op_idx = $this->request->getPost('room_op_idx') ?? 0;
-            $use_coupon_idx = $this->request->getPost('use_coupon_idx') ?? 0;
-            $used_coupon_money = $this->request->getPost('used_coupon_money') ?? 0;
-            $inital_price = $this->request->getPost('inital_price') ?? 0;
+            $order_status       = $this->request->getPost('order_status') ?? "W";
+            $product_idx        = $this->request->getPost('product_idx') ?? 0;
+            $ho_idx             = $this->request->getPost('ho_idx') ?? 0;
+            $room_op_idx        = $this->request->getPost('room_op_idx') ?? 0;
+            $use_coupon_idx     = $this->request->getPost('use_coupon_idx') ?? 0;
+            $used_coupon_money  = $this->request->getPost('used_coupon_money') ?? 0;
+            $inital_price       = $this->request->getPost('inital_price') ?? 0;
             $room_op_price_sale = $this->request->getPost('room_op_price_sale') ?? 0;
-            $order_price = $this->request->getPost('order_price') ?? 0;
-            $number_room = $this->request->getPost('number_room') ?? 0;
-            $number_day = $this->request->getPost('number_day') ?? 0;
-            $order_memo = $this->request->getPost('order_memo') ?? "";
-            $email_name = $this->request->getPost('email_1') ?? "";
-            $email_host = $this->request->getPost('email_2') ?? "";
-            $order_gender_list = $this->request->getPost('companion_gender') ?? "";
-            $order_user_name = $this->request->getPost('order_user_name') ?? "";
+            $order_price        = $this->request->getPost('order_price') ?? 0;
+            $number_room        = $this->request->getPost('number_room') ?? 0;
+            $number_day         = $this->request->getPost('number_day') ?? 0;
+            $order_memo         = $this->request->getPost('order_memo') ?? "";
+            $email_name         = $this->request->getPost('email_1') ?? "";
+            $email_host         = $this->request->getPost('email_2') ?? "";
+            $order_gender_list  = $this->request->getPost('companion_gender') ?? "";
+            $order_user_name    = $this->request->getPost('order_user_name') ?? "";
             $order_user_first_name_en = $this->request->getPost('order_user_first_name_en') ?? "";
-            $order_user_last_name_en = $this->request->getPost('order_user_last_name_en') ?? "";
-            $order_user_email = $email_name . "@" . $email_host;
-            $hotel = $this->productModel->find($product_idx);
-            $m_idx = session()->get("member")["idx"];
-            $ipAddress = $this->request->getIPAddress();
-            $device_type = get_device();
-            $code_name = $this->codeModel->getCodeName($hotel["product_code_1"]);
-            $radio_phone = $this->request->getPost('radio_phone') ?? "";
-            $phone_1 = $this->request->getPost('phone_1') ?? "";
-            $phone_2 = $this->request->getPost('phone_2') ?? "";
-            $phone_3 = $this->request->getPost('phone_3') ?? "";
-            $phone_thai = $this->request->getPost('phone_thai') ?? "";
-            $local_phone = $this->request->getPost('local_phone') ?? "";
+            $order_user_last_name_en  = $this->request->getPost('order_user_last_name_en') ?? "";
+            $order_user_email   = $email_name . "@" . $email_host;
+            $hotel              = $this->productModel->find($product_idx);
+            $m_idx              = session()->get("member")["idx"];
+            $ipAddress          = $this->request->getIPAddress();
+            $device_type        = get_device();
+            $code_name          = $this->codeModel->getCodeName($hotel["product_code_1"]);
+            $radio_phone        = $this->request->getPost('radio_phone') ?? "";
+            $phone_1            = $this->request->getPost('phone_1') ?? "";
+            $phone_2            = $this->request->getPost('phone_2') ?? "";
+            $phone_3            = $this->request->getPost('phone_3') ?? "";
+            $phone_thai         = $this->request->getPost('phone_thai') ?? "";
+            $local_phone        = $this->request->getPost('local_phone') ?? "";
             $additional_request = $this->request->getPost('additional_request') ?? "";
             if ($radio_phone == "kor") {
                 $order_user_phone = $phone_1 . "-" . $phone_2 . "-" . $phone_3;
@@ -1560,39 +1560,39 @@ class Product extends BaseController
             }
 
             $data = [
-                "m_idx" => $m_idx,
-                "device_type" => $device_type,
-                "product_idx" => $product_idx,
-                "product_code_1" => $hotel["product_code_1"],
-                "product_code_2" => $hotel["product_code_2"],
-                "product_code_3" => $hotel["product_code_3"],
-                "product_code_4" => $hotel["product_code_4"],
-                "product_code_list" => $hotel["product_code_list"],
-                "product_name" => $hotel["product_name"],
-                "code_name" => $code_name,
-                "order_gubun" => "hotel",
-                "order_user_name" => encryptField($order_user_name, "encode") ?? $order_user_name,
-                "order_user_mobile" => encryptField($order_user_phone, "encode") ?? $order_user_phone,
-                "local_phone" => encryptField($local_phone, "encode") ?? $local_phone,
-                "order_user_email" => encryptField($order_user_email, "encode") ?? $order_user_email,
+                "m_idx"                    => $m_idx,
+                "device_type"              => $device_type,
+                "product_idx"              => $product_idx,
+                "product_code_1"           => $hotel["product_code_1"],
+                "product_code_2"           => $hotel["product_code_2"],
+                "product_code_3"           => $hotel["product_code_3"],
+                "product_code_4"           => $hotel["product_code_4"],
+                "product_code_list"        => $hotel["product_code_list"],
+                "product_name"             => $hotel["product_name"],
+                "code_name"                => $code_name,
+                "order_gubun"              => "hotel",
+                "order_user_name"          => encryptField($order_user_name, "encode") ?? $order_user_name,
+                "order_user_mobile"        => encryptField($order_user_phone, "encode") ?? $order_user_phone,
+                "local_phone"              => encryptField($local_phone, "encode") ?? $local_phone,
+                "order_user_email"         => encryptField($order_user_email, "encode") ?? $order_user_email,
                 "order_user_first_name_en" => encryptField($order_user_first_name_en, "encode") ?? $order_user_first_name_en,
-                "order_user_last_name_en" => encryptField($order_user_last_name_en, "encode") ?? $order_user_last_name_en,
-                "order_gender_list" => $order_gender_list,
-                "order_memo" => $order_memo,
-                "room_op_price_sale" => $room_op_price_sale,
-                "inital_price" => $inital_price,
-                "order_price" => $order_price,
-                "order_date" => Time::now('Asia/Seoul', 'en_US'),
-                "used_coupon_idx" => $use_coupon_idx,
-                "used_coupon_money" => $used_coupon_money,
-                "ho_idx" => $ho_idx,
-                "room_op_idx" => $room_op_idx,
-                "order_room_cnt" => $number_room,
-                "order_day_cnt" => $number_day,
-                "order_r_date" => Time::now('Asia/Seoul', 'en_US'),
-                "order_status" => $order_status,
-                "encode" => "Y",
-                "additional_request" => $additional_request,
+                "order_user_last_name_en"  => encryptField($order_user_last_name_en, "encode") ?? $order_user_last_name_en,
+                "order_gender_list"        => $order_gender_list,
+                "order_memo"               => $order_memo,
+                "room_op_price_sale"       => $room_op_price_sale,
+                "inital_price"             => $inital_price,
+                "order_price"              => $order_price,
+                "order_date"               => Time::now('Asia/Seoul', 'en_US'),
+                "used_coupon_idx"          => $use_coupon_idx,
+                "used_coupon_money"        => $used_coupon_money,
+                "ho_idx"                   => $ho_idx,
+                "room_op_idx"              => $room_op_idx,
+                "order_room_cnt"           => $number_room,
+                "order_day_cnt"            => $number_day,
+                "order_r_date"             => Time::now('Asia/Seoul', 'en_US'),
+                "order_status"             => $order_status,
+                "encode"                   => "Y",
+                "additional_request"       => $additional_request,
                 "ip" => $ipAddress
             ];
 
@@ -1605,43 +1605,50 @@ class Product extends BaseController
                     $this->coupon->update($use_coupon_idx, ["status" => "E"]);
 
                     $cou_his = [
-                        "order_idx" => $order_idx,
-                        "product_idx" => $product_idx,
-                        "used_coupon_no" => $coupon["coupon_num"] ?? "",
-                        "used_coupon_idx" => $use_coupon_idx,
+                        "order_idx"         => $order_idx,
+                        "product_idx"       => $product_idx,
+                        "used_coupon_no"    => $coupon["coupon_num"] ?? "",
+                        "used_coupon_idx"   => $use_coupon_idx,
                         "used_coupon_money" => $used_coupon_money,
-                        "ch_r_date" => Time::now('Asia/Seoul', 'en_US'),
-                        "m_idx" => $m_idx
+                        "ch_r_date"         => Time::now('Asia/Seoul', 'en_US'),
+                        "m_idx"             => $m_idx
                     ];
 
                     $this->couponHistory->insert($cou_his);
                 }
 
-                $order_num_room = $this->request->getPost('order_num_room');
+                $order_num_room   = $this->request->getPost('order_num_room');
                 $order_first_name = $this->request->getPost('order_first_name');
-                $order_last_name = $this->request->getPost('order_last_name');
+                $order_last_name  = $this->request->getPost('order_last_name');
                 foreach ($order_num_room as $key => $value) {
-                    $first_name = encryptField($order_first_name[$key], "encode");
-                    $last_name = encryptField($order_last_name[$key], "encode");
+                    $first_name   = encryptField($order_first_name[$key], "encode");
+                    $last_name    = encryptField($order_last_name[$key], "encode");
                     $data_sub = [
-                        "m_idx" => $m_idx,
-                        "order_idx" => $order_idx,
-                        "product_idx" => $product_idx,
-                        "number_room" => filter_var(preg_replace('/[^0-9]/', '', $value), FILTER_SANITIZE_NUMBER_INT),
-                        "order_gubun" => "hotel",
+                        "m_idx"            => $m_idx,
+                        "order_idx"        => $order_idx,
+                        "product_idx"      => $product_idx,
+                        "number_room"      => filter_var(preg_replace('/[^0-9]/', '', $value), FILTER_SANITIZE_NUMBER_INT),
+                        "order_gubun"      => "hotel",
                         "order_first_name" => $first_name,
-                        "order_last_name" => $last_name,
-                        "encode" => "Y"
+                        "order_last_name"  => $last_name,
+                        "encode"           => "Y"
                     ];
                     $this->orderSubModel->insert($data_sub);
                 }
 
                 $this->response->deleteCookie('cart');
 
-                return $this->response->setJSON([
-                    'result' => true,
-                    'message' => "에약되었습니다."
-                ], 200);
+                if($order_status == "B") {
+					return $this->response->setJSON([
+						'result' => true,
+						'message' => "장바구니에 저장되었습니다."
+					], 200);
+				} else {
+					return $this->response->setJSON([
+						'result' => true,
+						'message' => "에약되었습니다."
+					], 200);
+				} 	
             }
 
             return $this->response->setJSON([
