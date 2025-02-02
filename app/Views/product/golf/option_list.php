2057551a780ec3af2b4a7253bac7a5c20e67cd40
diana tgkim <diana001@naver.com>
2025-02-03 02:49:19 +0900

    modified:   app/Views/product/golf/option_list.php

--- app/Views/product/golf/option_list.php
+++ app/Views/product/golf/option_list.php
@@ -5,6 +5,7 @@
         data-minute            = "<?=$option['minute']?>"
         data-idx               = "<?=$option['idx']?>"
         data-option_price      = "<?=$option['option_price']?>"
+		data-o_afternoon_yn    = "<?=$option['o_afternoon_yn']?>"
 		data-o_night_yn        = "<?=$option['o_night_yn']?>"
         data-option_price_baht = "<?=$option['option_price_baht']?>"
         data-caddy_fee         = "<?=$option['caddy_fee']?>"
