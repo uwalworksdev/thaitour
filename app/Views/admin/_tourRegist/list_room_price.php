834609288d599358905b47ad5d64d8f4bf0ecb54
diana tgkim <diana001@naver.com>
2025-02-05 16:43:24 +0900

    new file:   app/Controllers/TourRegistController.php.tmp 	modified:   app/Views/admin/_tourRegist/list_room_price.php

--- app/Views/admin/_tourRegist/list_room_price.php
+++ app/Views/admin/_tourRegist/list_room_price.php
@@ -231,7 +231,7 @@
 														<input type="text" name="goods_price3[]" id="price4_<?=$item['idx']?>" value="<?=number_format($item['goods_price3'])?>" class="price goods_price input_txt" numberonly="true" style="text-align:right;">
 													</td>
 													<td style="text-align:center">
-														<input type="text" name="price[]" id="price_<?=$item['idx']?>" value="<?=number_format($item['price'])?>" class="price goods_price input_txt" numberonly="true" style="text-align:right;">
+													    <?=number_format($item['goods_price2'] + $item['goods_price3'])?> 
 													</td>
 													<td style="text-align:center;">
 														<input type="checkbox" class="use_yn" name="use_yn[]" id="use_yn_<?=$item['idx']?>" data-idx= "<?=$item['idx']?>" value="<?=$item['goods_date']?>" <?php if($item['use_yn'] == "N") echo "checked";?> >
