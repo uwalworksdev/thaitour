320906ed9c40037f82dcec45c8a6c72dfe5b18e5
diana tgkim <diana001@naver.com>
2025-02-06 20:41:52 +0900

    modified:   app/Controllers/AjaxController.php

--- app/Controllers/AjaxController.php
+++ app/Controllers/AjaxController.php
@@ -870,8 +870,12 @@ $endDate = new DateTime($from_date);
 $endDate = $endDate->modify('+'.$days-1 .'days'); // 3일 포함하기 위해 +2 days
 $endDate = $endDate->format('Y-m-d');
 
-	$sql          = "select * from tbl_room_price where product_idx = '". $product_idx ."' and 
-	                                                    g_idx       = '". $room['g_idx'] ."' and 
+	$sql          = "select sum(goods_proce1) as goods_proce1,
+	                        sum(goods_proce2) as goods_proce2,
+							sum(goods_proce3) as goods_proce3,
+							baht_thai
+							from tbl_room_price where product_idx   = '". $product_idx ."'       and 
+	                                                    g_idx       = '". $room['g_idx'] ."'     and 
 														rooms_idx   = '". $room['rooms_idx'] ."' and 
 														(goods_date BETWEEN '". $from_date ."' and '". $endDate ."')";
 	$result       = $db->query($sql);
