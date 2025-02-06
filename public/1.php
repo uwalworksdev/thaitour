c7438aa7142c209f40202e3666ac8ee882dd7c98
diana tgkim <diana001@naver.com>
2025-02-06 13:40:08 +0900

    modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -1,17 +1,10 @@
 <?php
-function getDatesAfterSpecificDate($specificDate) {
-    $dates = [];
+// 특정 시작일 설정
+$startDate = new DateTime('2025-02-06'); // 원하는 날짜로 변경
 
-    $date = new DateTime($specificDate);
-    for ($i = 1; $i <= 3; $i++) {
-        $dates[] = $date->add(new DateInterval('P1D'))->format('Y-m-d');
-    }
-
-    return $dates;
+// 3일간 날짜 출력
+for ($i = 0; $i < 3; $i++) {
+    echo $startDate->format('Y-m-d') . "<br>";
+    $startDate->modify('+1 day'); // 하루씩 증가
 }
-
-$specificDate = '2025-02-06';
-$result = getDatesAfterSpecificDate($specificDate);
-
-print_r($result);
 ?>
