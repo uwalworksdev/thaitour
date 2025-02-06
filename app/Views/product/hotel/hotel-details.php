d245bfe407382f8b776f01c423df7f817075e649
diana tgkim <diana001@naver.com>
2025-02-07 00:19:40 +0900

    modified:   app/Views/product/hotel/hotel-details.php

--- app/Views/product/hotel/hotel-details.php
+++ app/Views/product/hotel/hotel-details.php
@@ -717,7 +717,6 @@
 					var room_qty      = parseInt($("#room_qty").val());
 					var day_qty       = parseInt($("#day_qty").val());
 					var total_last_price = price * room_qty;
-					alert(total_last_price);
 					
 					$("#room_op_idx").val(room_op_idx);
 					$("#bed_type").val(bed_type);
