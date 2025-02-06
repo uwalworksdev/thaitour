bfaa2c306d401cf067e733f40b0aa0e2d7195df9
diana tgkim <diana001@naver.com>
2025-02-06 15:05:09 +0900

    modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -6,7 +6,7 @@ $startDate = new DateTime($from_date); // 시작 날짜 설정
 
 // 종료일 계산 (+2 days 해야 정확히 3일 포함됨)
 $endDate = new DateTime($from_date);
-$endDate->modify('+'.$days .'days'); // 3일 포함하기 위해 +2 days
+$endDate->modify('+'.$days-1 .'days'); // 3일 포함하기 위해 +2 days
 
 // 날짜 출력
 echo $from_date . " ~ " . $endDate->format('Y-m-d');
