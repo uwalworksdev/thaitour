31d35521242d3cd61b833bc57fb59ed2a336ecbc
diana tgkim <diana001@naver.com>
2025-02-06 15:06:35 +0900

    modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -1,7 +1,7 @@
 <?php
 // 특정 시작일 설정
-$from_date = "2025-02-06";
-$days = "3";
+$from_date = "2025-02-10";
+$days = "2";
 $startDate = new DateTime($from_date); // 시작 날짜 설정
 
 // 종료일 계산 (+2 days 해야 정확히 3일 포함됨)
