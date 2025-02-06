94fb7140debe4612a1fc5e0f9470d09b8b40dd26
diana tgkim <diana001@naver.com>
2025-02-06 15:03:54 +0900

    modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -1,11 +1,12 @@
 <?php
 // 특정 시작일 설정
 $from_date = "2025-02-06";
+$days = "3";
 $startDate = new DateTime($from_date); // 시작 날짜 설정
 
 // 종료일 계산 (+2 days 해야 정확히 3일 포함됨)
 $endDate = new DateTime($from_date);
-$endDate->modify('+2 days'); // 3일 포함하기 위해 +2 days
+$endDate->modify('+'.$days .'days'); // 3일 포함하기 위해 +2 days
 
 // 날짜 출력
 echo $from_date . " ~ " . $endDate->format('Y-m-d');
