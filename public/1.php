b9c6a2f1a992f5fdc246e198dfcb87769c59263e
diana tgkim <diana001@naver.com>
2025-02-06 13:23:25 +0900

    modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -1,19 +1,21 @@
 <?php
-		$start_date = "2025-02-01";
-		$end_date   = "2025-02-10";
+function getDatesIncludingSpecificDate($specificDate) {
+    $dates = [];
 
-		$period = new DatePeriod(
-			new DateTime($start_date),
-			new DateInterval('P1D'), // 하루씩 증가
-			(new DateTime($end_date))->modify('+1 day') // 종료일 포함
-		);
+    $date = new DateTime($specificDate);
+    for ($i = -1; $i <= 1; $i++) {
+        $interval = new DateInterval('P' . abs($i) . 'D');
+        if ($i < 0) {
+            $interval->invert = 1; // 전날
+        }
+        $dates[] = $date->add($interval)->format('Y-m-d');
+    }
 
-		$date_list = [];
-		foreach ($period as $date) {
-			echo $date->format("Y-m-d") ."<br>";
-		}
+    return $dates;
+}
 
-		// 출력 확인
-		//print_r($date_list);
+$specificDate = '2025-02-06';
+$result = getDatesIncludingSpecificDate($specificDate);
 
+print_r($result);
 ?>
