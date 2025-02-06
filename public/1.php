df61dc48ebe1955fe2cf9f2a1703a14b329ab605
diana tgkim <diana001@naver.com>
2025-02-06 13:25:46 +0900

    modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -1,21 +1,17 @@
 <?php
-function getDatesIncludingSpecificDate($specificDate) {
+function getDatesAfterSpecificDate($specificDate) {
     $dates = [];
 
     $date = new DateTime($specificDate);
-    for ($i = -1; $i <= 1; $i++) {
-        $interval = new DateInterval('P' . abs($i) . 'D');
-        if ($i < 0) {
-            $interval->invert = 1; // 전날
-        }
-        $dates[] = $date->add($interval)->format('Y-m-d');
+    for ($i = 0; $i <= 3; $i++) {
+        $dates[] = $date->add(new DateInterval('P1D'))->format('Y-m-d');
     }
 
     return $dates;
 }
 
 $specificDate = '2025-02-06';
-$result = getDatesIncludingSpecificDate($specificDate);
+$result = getDatesAfterSpecificDate($specificDate);
 
 print_r($result);
 ?>
