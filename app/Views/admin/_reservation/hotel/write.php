b76ff57c8a9d5fef340f4a0cdfe8c4786bbab29b
diana tgkim <diana001@naver.com>
2025-02-09 12:41:23 +0900

    modified:   app/Views/admin/_reservation/hotel/write.php

--- app/Views/admin/_reservation/hotel/write.php
+++ app/Views/admin/_reservation/hotel/write.php
@@ -458,7 +458,7 @@
                             </table>
 							
 							<br>
-							<div style="font-size:12pt;margin-bottom:10px">■ 바우처/인보이스</div>
+							<div style="font-size:12pt;margin-bottom:10px">■ 바우처/인보이스xxxxxxx</div>
                             <table cellpadding="0" cellspacing="0" summary="" class="listTable mem_detail">
                                 <caption>
                                 </caption>
