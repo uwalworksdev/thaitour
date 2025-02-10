b4d879e0544de4e6a5da4fbf33601eadaae4354b
diana tgkim <diana001@naver.com>
2025-02-10 11:45:19 +0900

    modified:   app/Views/product/golf/customer-form.php

--- app/Views/product/golf/customer-form.php
+++ app/Views/product/golf/customer-form.php
@@ -530,6 +530,42 @@
     <script>
         function handleSubmit(status) {
 
+			if ($("#order_user_name").val() === "") {
+				alert("한글명을 입력해주세요!");
+				$("#order_user_name").focus();
+				return false;
+			}
+
+			if ($("#order_user_first_name_en").val() === "") {
+				alert("영문명을 입력해주세요!");
+				$("#order_user_first_name_en").focus()
+				return false;
+			}
+			
+			if ($("#order_user_last_name_en").val() === "") {
+				alert("영문명을 입력해주세요!");
+				$("#order_user_last_name_en").focus()
+				return false;
+			}
+
+			if ($("#order_passport_number").val() === "") {
+				alert("여권번호를 입력해주세요!");
+				$("#order_passport_number").focus();
+				return false;
+			}
+
+			if ($("#order_passport_expiry_date").val() === "") {
+				alert("여권만기일을 입력해주세요!");
+				$("#order_passport_expiry_date").focus();
+				return false;
+			}
+
+			if ($("#order_birth_date").val() === "") {
+				alert("생년월일을 입력해주세요!");
+				$("#order_birth_date").focus()
+				return false;
+			}
+				
 			$("#order_status").val(status);
             const frm = document.order_frm;
             let flag = true;
