d89520677ed6686220939ad9359973cf4b0eae7b
datdo <datvbhp123@gmail.com>
2025-02-11 14:47:55 +0700

    fix-css-hotel-detail

---
+++ app/Models/ProductImg.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProductImg extends Model
+{
+    protected $table = 'tbl_product_mst';
+
+    protected $primaryKey = 'product_idx';
+
+    protected $allowedFields = [
+        "product_idx", "ufile", "rfile", "m_date", "r_date"
+    ];
+
+    protected function initialize()
+    {
+    }
+
+    public function insertData($data)
+    {
+        $allowedFields = $this->allowedFields;
+
+        $filteredData = array_filter($data, function ($key) use ($allowedFields, $data) {
+            return in_array($key, $allowedFields) && (is_string($data[$key]) || is_numeric($data[$key]));
+        },
+            ARRAY_FILTER_USE_KEY
+        );
+
+        foreach ($filteredData as $key => $value) {
+            $filteredData[$key] = updateSQ($value);
+        }
+
+        return $this->insert($filteredData);
+    }
+
+    public function updateData($id, $data)
+    {
+        $allowedFields = $this->allowedFields;
+
+        $filteredData = array_filter(
+            $data,
+            function ($key) use ($allowedFields, $data) {
+                return in_array($key, $allowedFields) && (is_string($data[$key]) || is_numeric($data[$key]));
+            },
+            ARRAY_FILTER_USE_KEY
+        );
+
+        foreach ($filteredData as $key => $value) {	
+            $filteredData[$key] = updateSQ($value);
+        }
+
+		return $this->update($id, $filteredData);
+    }
+}
