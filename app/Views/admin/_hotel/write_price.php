f6b887ba5ca6c8ac2495c4b6416d7c377e93ff05
diana tgkim <diana001@naver.com>
2025-02-12 13:07:56 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -675,6 +675,7 @@ $links = "list";
 										// $img = get_img(${"ufile" . $i}, "/data/product/", "600", "440");
 										$img = "/uploads/rooms/" . ${"ufile" . $i};
 										?>
+										<?php if(${"ufile" . $i} != "") { ?>
 										<div class="file_input <?= empty(${"ufile" . $i}) ? "" : "applied" ?>">
 											<input type="file" name='room_ufile<?= $i ?>' id="room_ufile<?= $i ?>" onchange="productImagePreview2(this, '<?= $i ?>')">
 											<label for="room_ufile<?= $i ?>" <?= !empty(${"room_ufile" . $i}) ? "style='background-image:url($img)'" : "" ?>></label>
@@ -683,9 +684,9 @@ $links = "list";
 											<?php if(${"ufile" . $i} != "") { ?>
 											<button type="button" class="remove_btn" onclick="productImagePreviewRemove(this)"></button>
 											<a class="img_txt imgpop_p" href="<?= $img ?>" id="text_room_ufile<?= $i ?>">미리보기</a>
-											<?php } ?>
 												
 										</div>
+										<?php } ?>
 									<?php
 									endfor;
 									?>
