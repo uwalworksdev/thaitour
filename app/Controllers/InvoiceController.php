1f6a439e1018b14c25a5f95a27b4b4e604160ce2
diana tgkim <diana001@naver.com>
2025-02-14 19:19:45 +0900

    modified:   app/Controllers/InvoiceController.php

--- app/Controllers/InvoiceController.php
+++ app/Controllers/InvoiceController.php
@@ -31,6 +31,17 @@ class InvoiceController extends BaseController
     {
 		$db      = db_connect(); // DB 연결
 		$builder = $db->table('tbl_order_mst'); // 테이블 지정
+		$builder->select("
+			AES_DECRYPT(UNHEX(order_user_name), '$private_key') AS order_user_name,
+			AES_DECRYPT(UNHEX(order_user_email), '$private_key') AS order_user_email,
+			AES_DECRYPT(UNHEX(order_user_first_name_en), '$private_key') AS order_user_first_name_en,
+			AES_DECRYPT(UNHEX(order_user_last_name_en), '$private_key') AS order_user_last_name_en,
+			AES_DECRYPT(UNHEX(order_user_mobile), '$private_key') AS order_user_mobile,
+			AES_DECRYPT(UNHEX(local_phone), '$private_key') AS local_phone,
+			AES_DECRYPT(UNHEX(order_zip), '$private_key') AS order_zip,
+			AES_DECRYPT(UNHEX(order_addr1), '$private_key') AS order_addr1,
+			AES_DECRYPT(UNHEX(order_addr2), '$private_key') AS order_addr2
+		");		
 		$query   = $builder->where('order_idx', $idx)->get(); // 조건 추가 후 실행
 
 		$result  = $query->getResult(); // 결과 가져오기 (객체 배열)
