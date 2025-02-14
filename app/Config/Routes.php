3f25130eded00669f6a33c4f489a80441cce3efc
diana tgkim <diana001@naver.com>
2025-02-14 17:33:00 +0900

    modified:   app/Config/Routes.php 	modified:   app/Views/admin/_reservation/hotel/write.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -695,7 +695,7 @@ $routes->group("invoice", static function ($routes) {
     $routes->get("view_paid", "Orders::invoice_view_paid");
     $routes->get("golf", "InvoiceController::golf");
     $routes->get("hotel", "InvoiceController::hotel");
-    $routes->get('hotel_01/(:num)', 'InvoiceController::hotel_01/$1');
+    $routes->post('hotel_01/(:num)', 'InvoiceController::hotel_01/$1');
 	
     $routes->get("ticket_01", "InvoiceController::ticket_01");
     $routes->get("ticket_02", "InvoiceController::ticket_02");
