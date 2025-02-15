cccded4865db67b83afb87e23f8d4020e18ec625
datdo <datvbhp123@gmail.com>
2025-02-15 11:00:13 +0700

    update-img-room-guides

---
+++ app/Models/RoomImg.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class RoomImg extends Model
+{
+    protected $table = 'tbl_room_img';
+
+    protected $primaryKey = 'i_idx';
+
+    protected $allowedFields = [
+        "room_idx", "ufile", "rfile", "m_date", "r_date"
+    ];
+
+    protected function initialize()
+    {
+    }
+
+    public function getImg($room_idx)
+    {
+		return $this->where('room_idx', $room_idx)
+                    ->where('ufile !=', '') // ufile이 공란이 아닌 경우
+                    ->orderBy("i_idx", "asc")
+                    ->findAll();
+
+        //return $this->where('room_idx', $room_idx)->orderBy("i_idx", "asc")->findAll();
+    }
+
+    public function insertData($data)
+    {
+        $allowedFields = $this->allowedFields;
+
+        $filteredData = array_filter($data, function ($key) use ($allowedFields, $data) {
+            return in_array($key, $allowedFields) && (is_string($data[$key]) || is_numeric($data[$key]));
+        },
+            ARRAY_FILTER_USE_KEY
+        );
+
+        foreach ($filteredData as $key => $value) {
+            $filteredData[$key] = updateSQ($value);
+        }
+
+        return $this->insert($filteredData);
+    }
+
+    public function updateData($id, $data)
+    {
+        $allowedFields = $this->allowedFields;
+
+        $filteredData = array_filter(
+            $data,
+            function ($key) use ($allowedFields, $data) {
+                return in_array($key, $allowedFields) && (is_string($data[$key]) || is_numeric($data[$key]));
+            },
+            ARRAY_FILTER_USE_KEY
+        );
+
+        foreach ($filteredData as $key => $value) {	
+            $filteredData[$key] = updateSQ($value);
+        }
+
+		return $this->update($id, $filteredData);
+    }
+}
