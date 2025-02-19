8ce63d8d92fc6c76b5d219026aaaf4ff8499ad4c
diana tgkim <diana001@naver.com>
2025-02-19 12:18:04 +0900

    modified:   public/allimTalk/1.php

--- public/allimTalk/1.php
+++ public/allimTalk/1.php
@@ -1,6 +1,5 @@
 <?php
-        include_once '../Common.php';
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/../app/common.php';
 		
-		alimTalk_send("S20250210003","TY_1655");
-		
+	alimTalk_send("S20250210003","TY_1655");
 ?>		
