0aae5c44cbd5cac11efa674e912eb673307d22ea
diana tgkim <diana001@naver.com>
2025-02-19 12:32:42 +0900

    modified:   public/mailTest/1.php

--- public/mailTest/1.php
+++ public/mailTest/1.php
@@ -1,6 +1,8 @@
 <?php
         include_once dirname(__DIR__) . '/app/Common.php';
 
+        echo dirname(__DIR__);
+		/*
 		$code = "A01";
         $user_mail = "diana001@naver.com";
         $user_name = "김태균";
@@ -9,5 +11,5 @@
         ];
 		
         autoEmail($code, $user_mail, $_tmp_fir_array);
-		
+		*/
 ?>		
