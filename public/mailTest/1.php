1142bb042e358d37c4d78eb2d95bb83eb53f6130
diana tgkim <diana001@naver.com>
2025-02-19 11:50:13 +0900

    new file:   public/mailTest/1.php

---
+++ public/mailTest/1.php
@@ -0,0 +1,9 @@
+<?php
+		$code = "A01";
+        $user_mail = "diana001@naver.com";
+        $_tmp_fir_array = [
+            'name' => $user_name
+        ];
+        autoEmail($code, $user_mail, $_tmp_fir_array);
+		
+?>		
