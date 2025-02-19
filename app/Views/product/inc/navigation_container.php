a958426f0a7cae4ae0c28ddf31794557a7eb8e73
diana tgkim <diana001@naver.com>
2025-02-19 18:35:15 +0900

    modified:   app/Views/product/inc/navigation_container.php

--- app/Views/product/inc/navigation_container.php
+++ app/Views/product/inc/navigation_container.php
@@ -10,6 +10,7 @@
             </ul>
         </div>
     </div>
+	
     <div class="navigation-container-next">
         <img class="ball_dot_icon icon_open_depth_01 icon_open_depth_" data-depth="depth_1_tools_"
              src="/uploads/icons/ball_dot_icon.png"
@@ -22,14 +23,6 @@
                 <?php echo getHeaderTabSubChild($parent_code, $code_no); ?>
             </ul>
         </div>
-    </div>
-	
-    <div class="navigation-container-next">
-        <img class="ball_dot_icon icon_open_depth_01 icon_open_depth_" data-depth="depth_1_tools_"
-             src="/uploads/icons/ball_dot_icon.png"
-             alt="ball_dot_icon">
-        <img class="bread_arrow_right" src="/uploads/icons/bread_arrow_right.png" alt="bread_arrow_right">
-        <span class="font-bold">xcxcxcx</span>
     </div>
 	
     <div class="navigation-container-next">
