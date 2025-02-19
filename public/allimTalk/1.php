01d342c916f7d97dafbc7b9e8f54628828d84555
diana tgkim <diana001@naver.com>
2025-02-19 12:09:17 +0900

    new file:   public/allimTalk/1.php 	modified:   public/mailTest/1.php

---
+++ public/allimTalk/1.php
@@ -0,0 +1,6 @@
+<?php
+        include_once '../common.php';
+		
+		allimtalk('S20250210003','TY_1655');
+		
+?>		
--- public/mailTest/1.php
+++ public/mailTest/1.php
@@ -1,9 +1,13 @@
 <?php
+        include_once '../common.php';
+
 		$code = "A01";
         $user_mail = "diana001@naver.com";
+        $user_name = "김태균";
         $_tmp_fir_array = [
             'name' => $user_name
         ];
-       // autoEmail($code, $user_mail, $_tmp_fir_array);
+		
+        autoEmail($code, $user_mail, $_tmp_fir_array);
 		
 ?>		
