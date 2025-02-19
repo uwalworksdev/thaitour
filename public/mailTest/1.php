1e47656a1d9701038261ce3ac42ec89d601eda7a
diana tgkim <diana001@naver.com>
2025-02-19 12:29:36 +0900

    modified:   public/mailTest/1.php

--- public/mailTest/1.php
+++ public/mailTest/1.php
@@ -1,5 +1,5 @@
 <?php
-$common_path = $_SERVER['DOCUMENT_ROOT'] . '/app/Common.php';
+$common_path = '/app/Common.php';
 if (!file_exists($common_path)) {
     die("Common.php 파일이 존재하지 않습니다: " . realpath($common_path));
 }
