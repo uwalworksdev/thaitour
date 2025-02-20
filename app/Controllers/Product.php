0ebf4262d5d50dc0185d127d081e36bf4228e492
diana tgkim <diana001@naver.com>
2025-02-20 15:34:13 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -2353,7 +2353,9 @@ class Product extends BaseController
             $data['order_user_name'] = encryptField($data['order_user_name'], 'encode');
             $data['order_user_first_name_en'] = encryptField($data['order_user_first_name_en'], 'encode');
             $data['order_user_last_name_en'] = encryptField($data['order_user_last_name_en'], 'encode');
-
+				
+            $data['baht_thai']= $this->setting['baht_thai'];
+			
             if ($data['radio_phone'] == "kor") {
                 $order_user_mobile = $data['phone_1'] . "-" . $data['phone_2'] . "-" . $data['phone_3'];
             } else {
@@ -2502,10 +2504,10 @@ class Product extends BaseController
 			}
 
             for ($i = 0; $i < count($optIdx); $i++) {
-                $row = $this->golfOptionModel->getByIdx($optIdx[$i]);
+                $row          = $this->golfOptionModel->getByIdx($optIdx[$i]);
                 $option_price = $row['goods_price1'] * $this->setting['baht_thai'];
-                $option_tot = $row['goods_price1'] * $optCnt[$i] * $this->setting['baht_thai'];
-                $sql_order = "INSERT INTO tbl_order_option SET 
+                $option_tot   = $row['goods_price1'] * $optCnt[$i] * $this->setting['baht_thai'];
+                $sql_order    = "INSERT INTO tbl_order_option SET 
 														      option_type  = 'option'	
 														    , order_idx	   = '" . $order_idx . "'
 														    , product_idx  = '" . $data['product_idx'] . "'
