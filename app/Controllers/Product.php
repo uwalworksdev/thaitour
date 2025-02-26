e2a0fdfe4bb542c2c97666442ab5d243677fb2ab
diana tgkim <diana001@naver.com>
2025-02-27 02:36:00 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -2876,6 +2876,14 @@ class Product extends BaseController
                         //     }
                         // }
             if ($data['order_status'] == "W") {
+				
+			    $allim_replace = [
+									"#{고객명}" => $order_user_name,
+									"phone"     => $order_user_phone
+							     ];
+			    
+				alimTalkSend("TY_1652", $allim_replace);
+				
                 return $this->response->setBody("
 						<script>
 							alert('예약되었습니다');
