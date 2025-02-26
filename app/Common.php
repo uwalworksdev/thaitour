80bd627e95d3f48c5525ed5389758f94f83b3977
diana tgkim <diana001@naver.com>
2025-02-27 02:30:20 +0900

    modified:   app/Common.php

--- app/Common.php
+++ app/Common.php
@@ -1341,10 +1341,25 @@ function alimTalkSend($tmpCode, $allim_replace) {
 						]
 					];
 
-					// JSON 변환 후 변수에 할당
-					$_variables['button_1'] = json_encode(["button" => $buttons], JSON_UNESCAPED_UNICODE);
+				} else {
+					
+					// 버튼 정보 생성
+					$buttons = [
+						(object) [
+							"ordering"     => 2,
+							"name"         => "더투어랩",
+							"linkType"     => "WL",
+							"linkTypeName" => "웹링크",
+							"linkMo"       => "https://thetourlab.com",
+							"linkPc"       => "https://thetourlab.com",
+							"linkIos"      => "",
+							"linkAnd"      => ""
+						]
+					];
 				}
 
+				// JSON 변환 후 변수에 할당
+				$_variables['button_1'] = json_encode(["button" => $buttons], JSON_UNESCAPED_UNICODE);
 		}
 
 		//var_dump($button->linkType);
