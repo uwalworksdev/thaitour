6db80e1a11ca13443be97711527644c17f626cee
diana tgkim <diana001@naver.com>
2025-02-27 12:19:04 +0900

    modified:   app/Controllers/VoucherController.php

--- app/Controllers/VoucherController.php
+++ app/Controllers/VoucherController.php
@@ -79,6 +79,10 @@ class VoucherController extends BaseController
             $data['sup_options'] = $sup_options;
         }		
 
+		if (!is_array($data)) {
+			$data = []; // $data가 null이면 빈 배열로 설정
+		}
+
 		if (!is_array($row)) {
 			$row = []; // $row가 null이면 빈 배열로 설정
 		}
