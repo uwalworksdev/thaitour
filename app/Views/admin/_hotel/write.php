261caa0e79bb9059672698697c0ce8f26f5a9d19
diana tgkim <diana001@naver.com>
2025-02-28 14:48:57 +0900

    modified:   app/Views/admin/_hotel/write.php

--- app/Views/admin/_hotel/write.php
+++ app/Views/admin/_hotel/write.php
@@ -663,15 +663,19 @@ $links = "list";
                                     }
                                 });
 
-                                $('#all_code_best_utilities').change(function () {
-                                    if ($('#all_code_best_utilities').is(':checked')) {
-                                        $('.code_best_utilities').prop('checked', true)
-                                    } else {
-                                        $('.code_best_utilities').prop('checked', false)
-                                    }
-                                });
-
                                 $(document).ready(function () {										
+									// 전체 선택 체크박스 클릭 이벤트
+									$("#all_code_best_utilities").on("click", function () {
+										$(".code_best_utilities").prop("checked", $(this).prop("checked"));
+									});
+
+									// 개별 체크박스 클릭 시 전체 선택 체크박스 상태 변경
+									$(".code_utilities").on("click", function () {
+										$("#all_code_best_utilities").prop("checked", $(".code_best_utilities:checked").length === $(".code_best_utilities").length);
+									});										
+                                })
+
+								$(document).ready(function () {										
 									// 전체 선택 체크박스 클릭 이벤트
 									$("#all_code_utility").on("click", function () {
 										$(".code_utilities").prop("checked", $(this).prop("checked"));
