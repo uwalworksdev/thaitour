2cf839a6682206cc8a2aa089eb5fd2d0533baa68
diana tgkim <diana001@naver.com>
2025-03-10 18:36:54 +0900

    modified:   app/Views/mypage/info_option.php

--- app/Views/mypage/info_option.php
+++ app/Views/mypage/info_option.php
@@ -113,8 +113,8 @@ if ($_SESSION["member"]["mIdx"] == "") {
 							$gubun = chk_member_col(session('member.id'), "gubun");
 							if ($gubun == "naver") {
 								// 네이버 로그인 접근토큰 요청 예제
-								$client_id = "nEJfS5h48JVMBwecw1NJ";
-								$redirectURI = urlencode("https://" . $_SERVER["HTTP_HOST"] . "/include/naver.php");
+								$client_id = env('NAVER_CLIENT_ID');;
+								$redirectURI = urlencode("https://" . $_SERVER["HTTP_HOST"] . "/naver/callback");
 								$state = md5(microtime() . mt_rand()) . "myp";
 								$_SESSION['naver_state'] = $state;
 								$apiURL = "https://nid.naver.com/oauth2.0/authorize?mode=mypage&response_type=code&client_id=" . $client_id . "&redirect_uri=" . $redirectURI . "&state=" . $state;
