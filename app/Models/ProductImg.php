e49c9580c1fdb9d797c1faca09c59d033657155f
datdo <datvbhp123@gmail.com>
2025-03-11 14:39:09 +0700

    fix-img-hotel

--- app/Models/ProductImg.php
+++ app/Models/ProductImg.php
@@ -11,7 +11,7 @@ class ProductImg extends Model
     protected $primaryKey = 'i_idx';
 
     protected $allowedFields = [
-        "product_idx", "ufile", "rfile", "m_date", "r_date"
+        "product_idx", "ufile", "rfile", "onum", "m_date", "r_date"
     ];
 
     protected function initialize()
