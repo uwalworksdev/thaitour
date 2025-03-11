82027de2f40085803aa1971d9aa0f28acebcbc21
diana tgkim <diana001@naver.com>
2025-03-12 03:14:54 +0900

    modified:   app/Views/product/golf/customer-form.php

--- app/Views/product/golf/customer-form.php
+++ app/Views/product/golf/customer-form.php
@@ -541,42 +541,44 @@
     <script>
         function handleSubmit(status) {
 
-			if ($("#order_user_name_kor").val() === "") {
-				alert("한글명을 입력해주세요!");
-				$("#order_user_name_kor").focus();
-				return false;
-			}
+            if(status == "W") {
+					if ($("#order_user_name_kor").val() === "") {
+						alert("한글명을 입력해주세요!");
+						$("#order_user_name_kor").focus();
+						return false;
+					}
 
-			if ($("#order_user_first_name_en").val() === "") {
-				alert("영문명을 입력해주세요!");
-				$("#order_user_first_name_en").focus()
-				return false;
+					if ($("#order_user_first_name_en").val() === "") {
+						alert("영문명을 입력해주세요!");
+						$("#order_user_first_name_en").focus()
+						return false;
+					}
+					
+					if ($("#order_user_last_name_en").val() === "") {
+						alert("영문명을 입력해주세요!");
+						$("#order_user_last_name_en").focus()
+						return false;
+					}
+
+					if ($("#order_passport_number").val() === "") {
+						alert("여권번호를 입력해주세요!");
+						$("#order_passport_number").focus();
+						return false;
+					}
+
+					if ($("#order_passport_expiry_date").val() === "") {
+						alert("여권만기일을 입력해주세요!");
+						$("#order_passport_expiry_date").focus();
+						return false;
+					}
+
+					if ($("#order_birth_date").val() === "") {
+						alert("생년월일을 입력해주세요!");
+						$("#order_birth_date").focus()
+						return false;
+					}
 			}
 			
-			if ($("#order_user_last_name_en").val() === "") {
-				alert("영문명을 입력해주세요!");
-				$("#order_user_last_name_en").focus()
-				return false;
-			}
-
-			if ($("#order_passport_number").val() === "") {
-				alert("여권번호를 입력해주세요!");
-				$("#order_passport_number").focus();
-				return false;
-			}
-
-			if ($("#order_passport_expiry_date").val() === "") {
-				alert("여권만기일을 입력해주세요!");
-				$("#order_passport_expiry_date").focus();
-				return false;
-			}
-
-			if ($("#order_birth_date").val() === "") {
-				alert("생년월일을 입력해주세요!");
-				$("#order_birth_date").focus()
-				return false;
-			}
-				
 			$("#order_status").val(status);
             const frm = document.order_frm;
             let flag = true;
