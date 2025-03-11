7438e9f3a169159a74b7e2b545bc8dc577b2023a
diana tgkim <diana001@naver.com>
2025-03-12 03:31:45 +0900

    modified:   app/Views/cart/item-list.php

--- app/Views/cart/item-list.php
+++ app/Views/cart/item-list.php
@@ -857,7 +857,69 @@
 			<?php } ?>
 			<!-- 쇼ㆍ입장권 END -->
 
-
+			<!-- 레스토랑 S: -->
+			<?php if($restaurant_cnt > 0) { ?>
+			<div class="cart-left only_mo">
+				<div class="main-cart">
+					<div class="checkbox-group-2 form-group">
+						<input type="checkbox" id="group_restaurant_mo" class="checkbox" data-value="">
+						<label class="font-bold" for="group_restaurant_mo">레스토랑 :<span class="text-red"> <?=$restaurant_cnt?></span>
+						</label>
+					</div>
+					<div class="table-container custom-mo only_mo">
+						<?php $i = 0;?>
+						<?php foreach ($restaurant_result as $item): ?>
+						<?php $i++;?>
+						<div class="item">
+							<div class="con-up">
+								<div class="picture-con">
+									<img src="/data/product/<?=$item['ufile1']?>" alt="">
+									<div class="checkbox-group-2 form-group form-table">
+										<input type="checkbox" id="group_5_mo_item<?=$i?>" class="chkrestaurant checkbox" data-idx="<?=$item['order_idx']?>" data-value="<?=$item['order_no']?>">
+										<label class="font-bold" for="group_5_mo_item<?=$i?>"></label>
+									</div>
+								</div>
+								<div class="text-right-p">
+									<h3 class="title-p">
+										<?=$item['product_name']?>
+									</h3>
+									<div class="time-date-p">
+										<?=$item['order_date']?>
+									</div>
+									<p class="des-p">
+										<?php 
+											if (!empty($item['options'])) {
+												$options = explode('|', $item['options']);
+												foreach ($options as $option) {
+													$option_r = explode(":", esc($option));
+													echo $option_r[0] ."/ ". $option_r[1] ." EA / ". number_format($option_r[2]) ." 원<br>";
+												}
+											}
+										?>
+									</p>
+								</div>
+							</div>
+							<div class="des-space-p">
+								<div class="des-item">
+									<span class="space-left">금액</span>
+									<span><?=number_format($item['order_price']-$item['option_amt'])?> 원</span>
+								</div>
+								<div class="des-item">
+									<span class="space-left">옵션금액</span>
+									<span><?=number_format($item['option_amt'])?> 원</span>
+								</div>
+								<div class="des-item">
+									<span class="space-left">결제예정금액</span>
+									<span><?=number_format($item['order_price'])?> 원</span>
+								</div>
+							</div>
+						</div>
+						<?php endforeach; ?>
+					</div>
+				</div>
+			</div>
+			<?php } ?>				
+			<!-- 레스토랑 E; -->
 
 			<!-- 차량 START -->
             <?php if($car_cnt > 0) { ?>
