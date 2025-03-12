2ad9d606fe844d2e881db5b8d3a7a50845aef0b2
diana tgkim <diana001@naver.com>
2025-03-12 16:47:33 +0900

    modified:   app/Views/cart/item-list.php

--- app/Views/cart/item-list.php
+++ app/Views/cart/item-list.php
@@ -834,7 +834,7 @@
 													$options = explode('|', $item['options']);
 													foreach ($options as $option) {
 														$option_r = explode(":", esc($option));
-														echo $option_r[0] ."/ ". $option_r[1] ." EA / ". number_format($option_r[2]) ." 원<br>";
+														echo $option_r[0].$option_r[1] ."/ ". $option_r[2] ." EA / ". number_format($option_r[3]) ." 원<br>";
 													}
 												}
 											?>
