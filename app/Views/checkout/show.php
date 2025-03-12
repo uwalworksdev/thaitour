937567635e7791ec61248b6a09961573712d3725
diana tgkim <diana001@naver.com>
2025-03-12 15:06:23 +0900

    modified:   app/Views/checkout/show.php

--- app/Views/checkout/show.php
+++ app/Views/checkout/show.php
@@ -248,14 +248,14 @@
 								</div>
 								<div class="form-group">
 									<label for="order_passport_expiry_date">여권만기일 *</label>
-									<input type="text" id="order_passport_expiry_date" class="date_form hasDatepicker" name="order_passport_expiry_date" required="" data-label="여권만기일" placeholder="여권만기일" readonly="true">
+									<input type="text" id="order_passport_expiry_date" class="date_form" name="order_passport_expiry_date" required="" data-label="여권만기일" placeholder="여권만기일" readonly="true">
 								</div>
 					        </div>		
 							
                             <div class="con-form mb-40">
 								<div class="form-group">
 									<label for="order_birth_date">생년월일 *</label>
-									<input type="text" id="order_birth_date" class="date_form hasDatepicker" name="order_birth_date" required="" data-label="생년월일" placeholder="생년월일" readonly="true">
+									<input type="text" id="order_birth_date" class="date_form" name="order_birth_date" required="" data-label="생년월일" placeholder="생년월일" readonly="true">
 								</div>
 					        </div>							
                             <h3 class="title-sub-c">연락처</h3>
