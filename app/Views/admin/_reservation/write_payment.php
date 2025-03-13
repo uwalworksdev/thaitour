972bfb6551fc724043cfc823a0215a799049c17f
diana tgkim <diana001@naver.com>
2025-03-13 14:42:24 +0900

    modified:   app/Views/admin/_reservation/write_payment.php

--- app/Views/admin/_reservation/write_payment.php
+++ app/Views/admin/_reservation/write_payment.php
@@ -126,7 +126,7 @@
                                 <tr>
                                     <th>총 결제금액</th>
                                     <td>
-                                        <?=number_format($payment_row['payment_tot'])?>원 | 5,980 TH
+                                        <?=number_format($payment_row['payment_tot'])?>원 | <?=number_format($payment_row['payment_tot']/$order['baht_thai'])?> TH
                                     </td>
                                     <th>결제방법</th>
                                     <td>
