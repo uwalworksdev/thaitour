11998917943582d78353fd251dcfd6873ede329e
diana tgkim <diana001@naver.com>
2025-03-13 14:14:56 +0900

    modified:   app/Controllers/Product.php

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -5054,7 +5054,7 @@ class Product extends BaseController
             $data['local_phone'] = encryptField($data['local_phone'], 'encode');
 
             $this->orderModel->save($data);
-			write_log("last query(tourPaymentOk)- ". $this->connect->getLastQuery());
+			write_log("last query(tourPaymentOk)- ". $db->getLastQuery());
 
             $order_idx = $this->orderModel->getInsertID();
 
