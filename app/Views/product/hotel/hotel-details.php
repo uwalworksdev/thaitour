28c0cedc11a3ba30adfa864af314642181dfba46
diana tgkim <diana001@naver.com>
2025-03-15 21:47:27 +0900

    modified:   app/Views/product/hotel/hotel-details.php

--- app/Views/product/hotel/hotel-details.php
+++ app/Views/product/hotel/hotel-details.php
@@ -1538,10 +1538,10 @@
                                                 <?php } ?>
 												
 												<?php if($extra_won > 0) { ?>
-                                                <!--div class="wrap_check"-->
+                                                <div class="wrap_check">
 													<input type="checkbox" name="extra_" id="extra_<?=$room['g_idx']?>" data-name="Extra베드" data-won="<?=$extra_won?>" data-bath="<?=$extra_bath?>" value="<?=$room['rooms_idx']?>">
 													<label for="extra_<?=$room['g_idx']?>">Extra 베드:<span style="color :coral"><?=number_format($extra_won)?>원 (<?=number_format($extra_bath)?>바트)</span></label>
-                                                <!--/div-->
+                                                </div>
 												<?php } ?>
 													
                                             </div>
@@ -1569,6 +1569,14 @@
                                                 <?php
                                                     }
                                                 ?>
+												
+												<?php if($extra_won > 0) { ?>
+                                                <div class="wrap_check">
+													<input type="checkbox" name="extra_" id="extra_<?=$room['g_idx']?>" data-name="Extra베드" data-won="<?=$extra_won?>" data-bath="<?=$extra_bath?>" value="<?=$room['rooms_idx']?>">
+													<label for="extra_<?=$room['g_idx']?>">Extra 베드:<span style="color :coral"><?=number_format($extra_won)?>원 (<?=number_format($extra_bath)?>바트)</span></label>
+                                                </div>
+												<?php } ?>
+												
                                             </div>
                                         </div>
 										
