61a66a3aaa9398cb4c65db2b133cebcee27833e5
diana tgkim <diana001@naver.com>
2025-03-15 18:42:45 +0900

    modified:   app/Views/product/hotel/hotel-details.php

--- app/Views/product/hotel/hotel-details.php
+++ app/Views/product/hotel/hotel-details.php
@@ -1480,8 +1480,8 @@
 								       $basic_won  =  (int)($room['goods_price1'] * $room['baht_thai']);
 								       $basic_bath =  $room['goods_price1'];
 									   
-								       $price_won  =  (int)(($room['goods_price2'] + $room['goods_price3'] + $room['goods_price4']) * $room['baht_thai']);
-								       $price_bath =  $room['goods_price2'] + $room['goods_price3'] + $room['goods_price4'];
+								       $price_won  =  (int)(($room['goods_price2'] + $room['goods_price3']) * $room['baht_thai']);
+								       $price_bath =  $room['goods_price2'] + $room['goods_price3'];
 									   
 								?>
                                 <td>
@@ -1546,6 +1546,9 @@
                                                 ?>    
                                                     <div class="layer_promotion layer_promotion2">
                                                         <p style="white-space: pre-line"><?=$room['r_contents3']?></p>
+                                                    </div>   
+                                                    <div class="layer_promotion layer_promotion2">
+                                                        <p style="white-space: pre-line"><?=$room['r_contents4']?></p>
                                                     </div>   
                                                 <?php
                                                     }
