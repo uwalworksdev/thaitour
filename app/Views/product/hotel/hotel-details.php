cc51674d1c899c37531006c363609dbf7bdc1aa3
diana tgkim <diana001@naver.com>
2025-03-15 21:39:54 +0900

    modified:   app/Views/product/hotel/hotel-details.php

--- app/Views/product/hotel/hotel-details.php
+++ app/Views/product/hotel/hotel-details.php
@@ -1530,7 +1530,7 @@
                                                 <?php } ?>
 												
 												<?php if($extra_won > 0) { ?>
-                                                <div class="wrap_input">
+                                                <div class="wrap_inputx">
 													<input type="checkbox" name="extra_" id="extra_<?=$room['g_idx']?>" data-name="Extra베드" data-won="<?=$extra_won?>" data-bath="<?=$extra_bath?>" value="<?=$room['rooms_idx']?>">
 													<label for="extra_<?=$room['g_idx']?>">Extra 베드:<span style="color :coral"><?=number_format($extra_won)?>원 (<?=number_format($extra_bath)?>바트)</span></label>
                                                 </div>
