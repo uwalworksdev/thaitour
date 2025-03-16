63eadd07fd14fd9916ad686f07ce28b74b340ba5
diana tgkim <diana001@naver.com>
2025-03-16 12:20:22 +0900

    modified:   app/Views/admin/_tourRegist/list_spas.php

--- app/Views/admin/_tourRegist/list_spas.php
+++ app/Views/admin/_tourRegist/list_spas.php
@@ -323,6 +323,14 @@
                             <button type="button" class="btn_filter" onclick="orderBy_set('3');"><img
                                         src="/images/admin/common/filter.png" alt="">예약순
                             </button>
+							
+							<select id="g_list_rows" name="g_list_rows" class="input_select" style="width: 80px" onchange="submitForm();">
+								<option value="30"  <?= ($g_list_rows == 30)  ? 'selected' : '' ?>>30개</option>
+								<option value="50"  <?= ($g_list_rows == 50)  ? 'selected' : '' ?>>50개</option>
+								<option value="100" <?= ($g_list_rows == 100) ? 'selected' : '' ?>>100개</option>
+								<option value="200" <?= ($g_list_rows == 200) ? 'selected' : '' ?>>200개</option>
+							</select>
+							
                         </div>
 
                     </div><!-- // listTop -->
@@ -511,6 +519,12 @@
 
         </div><!-- 인쇄 영역 끝 //-->
     </div><!-- // container -->
+
+	<script>
+		function submitForm() {
+			document.getElementById("frm").submit();
+		}
+	</script>
 
     <script>
         function check_best(idx) {
