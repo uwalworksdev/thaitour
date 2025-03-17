2b5c16fe68452b73a6043afc22c208cf245b76b0
diana tgkim <diana001@naver.com>
2025-03-17 13:50:17 +0900

    modified:   app/Views/admin/_reservation/write_payment.php

--- app/Views/admin/_reservation/write_payment.php
+++ app/Views/admin/_reservation/write_payment.php
@@ -32,7 +32,7 @@
 
             <header id="headerContainer">
                 <div class="inner">
-                    <h2><?= $titleStr ?></h2>
+                    <h2>결제정보</h2>
                     <div class="menus">
                         <ul>
                             <li>
