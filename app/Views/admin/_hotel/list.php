e70dcc0c478e64d7614c46a5871eb81508dce2c9
diana tgkim <diana001@naver.com>
2025-03-17 11:12:53 +0900

    modified:   app/Views/admin/_hotel/list.php

--- app/Views/admin/_hotel/list.php
+++ app/Views/admin/_hotel/list.php
@@ -492,7 +492,7 @@
                         </table>
                     </div><!-- // listBottom -->
 
-                <?= ipageListing($pg, $nPage, $g_list_rows, site_url('/AdmMaster/_hotel/list') . "?g_list_rows=$g_list_rows&search_name=$search_name&pg=") ?>
+                <?= ipageListing($pg, $nPage, $g_list_rows, site_url('/AdmMaster/_hotel/list') . "?product_code_1=$product_code_1&s_status=$s_status&search_category=$search_category&g_list_rows=$g_list_rows&search_name=$search_name&pg=" . $arrays_paging) ?>
 
                 <div id="headerContainer">
 
