5833ce2f5c63c15c5b81e6556346105ec9aced97
diana tgkim <diana001@naver.com>
2025-03-19 08:59:36 +0900

    modified:   app/Common/roomPrice.php

--- app/Common/roomPrice.php
+++ app/Common/roomPrice.php
@@ -27,6 +27,7 @@ function roomPrice($db, $rooms_idx, $baht_thai, $product_idx, $g_idx, $o_sdate, 
                                                 rooms_idx   = ? AND 
                                                 bed_idx     = ? AND 
                                                 goods_date  = ?";
+		write_log($sql);										
         $query = $db->query($sql, [$product_idx, $g_idx, $rooms_idx, $row['bed_idx'], $o_sdate]);
         $priceRow = $query->getRow();
 
