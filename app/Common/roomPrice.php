5b0dd5448bf58f7d851069fd96bd13fb3d755d89
diana tgkim <diana001@naver.com>
2025-03-19 04:21:28 +0900

    modified:   app/Common/roomPrice.php

--- app/Common/roomPrice.php
+++ app/Common/roomPrice.php
@@ -1,51 +1,50 @@
 <?php
 
-// /app/Common/insertRoomPrice.php
-
 use CodeIgniter\Database\QueryBuilder;
 use DateTime;
 
 function roomPrice($db, $rooms_idx, $baht_thai, $product_idx, $g_idx, $o_sdate, $days)
 {
-	
-	if (!$db) {
-		$db = \Config\Database::connect();
-	}	
+    if (!$db) {
+        $db = \Config\Database::connect();
+    }
 
-	// 방 정보 가져오기
-	$bed_type = $bed_price = "";
-    $sql   = "SELECT * FROM tbl_room_beds WHERE rooms_idx = ? ORDER BY bed_seq";
+    // 방 정보 가져오기
+    $bed_type = "";
+    $bed_price = "";
+    $result = "";
+
+    $sql = "SELECT * FROM tbl_room_beds WHERE rooms_idx = ? ORDER BY bed_seq";
     $query = $db->query($sql, [$rooms_idx]);
     $rows  = $query->getResultArray(); // 연관 배열 반환
 
     foreach ($rows as $row) {
-		
-		     if($bed_type == "") {
-                $bed_type .= $row['bed_type'];
-			 } else {
-                $bed_type .= ",". $row['bed_type']; 
-			 }	
+        // bed_type 문자열 조합
+        $bed_type .= ($bed_type === "") ? $row['bed_type'] : "," . $row['bed_type'];
 
-			 $sql     = "SELECT * FROM tbl_room_price WHERE product_idx = '". $product_idx ."'   AND 
-			                                                g_idx       = '". $g_idx ."'         AND 
-															rooms_idx   = '". $rooms_idx ."'     AND 
-															bed_idx     = '". $row['bed_idx'] ."'  AND 
-															goods_date  = '". $o_sdate ."' ";
-		 	 write_log("roomPrice- ". $sql); 												
-             $row        = $db->query($sql)->getRow();
-			 $price_won  = ($row->goods_price1 + $row->goods_price2 + $row->goods_price3) * $row->baht_thai; 
-			 $price_baht =  $row->goods_price1 + $row->goods_price2 + $row->goods_price3;
+        $sql = "SELECT * FROM tbl_room_price WHERE product_idx = ? AND 
+                                                g_idx       = ? AND 
+                                                rooms_idx   = ? AND 
+                                                bed_idx     = ? AND 
+                                                goods_date  = ?";
+        $query = $db->query($sql, [$product_idx, $g_idx, $rooms_idx, $row['bed_idx'], $o_sdate]);
+        $priceRow = $query->getRow();
 
-			 if($bed_price == "") {
-                $bed_price .= $price_won;
-			 } else {
-                $bed_price .= ",". $price_won;
-			 }	
-			 
-			 $result = $bed_type ."|". $bed_price ."|". $row->goods_price1;
+        if ($priceRow) {
+            $price_won  = ($priceRow->goods_price1 + $priceRow->goods_price2 + $priceRow->goods_price3) * $priceRow->baht_thai;
+            $price_baht =  $priceRow->goods_price1 + $priceRow->goods_price2 + $priceRow->goods_price3;
+            $goods_price1 = $priceRow->goods_price1;
+        } else {
+            $price_won  = 0;
+            $price_baht = 0;
+            $goods_price1 = 0;
+        }
 
+        // bed_price 문자열 조합
+        $bed_price .= ($bed_price === "") ? $price_won : "," . $price_won;
     }
 
+    $result = $bed_type . "|" . $bed_price . "|" . $goods_price1;
     return $result; // 성공적으로 처리된 경우
 }
 
