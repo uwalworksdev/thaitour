64b71d81815fdd9ec5d666389b25ed8a19cb8604
diana tgkim <diana001@naver.com>
2025-03-19 23:22:05 +0900

    modified:   app/Views/admin/_tourRegist/list_room_price.php

--- app/Views/admin/_tourRegist/list_room_price.php
+++ app/Views/admin/_tourRegist/list_room_price.php
@@ -189,7 +189,7 @@
 									<col width="8%">
 									<col width="8%">
 									<col width="8%">
-									<col width="6%">
+									<col width="4%">
 									<col width="10%">
 									<col width="10%">
 									<col width="8%">
