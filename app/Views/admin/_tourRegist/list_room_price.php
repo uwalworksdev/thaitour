fb15fc8496809a78ad5788e2828eedf4f47e72ef
diana tgkim <diana001@naver.com>
2025-03-19 23:32:12 +0900

    modified:   app/Views/admin/_tourRegist/list_room_price.php

--- app/Views/admin/_tourRegist/list_room_price.php
+++ app/Views/admin/_tourRegist/list_room_price.php
@@ -177,7 +177,7 @@
                             </tbody>
                         </table>
                     </div>
-                    <p>수정되는 것은 자동으로 체크됩니다. 마감, 지난 날짜, 수정된 것은 체크됩니다. <span style="color:red;">체크를 풀고 저장하면, 전체 저장 시 수정됩니다.</span></p>
+                    <p>&#9758; 수정되는 것은 자동으로 체크됩니다. 마감, 지난 날짜, 수정된 것은 체크됩니다. <span style="color:red;">체크를 풀고 저장하면, 전체 저장 시 수정됩니다.</span></p>
                     <div class="listBottom">
          				<table cellpadding="0" cellspacing="0" summary="" class="listTable mem_detail">
 									<colgroup>
