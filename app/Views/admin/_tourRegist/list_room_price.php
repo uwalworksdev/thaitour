ef76449fc8426a1e35c7df0032b8f931b5b0c636
diana tgkim <diana001@naver.com>
2025-03-20 10:40:14 +0900

    modified:   app/Views/admin/_tourRegist/list_room_price.php

--- app/Views/admin/_tourRegist/list_room_price.php
+++ app/Views/admin/_tourRegist/list_room_price.php
@@ -284,12 +284,12 @@
 													</td>
 													<td style="text-align:center"><?=$item['bed_type']?></td>
 													
-													<?php //if($com_date != $item['goods_date']) { ?>
-													<?php //$com_date = $item['goods_date']?>
+													<?php if($com_date != $item['goods_date']) { ?>
+													<?php $com_date = $item['goods_date']?>
 													<td style="text-align:center"><?=$item['goods_date']?> [<?=$item['dow']?>]</td>
-													<?php //} else { ?>
-													<!--td></td-->	
-													<?php //} ?>
+													<?php } else { ?>
+													<td></td>	
+													<?php } ?>
 													<td style="text-align:center">
 														<input type="hidden" name="idx[]" id="idx" value="<?=$item['idx']?>">
 														<input type="hidden" name="goods_date[]" id="goods_date_<?=$item['idx']?>" value="<?=$item['goods_date']?>">
