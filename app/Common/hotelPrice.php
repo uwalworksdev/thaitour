a328771af983c3caf1dc2b0847a00a3d7e8894c7
diana tgkim <diana001@naver.com>
2025-03-21 23:46:52 +0900

    modified:   app/Common/hotelPrice.php

--- app/Common/hotelPrice.php
+++ app/Common/hotelPrice.php
@@ -12,11 +12,19 @@ function mainPrice($db, $rooms_idx, $baht_thai, $product_idx, $g_idx, $o_sdate, 
         $o_sdate   = date('Y-m-d', strtotime('+1 day'));
         $o_sdate   = date('Y-m-d');
 
+        $sql = "SELECT *
+				FROM  tbl_room_price
+				WHERE product_idx   = ?
+				AND   g_idx         = ?
+				AND   rooms_idx     = ?
+				AND   goods_date    = ?
+				ORDER BY (goods_price2 + goods_price3) ASC
+				LIMIT 1 ";
+
         $sql = "SELECT * FROM tbl_room_price WHERE product_idx = ? AND 
                                                    g_idx       = ? AND 
                                                    rooms_idx   = ? AND 
                                                    goods_date  = ?";
-        $query    = $db->query($sql, [$product_idx, $g_idx, $rooms_idx, $o_sdate]);
         $priceRow = $query->getRow();
 
         if ($priceRow) {
