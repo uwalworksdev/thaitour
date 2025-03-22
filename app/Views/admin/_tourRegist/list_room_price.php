2199111f488f41ad1a8b7afd5a573ea46590d8c1
diana tgkim <diana001@naver.com>
2025-03-22 12:27:28 +0900

    modified:   app/Views/admin/_tourRegist/list_room_price.php

--- app/Views/admin/_tourRegist/list_room_price.php
+++ app/Views/admin/_tourRegist/list_room_price.php
@@ -331,8 +331,12 @@
         // 화요일 체크
         $(".priceDow").click(function () {
             let day = $(this).val();
-            $(`.upd_chk.${day}`).prop("checked", true);  // "화" 체크박스 체크
-        });
+			if ($(this).prop("checked")) {  // 체크박스가 체크된 경우
+				$(`.upd_chk.${day}`).prop("checked", true);  // 해당 요일 체크박스를 체크
+			} else {  // 체크박스가 해제된 경우
+				$(`.upd_chk.${day}`).prop("checked", false);  // 해당 요일 체크박스를 해제
+			}
+		});
 
     });
 </script>
