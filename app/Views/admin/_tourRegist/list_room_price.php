ec5153b77c9be5d7c3966846a4c9ee76d3832d4f
diana tgkim <diana001@naver.com>
2025-03-22 13:01:41 +0900

    modified:   app/Views/admin/_tourRegist/list_room_price.php

--- app/Views/admin/_tourRegist/list_room_price.php
+++ app/Views/admin/_tourRegist/list_room_price.php
@@ -197,7 +197,7 @@
                     <p><span style="font-weight: bold; color: red;">※</span> 수정되는 것은 자동으로 체크됩니다. 마감, 지난 날짜, 수정된 것도 체크됩니다. <span style="color:red;">체크를 풀고 저장하면, 전체 저장 시 수정됩니다.</span>
 					<!--button type="button" class="allUpdate" >일괄수정</button-->
 					
-						<select id="g_list_rows" name="list_rows" id="list_rows" class="input_select" style="width: 80px" onchange="submitForm();">
+						<select id="list_rows" name="list_rows" id="list_rows" class="input_select" style="width: 80px" onchange="submitForm();">
 							<option value="30"  <?= ($g_list_rows == 30)  ? 'selected' : '' ?>>30개</option>
 							<option value="50"  <?= ($g_list_rows == 50)  ? 'selected' : '' ?>>50개</option>
 							<option value="100" <?= ($g_list_rows == 100) ? 'selected' : '' ?>>100개</option>
