14a2e211a7194953091fea6081e353545d2a2787
diana tgkim <diana001@naver.com>
2025-03-23 18:31:27 +0900

    modified:   app/Controllers/AjaxController.php

--- app/Controllers/AjaxController.php
+++ app/Controllers/AjaxController.php
@@ -1021,7 +1021,7 @@ class AjaxController extends BaseController {
 													 if($_room[5] > 0) {
 														  $extra_won  = (int)($_room[5] * $baht_thai);
 														  $extra_bath = $_room[5];	  
-												    	  $msg .= '<div class="wrap_check extra" id="chk_'. $room['g_idx'].$room['rooms_idx'].$bed_idx .'">';
+												    	  $msg .= '<div class="wrap_check extra" id="chk_'. $room['g_idx'].$room['rooms_idx'].$bed_idx .'" style="display:none;">';
 													      $msg .= '<input type="checkbox" name="extra_" id="extra_'. $room['g_idx'].$room['rooms_idx'].$bed_idx .'" 
 																    data-name="Extra베드" data-won="'. $extra_won .'" data-bath="'. $extra_bath .'" value="'. $room['rooms_idx'] .'" >';
 													      $msg .= '<label for="extra_'. $room['g_idx'].$room['rooms_idx'].$bed_idx .'" >Extra 베드: <span style="color :coral">'. number_format($extra_won) .'원 ('.  number_format($extra_bath) .'바트)</span></label>';
