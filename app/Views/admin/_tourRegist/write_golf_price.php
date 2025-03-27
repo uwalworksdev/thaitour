c3ed4386c03a290e92a1837d6b299b43306e3e8b
diana tgkim <diana001@naver.com>
2025-03-28 01:50:02 +0900

    modified:   app/Views/admin/_tourRegist/write_golf_price.php

--- app/Views/admin/_tourRegist/write_golf_price.php
+++ app/Views/admin/_tourRegist/write_golf_price.php
@@ -29,7 +29,7 @@
     <div id="print_this"><!-- 인쇄영역 시작 //-->
         <header id="headerContainer">
             <div class="inner">
-                <h2>골프 상품관리 정보입력</h2>
+                <h2>골프 상품관리 정보입력[<?=$product_name?>]</h2>
                 <div class="menus">
                     <ul>
                         <li>
