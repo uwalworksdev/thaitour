5c15aee4da0b8206c9e2d5a701fdc6fd8326e40c
diana tgkim <diana001@naver.com>
2025-03-27 21:52:18 +0900

    modified:   app/Views/admin/_tourRegist/write_golf_price.php

--- app/Views/admin/_tourRegist/write_golf_price.php
+++ app/Views/admin/_tourRegist/write_golf_price.php
@@ -181,7 +181,7 @@
                                                         value="<?= $frow3['o_golf'] ?>" size="70" class="hole_cnt"/>
                                                 <input type='hidden' name='o_name[]' id=''
                                                         value="<?= $frow3['goods_name'] ?>" size="70"/>
-                                                <td rowspan="3" style="text-align:center;">
+                                                <td rowspan="4" style="text-align:center;">
                                                     <?= $frow3['goods_name'] ?>
                                                 </td>
                                                 <td>
