7bdadaf2c31911b1b4469f8c20c91ae10a94010c
diana tgkim <diana001@naver.com>
2025-03-28 08:17:20 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -492,7 +492,7 @@ $links = "list";
 														<td>
 															<p style="margin-bottom: 3px;">침대타입추가 (침대타입의 가격은 추가되는 금액만 넣습니다. (침대명/금액))
 															   <button type="button" onclick="InitTypePopup(this, 3)" style="width: 50px; background-color: #4f728a; color : #fff;" class="btn_set">설명</button>
-															   <button type="button" class="bedAddBtn" data-product-idx="<?=$product_idx?>" data-g-idx="<?=$type['g_idx']?>" data-rooms-idx="<?=$row['rooms_idx']?>" style="width: 50px; background-color: #4f728a; color : #fff;" >침대타입추가</button>
+															   <button type="button" class="bedAddBtn" data-product-idx="<?=$product_idx?>" data-g-idx="<?=$type['g_idx']?>" data-rooms-idx="<?=$row['rooms_idx']?>" style="width: 100px; background-color: #4f728a; color : #fff;" >침대타입추가</button>
 															</p>
                                                         </td>
 													</tr>													
