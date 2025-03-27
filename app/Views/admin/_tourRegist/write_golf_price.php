b1ec6a9ee6b6cf4aea2eafab81e580d2a95fc2b5
diana tgkim <diana001@naver.com>
2025-03-27 22:09:31 +0900

    modified:   app/Views/admin/_tourRegist/write_golf_price.php

--- app/Views/admin/_tourRegist/write_golf_price.php
+++ app/Views/admin/_tourRegist/write_golf_price.php
@@ -283,14 +283,14 @@
                                             
                                             <tr color='<?= $_tmp_color ?>' size='<?= $frow2['type'] ?>'>
                                                 <td colspan="8">왕복(단위: 바트)&nbsp;
-                                                    승용차:      <input type='text' name='vehicle_price1[]' style="width:5%;text-align:right;" value='<?= $frow3['vehicle_price1'] ?>'/>&nbsp;&nbsp; 
-                                                    밴 (승합차): <input type='text' name='vehicle_price2[]' style="width:5%;text-align:right;" value='<?= $frow3['vehicle_price2'] ?>'/>&nbsp;&nbsp; 
-                                                    SUV:        <input type='text' name='vehicle_price3[]' style="width:5%;text-align:right;" value='<?= $frow3['vehicle_price3'] ?>'/>&nbsp;&nbsp;&nbsp; 
+                                                    승용차:      <input type='text' name='vehicle_price1[]' style="width:7%;text-align:right;" value='<?= $frow3['vehicle_price1'] ?>'/>&nbsp;&nbsp; 
+                                                    밴 (승합차): <input type='text' name='vehicle_price2[]' style="width:7%;text-align:right;" value='<?= $frow3['vehicle_price2'] ?>'/>&nbsp;&nbsp; 
+                                                    SUV:        <input type='text' name='vehicle_price3[]' style="width:7%;text-align:right;" value='<?= $frow3['vehicle_price3'] ?>'/>&nbsp;&nbsp;&nbsp; 
                                                     
 													편도(단위: 바트)&nbsp;
-                                                    승용차:      <input type='text' name='vehicle_o_price1[]' style="width:5%;text-align:right;" value='<?= $frow3['vehicle_o_price1'] ?>'/>&nbsp;&nbsp; 
-                                                    밴 (승합차): <input type='text' name='vehicle_o_price2[]' style="width:5%;text-align:right;" value='<?= $frow3['vehicle_o_price2'] ?>'/>&nbsp;&nbsp; 
-                                                    SUV:        <input type='text' name='vehicle_o_price3[]' style="width:5%;text-align:right;" value='<?= $frow3['vehicle_o_price3'] ?>'/>&nbsp;&nbsp;&nbsp; 
+                                                    승용차:      <input type='text' name='vehicle_o_price1[]' style="width:7%;text-align:right;" value='<?= $frow3['vehicle_o_price1'] ?>'/>&nbsp;&nbsp; 
+                                                    밴 (승합차): <input type='text' name='vehicle_o_price2[]' style="width:7%;text-align:right;" value='<?= $frow3['vehicle_o_price2'] ?>'/>&nbsp;&nbsp; 
+                                                    SUV:        <input type='text' name='vehicle_o_price3[]' style="width:7%;text-align:right;" value='<?= $frow3['vehicle_o_price3'] ?>'/>&nbsp;&nbsp;&nbsp; 
                                                 </td>
                                             </tr>
                                             
