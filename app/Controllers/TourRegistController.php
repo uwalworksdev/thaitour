26704fe24642233d67b085141b30551c877f3e18
diana tgkim <diana001@naver.com>
2025-03-27 16:20:54 +0900

    modified:   app/Controllers/TourRegistController.php

--- app/Controllers/TourRegistController.php
+++ app/Controllers/TourRegistController.php
@@ -734,11 +734,26 @@ class TourRegistController extends BaseController
         $query = $db->query($sql);
         $product = $query->getRowArray();
 
+        $filters = $this->codeModel->getByParentAndDepth(45, 2)->getResultArray();
+
+        foreach ($filters as $key => $filter) {
+            $filters[$key]['children'] = $this->codeModel->getByParentAndDepth($filter['code_no'], 3)->getResultArray();
+            if ($filter['code_no'] == 4501) $filters[$key]['filter_name'] = "green_peas";
+            if ($filter['code_no'] == 4502) $filters[$key]['filter_name'] = "sports_days";
+            if ($filter['code_no'] == 4503) $filters[$key]['filter_name'] = "slots";
+            if ($filter['code_no'] == 4504) $filters[$key]['filter_name'] = "golf_course_odd_numbers";
+            if ($filter['code_no'] == 4505) $filters[$key]['filter_name'] = "travel_times";
+            if ($filter['code_no'] == 4506) $filters[$key]['filter_name'] = "carts";
+            if ($filter['code_no'] == 4507) $filters[$key]['filter_name'] = "facilities";
+        }
+		
         $new_data = [
             'product_idx' => $product_idx,
             'product'     => $product,
-            'options'     => $options
+            'options'     => $options,
+			'filters'     => $filters
         ];
+
 
         $data = array_merge($data, $new_data);
 
