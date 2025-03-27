538cc3ea05635d6841eb662ebd07c5bdc4cdf71f
diana tgkim <diana001@naver.com>
2025-03-27 22:41:59 +0900

    modified:   app/Views/admin/_tourRegist/write_golf_price.php

--- app/Views/admin/_tourRegist/write_golf_price.php
+++ app/Views/admin/_tourRegist/write_golf_price.php
@@ -302,7 +302,7 @@
                                             </tr>
                                             
                                             <tr color='<?= $_tmp_color ?>' size='<?= $frow2['type'] ?>'>
-                                                <td colspan="8"><span class="badge"><img src="https://cdn-icons-png.flaticon.com/512/9539/9539523.png" class="badge-icon" alt="icon">왕복(단위: 바트)</span>&nbsp;
+                                                <td colspan="8"><span class="badge">왕복(단위: 바트)</span>&nbsp;
                                                     승용차:      <input type='text' name='vehicle_price1[]' style="width:7%;text-align:right;" value='<?= $frow3['vehicle_price1'] ?>'/>&nbsp;&nbsp; 
                                                     밴 (승합차): <input type='text' name='vehicle_price2[]' style="width:7%;text-align:right;" value='<?= $frow3['vehicle_price2'] ?>'/>&nbsp;&nbsp; 
                                                     SUV:        <input type='text' name='vehicle_price3[]' style="width:7%;text-align:right;" value='<?= $frow3['vehicle_price3'] ?>'/>&nbsp;&nbsp;&nbsp; 
