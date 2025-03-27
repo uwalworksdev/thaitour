1d6d83135e6b160959cfec66dd589d8d5eb9c94b
diana tgkim <diana001@naver.com>
2025-03-27 16:25:19 +0900

    modified:   app/Views/admin/_tourRegist/write_golf.php

--- app/Views/admin/_tourRegist/write_golf.php
+++ app/Views/admin/_tourRegist/write_golf.php
@@ -906,7 +906,6 @@
                                     border: 1px solid rgba(255, 0, 0, 0.8);
                                 }
                             </style>
-							<!--
                             <table cellpadding="0" cellspacing="0" summary="" class="listTable mem_detail"
                                    style="margin-top:50px;">
                                 <caption>
@@ -962,7 +961,7 @@
                                                         </div>
                                                     <?php } ?>
                                                 <?php } ?>
-                                            </div> 
+                                            </div> -->
                                             <?php foreach ($filter['children'] as $item) { ?>
                                                 <input type="checkbox" class="code_<?= $filter['filter_name'] ?>" id="<?= $filter['filter_name'] ?>_<?= $item['code_no'] ?>" name="<?= $filter['filter_name'] ?>[]"
                                                         value="<?= $item['code_no'] ?>" <?php if (in_array($item['code_no'], $filter_arr)) { echo "checked"; } ?> <?php if($filter['filter_name'] == "golf_course_odd_numbers" || $filter['filter_name'] == "green_peas") echo "disabled";?> />
@@ -974,7 +973,7 @@
                                     </tr>
                                 <?php } ?>
                                 </tbody>
-                            </table-->
+                            </table>
                         </div>
                     </div>
 
