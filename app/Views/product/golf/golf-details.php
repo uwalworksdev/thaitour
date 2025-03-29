eff59198b11bc7a3b0eceb7bf238de07372731ef
diana tgkim <diana001@naver.com>
2025-03-29 18:34:14 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -80,6 +80,12 @@ $(document).ready(function() {
                 <input type="hidden" name="teeoff_min" id="teeoff_min" value="">
 
 
+                <?php foreach ($golf_price as $price) { ?>
+                    <input type="hidden" id="firstDate"  value="<?= $price['goods_date'] ?>">
+                    <input type="hidden" id="firstPrice" value="<?= $price['price'] ?>">
+                    <input type="hidden" name="afternoon_yn" id="afternoon_yn" value="<?= $price['o_afternoon_yn'] ?>">
+                    <input type="hidden" name="night_yn" id="night_yn" value="<?= $price['o_night_yn'] ?>">
+                <?php } ?>
 
                 <div class="title-container">
                     <h2><?= viewSQ($product['product_name']) ?> <span style="margin-left: 15px;"><?= viewSQ($product['product_name_en']) ?></span></h2>
