6ca6d2c0cc0607ea87674d897604171f634a4dc2
diana tgkim <diana001@naver.com>
2025-03-30 14:23:31 +0900

    modified:   app/Controllers/AjaxController.php

--- app/Controllers/AjaxController.php
+++ app/Controllers/AjaxController.php
@@ -94,6 +94,7 @@ class AjaxController extends BaseController {
         $baht_thai    = (float)($setting['baht_thai'] ?? 0);
 
         $sql  = "SELECT * FROM tbl_golf_option WHERE product_idx = '$product_idx' AND goods_name = '$goods_name' ";
+		write_log("get_golf_option- ". $sql);
         $rows = $db->query($sql)->getResultArray();
 
 		foreach ($rows as $row) {
