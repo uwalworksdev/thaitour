a2a137856641e971a3026b5aba536c4eb79897a6
diana tgkim <diana001@naver.com>
2025-04-01 10:53:07 +0900

    modified:   app/Views/mypage/order_view_item_tour.php

--- app/Views/mypage/order_view_item_tour.php
+++ app/Views/mypage/order_view_item_tour.php
@@ -102,8 +102,8 @@ $deli_types = get_deli_type();
 						</td>
 						<td class="content">
 							<p>
-							<span>성인: <?= $people_adult_cnt ?></span>명<br>  
-							<span>아동: <?= $people_kids_cnt ?></span>명<br>  
+							<span>성인: <?= $people_adult_cnt ?></span>명, 
+							<span>아동: <?= $people_kids_cnt ?></span>명,  
 							<span>유아: <?= $people_baby_cnt?></span>명  
 							</p>
 						</td>
