672706b3ccc07915ec41354e105b987057021b70
diana tgkim <diana001@naver.com>
2025-04-01 12:52:38 +0900

    modified:   app/Views/mypage/order_view_item_spa.php

--- app/Views/mypage/order_view_item_spa.php
+++ app/Views/mypage/order_view_item_spa.php
@@ -77,7 +77,7 @@ $deli_types = get_deli_type();
 						<td class="subject">예약번호</td>
 						<td class="subject">예약일자</td>
 						<td class="subject">예약인원</td>
-						<td class="subject">투어일자</td>
+						<td class="subject">스차일자</td>
 						<td class="subject">성인</td>
 						<td class="subject">아동</td>
 					</tr>
