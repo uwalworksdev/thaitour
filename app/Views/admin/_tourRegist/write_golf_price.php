b14239d86797e927b0416a30524a071273e4b482
diana tgkim <diana001@naver.com>
2025-04-04 00:36:35 +0900

    modified:   app/Views/admin/_tourRegist/write_golf_price.php

--- app/Views/admin/_tourRegist/write_golf_price.php
+++ app/Views/admin/_tourRegist/write_golf_price.php
@@ -159,9 +159,7 @@
                         </tr>
                         <th>
                             홀등록
-                            <p style="display:block;margin-top:10px;">
                                 <button type="button" id="btn_add_prod" class="btn_01">추가</button>
-                            </p>
                         </th>
 
                         <td>
