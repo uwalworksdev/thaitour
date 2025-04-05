dce9507296f9e0e4070d9e524c80f6c0af9108c4
diana tgkim <diana001@naver.com>
2025-04-06 04:25:47 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -20,7 +20,7 @@ $(document).ready(function() {
 $(document).ready(function() {
     // 페이지 어디든 클릭 시 실행
     $(document).on('click', function(event) {
-
+alert('xxxxxxxxxx');
 			if($("#o_caddy_due").val() == "Y") {
 			   $('#vehicle_5').val($("#people_adult_cnt").val()).prop('disabled', true);
 			   $("#caddy_yes").show();	
