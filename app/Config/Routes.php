be185971a8db7d96e836d78f6d49aac1058321f5
diana tgkim <diana001@naver.com>
2025-04-07 00:40:25 +0900

    modified:   app/Config/Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -621,7 +621,7 @@ $routes->group("ajax", static function ($routes) {
 	$routes->post("ajax_golfDay_update", "AjaxController::ajax_golfDay_update");
 	$routes->post("ajax_golfOpt_ranks", "AjaxController::ajax_golfOpt_ranks");
 	$routes->post("ajax_golf_upd_y", "AjaxController::ajax_golf_upd_y");
-	
+	$routes->post("ajax_golf_end", "AjaxController::ajax_golf_end");
 });
 
 $routes->group("api", static function ($routes) {
