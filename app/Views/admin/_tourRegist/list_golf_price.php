3daeb2903093c5f9f4d33e1bd2b002b550f68542
diana tgkim <diana001@naver.com>
2025-04-08 09:18:10 +0900

    modified:   app/Views/admin/_tourRegist/list_golf_price.php

--- app/Views/admin/_tourRegist/list_golf_price.php
+++ app/Views/admin/_tourRegist/list_golf_price.php
@@ -323,7 +323,10 @@
 													<td style="text-align:center;"><?=$item['reg_date']?></td> 
 													<td style="text-align:center;"><?=$item['upd_date']?></td> 
 													<td style="text-align:center;">
+													    <?php if($item['upd_yn'] != "Y" || $item['use_yn'] != "N") { ?>
+														<?php } else { ?>
 														<button type="button" class="chargeUpdate" value="<?=$item['idx']?>">수정</button>
+														<?php } ?>
 														<!--button type="button" class="chargeDelete" value="<?=$item['idx']?>">삭제</button-->
 													</td> 
 												</tr>
