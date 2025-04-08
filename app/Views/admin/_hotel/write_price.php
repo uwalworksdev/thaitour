d866402c82cbcefcb8946ccdde508703f96c0a9b
diana tgkim <diana001@naver.com>
2025-04-08 18:26:05 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -239,6 +239,7 @@ $links = "list";
 				<h2>호텔가격 상세정보</h2>
 				<div class="menus">
 					<ul>
+     					<li><a href="/product-hotel/hotel-detail/<?= $product_idx?>" class="btn btn-default"><span class="glyphicon glyphicon-th-list"></span><span class="txt">상품상세</span></a></li>
 						<li><a href="/AdmMaster/_hotel/list" class="btn btn-default"><span class="glyphicon glyphicon-th-list"></span><span class="txt">리스트</span></a></li>
 						<!--li><a href="javascript:send_it_price()" class="btn btn-default"><span class="glyphicon glyphicon-cog"></span><span class="txt">등록</span></a></li-->
 					</ul>
