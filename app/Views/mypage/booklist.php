48426bd21084df0a7b4751525a96a6ac3f949ce5
diana tgkim <diana001@naver.com>
2025-04-09 23:11:30 +0900

    modified:   app/Views/mypage/booklist.php

--- app/Views/mypage/booklist.php
+++ app/Views/mypage/booklist.php
@@ -591,7 +591,7 @@ endforeach;
 </form>
 -->
 
-<form id="checkOut" action="/checkout/payment" method="post">
+<form id="checkOut" action="/checkout/confirm" method="post">
 <input type="text" name="payment_no" id="payment_no" value="" >
 <input type="text" name="dataValue" id="dataValue" value="" >
 </form>
