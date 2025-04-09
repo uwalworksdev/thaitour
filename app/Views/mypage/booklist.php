705e26445e12ee65ac325f7115d08c7118d2e9d7
diana tgkim <diana001@naver.com>
2025-04-09 22:43:19 +0900

    modified:   app/Views/mypage/booklist.php

--- app/Views/mypage/booklist.php
+++ app/Views/mypage/booklist.php
@@ -154,6 +154,11 @@ endforeach;
                     </div>
                 </div>
 
+
+<form id="checkOut" action="/checkout/payment" method="post">
+<input type="text" name="dataValue" id="dataValue" value="" >
+</form>
+
                 <form name="search" id="search">
                     <input type="hidden" name="s_status" value="">
                     <input type="hidden" name="pg" value="">
@@ -583,11 +588,6 @@ endforeach;
     </div>
     <div class="dim" style="justify-content: space-between;"></div>
 </div>
-
-
-<form id="checkOut" action="/checkout/payment" method="post">
-<input type="hidden" name="dataValue" id="dataValue" value="" >
-</form>
 
 <!--
 <form id="checkOut" action="/checkout/confirm" method="post">
