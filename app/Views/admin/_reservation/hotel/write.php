03a17efe24eea77355b711377a424fa590d813c7
diana tgkim <diana001@naver.com>
2025-04-16 16:36:08 +0900

    modified:   app/Views/admin/_reservation/hotel/write.php

--- app/Views/admin/_reservation/hotel/write.php
+++ app/Views/admin/_reservation/hotel/write.php
@@ -562,7 +562,7 @@
 
 		// .exp_amt 클래스의 input 값이 변경될 때
 		$("#real_price_bath").on('input', function () {
-			alert('1111111111');
+			
 			// 현재 입력된 baht 필드의 ID에서 인덱스 추출
 			// 입력된 값 가져오기
 			var bath = parseFloat($("#real_price_bath").val().replace(/,/g, '')) || 0;
@@ -571,7 +571,7 @@
 			var won = Math.round(bath * baht_thai);
 
 			// 해당 인덱스의 원화 input에 값 넣기
-			$("#real_price_bath").val(won.toLocaleString());
+			$("#real_price_won").val(won.toLocaleString());
 		});
 	});
 	</script>
