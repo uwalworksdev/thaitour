dcaabf9e732b28eba5e87967584040b0361a5afc
diana tgkim <diana001@naver.com>
2025-04-17 01:17:01 +0900

    modified:   app/Views/mypage/booklist.php

--- app/Views/mypage/booklist.php
+++ app/Views/mypage/booklist.php
@@ -363,7 +363,7 @@ endforeach;
 									       if($order['order_status'] == "X" || $order['order_status'] == "G") {
 										     echo '<input type="checkbox" 
 											              data-idx="'. $order['order_no'] .'" 
-														  data-price="'. $order['order_price'] .'" 
+														  data-price="'. $order['real_price_won'] .'" 
 														  id="prod'.esc($order['order_idx']).'" 
 														  class="pay sub'.esc($group['group_no']).'" 
 														  value="Y">';
@@ -396,7 +396,7 @@ endforeach;
                                            echo "<p>성인 ". $order['people_adult_cnt'] ."명</p>";
 									    }
 									?>	   
-                                    <p><?= esc(number_format($order['order_price']))?>원 (<?= esc(number_format($order['order_price'] / $order['baht_thai']))?>바트)</p>
+                                    <p><?= esc(number_format($order['real_price_won']))?>원 (<?= esc(number_format($order['real_price_bath'])?>바트)</p>
                                 </div>
                                 <div class="info_name">
                                     <p>여행자 이름: <?= esc($order["order_user_name"]);?>[<?= esc($order["order_user_first_name_en"]);?> <?= esc($order["order_user_last_name_en"]);?>]</p>
@@ -411,8 +411,8 @@ endforeach;
 							    
 								<?php if($order['order_status'] == "X" || $order['order_status'] == "G") { ?>
                                 <div class="info_total_price flex__c box">
-                                    <p class="pri_won"><?= esc(number_format($order['order_price']))?> <span>원</span></p>
-                                    <p class="pri_bath">(<?= esc(number_format($order['order_price'] / $order['baht_thai']))?>바트)</p>
+                                    <p class="pri_won"><?= esc(number_format($order['real_price_won']))?> <span>원</span></p>
+                                    <p class="pri_bath">(<?= esc(number_format($order['real_price_bath'])?>바트)</p>
                                     <div class="btn_payment" data-idx="<?=$order['order_no']?>" >
                                         <p>결제하기</p>
                                     </div>
