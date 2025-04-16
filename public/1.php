10b1d28136d2dd1c1cd02ba14c09642c2f55b334
diana tgkim <diana001@naver.com>
2025-04-16 18:13:26 +0900

    new file:   app/Controllers/Product.php.tmp 	modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -1,12 +1,6 @@
 <?php
-$date_check_in = "2025-02-10";
-$from_date  = $date_check_in;
-$days       = "2";
-$startDate  = new DateTime($from_date); // 시작 날짜 설정
+$baht_thai    = "42.41";
+$order_price  = "4157";
+$real_amt_bath = $order_price / $baht_thai; 
 
-// 종료일 계산 
-$endDate = new DateTime($from_date);
-$endDate = $endDate->modify('+'.$days-1 .'days'); // 3일 포함하기 위해 +2 days
-$endDate = $endDate->format('Y-m-d');
-
-echo $date_check_in." - ". $endDate;
+echo $real_amt_bath;
