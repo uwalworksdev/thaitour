0fd662c28c6b62c5ccbb60a43c5833350b3f6019
diana tgkim <diana001@naver.com>
2025-04-17 16:45:53 +0900

    modified:   app/Views/mypage/booklist.php

--- app/Views/mypage/booklist.php
+++ app/Views/mypage/booklist.php
@@ -342,6 +342,30 @@ endforeach;
                         </script>
                         <script>
                             function openGroupMovement() {
+								
+				const groupNo = $(this).data('group'); // 또는 .attr('data-group')
+
+				alert('선택된 그룹 번호:'+ groupNo);
+			
+				$.ajax({
+
+					url: "/ajax/ajax_group_movement",
+					type: "POST",
+					data: {
+							"group_no": groupNo 
+					},
+					dataType: "json",
+					async: false,
+					cache: false,
+					success: function (data, textStatus) {
+						var message = data.message;
+						alert(message);
+						$('#popupContainer').html(message); // 받은 HTML 삽입
+					},
+					error: function (request, status, error) {
+						alert("code = " + request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
+					}
+				});								
                                 $(".group_movement_popup_wrap").show();
                                 $(".group_movement_popup_content .btn_close_popup").click(function() {
                                     $(".group_movement_popup_wrap").hide();
@@ -682,7 +706,9 @@ endforeach;
 <script>
 $(document).ready(function () {
 		$(document).on('click', '.btn_group_movement', function () {
+			openGroupMovement();
 				// 클릭된 요소에서 data-group 값 가져오기
+			/*	
 				const groupNo = $(this).data('group'); // 또는 .attr('data-group')
 
 				alert('선택된 그룹 번호:'+ groupNo);
@@ -706,7 +732,8 @@ $(document).ready(function () {
 					error: function (request, status, error) {
 						alert("code = " + request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
 					}
-				});	
+				});
+			*/	
 		});
 });
 </script>
