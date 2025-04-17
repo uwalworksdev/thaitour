c6c7f3969404a47d8e4d307d37d9c50238483ee3
diana tgkim <diana001@naver.com>
2025-04-17 23:27:11 +0900

    new file:   app/Views/admin/_reservation/popup_group_movement.php.tmp 	modified:   app/Views/mypage/booklist.php

--- app/Views/mypage/booklist.php
+++ app/Views/mypage/booklist.php
@@ -325,8 +325,7 @@ endforeach;
                                     <img src="/images/mypage/printer_ic_m.png" alt="" class="only_mo">
                                     <p class="only_web">그룹 견적서</p>
                                 </div>
-                                <!--div onclick="openGroupMovement()" class="btn_group_movement">그룹이동</div-->
-								<div class="btn_group_movement" data-group="<?= esc($group['group_no']) ?>">그룹이동</div>
+                                <div onclick="openGroupMovement()" class="btn_group_movement">그룹이동</div>
                             </div>
 
                         </div>
@@ -576,9 +575,78 @@ endforeach;
           </div>
       </div>
   </div>
-<div class="group_movement_popup_wrap" style="display:none;">
-    <!-- 여기 안에 Ajax로 가져온 내용이 들어가야 함 -->
-</div>
+  <div class="group_movement_popup_wrap custom_popup">
+      <div class="group_movement_popup_content custom_popup_content">
+         <div class="btn_close_popup">
+              <img src="/img/btn/btn_close_black_20x20.png" alt="">
+          </div>
+          <h1>그룹이동</h1>
+          <div class="sec2">
+            <div class="box_select">
+                <select name="" id="">
+                    <option value="">그룹명 선택 </option>
+                    <option value="">그룹번호 1</option>
+                    <option value="">그룹번호 2</option>
+                </select>
+                <div class="btn_select">그룹이동</div>
+            </div>
+              <table>
+                  <colgroup>
+                    <col width="30px">
+                      <col width="70px">
+                      <col width="*">
+                      <col width="110px">
+                  </colgroup>
+                  <tbody>
+                      <tr>
+                        <th>
+                            <input id="check_b_1" type="checkbox">
+                            <label for="check_b_1"></label>
+                        </th>
+                          <th>품목</th>
+                          <th>상세</th>
+                          <th>금액</th>
+
+                      </tr>
+                      <tr>
+                        <td>
+                            <input id="check_b_2" type="checkbox">
+                            <label for="check_b_2"></label>
+                        </td>
+                          <td>골프 </td>
+                          <td>
+                              <p class="time">2025-03-28(금) | 로얄 방파인 골프 클럽 </p>
+                              <p>18홀 오전 | 성인 2명 | 그린피 : 6,700바트 | 3,350바트 X 2명 </p>
+                          </td>
+                          <td>
+                              <p>303,175원 </p>
+                              <p>(6,700바트) </p>
+                          </td>
+                      </tr>
+                      <tr>
+                        <td>
+                            <input id="check_b_3" type="checkbox">
+                            <label for="check_b_3"></label>
+                        </td>
+                          <td>투어 </td>
+                          <td>
+                              <p class="time">2025-03-28(금) | (아속출발) 아유타야 선셋 리버크루즈 반일 투어 </p>
+                              <p>[프로모션] 아유타야 오후 | 성인 1명 | 39,000원 X 1명 </p>
+                          </td>
+                          <td>
+                            <p>303,175원 </p>
+                            <p>(6,700바트) </p>
+                          </td>
+                      </tr>
+  
+              </table>
+          </div>
+  
+          <div class="list_desc">
+              <p>* 상품을 선택하고 그룹을 선택 후 그룹이동 버튼을 클릭합니다. </p>
+          </div>
+      </div>
+  </div>
 <div class="popup_wrap place_pop policy_pop">
     <div class="pop_box">
         <button type="button" class="close" onclick="closePopup()"></button>
@@ -598,39 +666,7 @@ endforeach;
     <div class="dim" style="justify-content: space-between;"></div>
 </div>
 
-<script>
-$(document).on('click', '.btn_group_movement', function () {
-    const groupNo = $(this).data('group');
 
-    $.ajax({
-        url: "/ajax/ajax_group_movement",
-        type: "POST",
-        data: { "group_no": groupNo },
-        dataType: "html", // HTML로 응답받는다고 명시
-        success: function (html) {
-            // 받은 HTML을 팝업 내부에 삽입
-            $(".group_movement_popup_wrap").html(html);
-
-            // 팝업 오픈 함수 호출
-            openGroupMovement();
-        },
-        error: function (request, status, error) {
-            alert("code = " + request.status + "\nmessage = " + request.responseText + "\nerror = " + error);
-        }
-    });
-});
-</script>
-
-<script>
-function openGroupMovement() {
-    $(".group_movement_popup_wrap").show();
-
-    // 닫기 버튼 바인딩 (중복 방지 위해 off)
-    $(".group_movement_popup_wrap .btn_close_popup").off("click").on("click", function () {
-        $(".group_movement_popup_wrap").hide();
-    });
-}
-</script>
 <!--
 <form id="checkOut" action="/checkout/payment" method="post">
 <input type="hidden" name="payment_no" id="payment_no" value="" >
