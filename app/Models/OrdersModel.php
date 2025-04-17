ef2c4656aed122911cc3f744f5d42d3a8cc6135c
diana tgkim <diana001@naver.com>
2025-04-17 11:18:52 +0900

    modified:   app/Models/OrdersModel.php

--- app/Models/OrdersModel.php
+++ app/Models/OrdersModel.php
@@ -16,7 +16,7 @@ class OrdersModel extends Model
         , "order_memo", "admin_memo", "manager_name", "manager_phone", "manager_email", "start_date", "end_date"
         , "product_period", "tour_period", "people_adult_cnt", "people_adult_price", "people_kids_cnt", "people_kids_price"
         , "people_baby_cnt", "people_baby_price", "oil_price", "inital_price", "order_price", "order_price_bath", "option_amt", "extra_cost", "order_confirm_price"
-		, "real_price_bath", "real_price_won"
+		, "real_price_bath", "real_price_won", "voucher_price_bath", "voucher_price_won"
 	    , "price", "price_won", "extra_won", "extra_bath","room", "room_type", "bed_type", "date_price", "adult", "kids", "last_price" 				
         , "order_confirm_date", "confirm_method", "deposit_price", "deposit_date", "deposit_method", "order_method", "used_op_type", "room_op_price_sale"
         , "used_coupon_idx", "used_coupon_no", "used_coupon_point", "used_coupon_money", "product_mileage", "used_mileage_money"
