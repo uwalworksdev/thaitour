8e3b40da7c6cdd6163133474bbbe328f31fbb911
diana tgkim <diana001@naver.com>
2025-04-17 22:58:28 +0900

    modified:   app/Views/admin/_reservation/popup_group_movement.php

--- app/Views/admin/_reservation/popup_group_movement.php
+++ app/Views/admin/_reservation/popup_group_movement.php
@@ -1,51 +1,70 @@
-<div class="group_movement_popup_content custom_popup_content">
-    <div class="btn_close_popup">
-        <img src="/img/btn/btn_close_black_20x20.png" alt="닫기">
-    </div>
-    <h1>그룹이동</h1>
+      <div class="group_movement_popup_content custom_popup_content">
+         <div class="btn_close_popup">
+              <img src="/img/btn/btn_close_black_20x20.png" alt="">
+          </div>
+          <h1>그룹이동</h1>
+          <div class="sec2">
+            <div class="box_select">
+                <select name="" id="">
+                    <option value="">그룹명 선택 </option>
+                    <option value="">그룹번호 1</option>
+                    <option value="">그룹번호 2</option>
+                </select>
+                <div class="btn_select">그룹이동</div>
+            </div>
+              <table>
+                  <colgroup>
+                    <col width="30px">
+                      <col width="70px">
+                      <col width="*">
+                      <col width="110px">
+                  </colgroup>
+                  <tbody>
+                      <tr>
+                        <th>
+                            <input id="check_b_1" type="checkbox">
+                            <label for="check_b_1"></label>
+                        </th>
+                          <th>품목</th>
+                          <th>상세</th>
+                          <th>금액</th>
 
-    <div class="sec2">
-        <div class="box_select">
-            <select id="groupSelect">
-                <option value="">그룹명 선택</option>
-                <?php foreach ($groups as $g): ?>
-                    <option value="<?= esc($g['group_no']) ?>" <?= $g['group_no'] == $group_no ? 'selected' : '' ?>>
-                        <?= esc($g['group_name']) ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
-            <div class="btn_select">그룹이동</div>
-        </div>
-
-        <table>
-            <colgroup>
-                <col width="30px">
-                <col width="70px">
-                <col width="*">
-                <col width="110px">
-            </colgroup>
-            <thead>
-                <tr>
-                    <th><input type="checkbox" id="check_all"></th>
-                    <th>종류</th>
-                    <th>내용</th>
-                    <th>가격</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($items as $i): ?>
-                <tr>
-                    <td><input type="checkbox" class="check_item" value="<?= esc($i['id']) ?>"></td>
-                    <td><?= esc($i['type']) ?></td>
-                    <td><?= esc($i['title']) ?> <br> <?= esc($i['date']) ?></td>
-                    <td><?= number_format($i['price']) ?>원</td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-
-    <div class="list_desc">
-        <p>* 상품을 선택하고 그룹을 선택 후 그룹이동 버튼을 클릭합니다.</p>
-    </div>
-</div>
+                      </tr>
+                      <tr>
+                        <td>
+                            <input id="check_b_2" type="checkbox">
+                            <label for="check_b_2"></label>
+                        </td>
+                          <td>골프 </td>
+                          <td>
+                              <p class="time">2025-03-28(금) | 로얄 방파인 골프 클럽 </p>
+                              <p>18홀 오전 | 성인 2명 | 그린피 : 6,700바트 | 3,350바트 X 2명 </p>
+                          </td>
+                          <td>
+                              <p>303,175원 </p>
+                              <p>(6,700바트) </p>
+                          </td>
+                      </tr>
+                      <tr>
+                        <td>
+                            <input id="check_b_3" type="checkbox">
+                            <label for="check_b_3"></label>
+                        </td>
+                          <td>투어 </td>
+                          <td>
+                              <p class="time">2025-03-28(금) | (아속출발) 아유타야 선셋 리버크루즈 반일 투어 </p>
+                              <p>[프로모션] 아유타야 오후 | 성인 1명 | 39,000원 X 1명 </p>
+                          </td>
+                          <td>
+                            <p>303,175원 </p>
+                            <p>(6,700바트) </p>
+                          </td>
+                      </tr>
+  
+              </table>
+          </div>
+  
+          <div class="list_desc">
+              <p>* 상품을 선택하고 그룹을 선택 후 그룹이동 버튼을 클릭합니다. </p>
+          </div>
+      </div>
