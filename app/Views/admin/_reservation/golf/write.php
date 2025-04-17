6a57bfa75d47f663ce5f170e1d42a51031a5cfdd
diana tgkim <diana001@naver.com>
2025-04-17 15:12:32 +0900

    modified:   app/Views/admin/_reservation/golf/write.php

--- app/Views/admin/_reservation/golf/write.php
+++ app/Views/admin/_reservation/golf/write.php
@@ -383,7 +383,7 @@
 								
                                 <tr>
                                         <th>예약 문자발송(알림톡)</th>
-                                        <td>
+                                        <td colspan="3">
                                          <button type="button" class="btn btn-primary" style="width: unset;" onclick="allimtalk('<?=$order_no?>','TY_1652');">예약접수</button>
 										 <button type="button" class="btn btn-primary" style="width: unset;" onclick="allimtalk('<?=$order_no?>','TY_1652');">예약확인</button>
 										 <button type="button" class="btn btn-primary" style="width: unset;" onclick="allimtalk('<?=$order_no?>','TY_1655');">예약확정</button>
@@ -391,7 +391,7 @@
 										 <button type="button" class="btn btn-primary" style="width: unset;" onclick="allimtalk('<?=$order_no?>','TY_1654');">결제완료</button>
 										 <button type="button" class="btn btn-primary" style="width: unset;" onclick="allimtalk('<?=$order_no?>','TY_1657');">예약취소</button>
                                         </td>
-										<th>바우쳐 금액</th>
+										<!--th>바우쳐  금액</th>
 										<td>
 											<input type="text" id="voucher_price_bath" name="voucher_price_bath"
 												   value="<?= number_format($voucher_price_bath)?>" class="input_txt price"
@@ -403,7 +403,7 @@
                                             <a href="#!" class="btn btn-default" id="voucher_update" >
 										    <span class="glyphicon glyphicon-cog"></span><span class="txt">금액수정</span></a><br>
 										    <span style="color:red;" >* 바트를 넣으면 원화가 계산됩니다.</span>
-									 	</td>
+									 	</td-->
 										
                                   </tr>                                
                                 </tbody>
