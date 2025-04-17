93b5dfc9e5c8e6874d0675defcfa72939267c5fa
diana tgkim <diana001@naver.com>
2025-04-17 15:28:50 +0900

    modified:   app/Views/admin/_reservation/golf/write.php 	modified:   app/Views/admin/_reservation/guide/write.php 	modified:   app/Views/admin/_reservation/hotel/write.php 	modified:   app/Views/admin/_reservation/inc/spa_ticket_restaurant/write_.php 	modified:   app/Views/admin/_reservation/tour/write.php 	modified:   app/Views/admin/_reservation/vehicle/write.php

--- app/Views/admin/_reservation/hotel/write.php
+++ app/Views/admin/_reservation/hotel/write.php
@@ -401,7 +401,7 @@
 								
 								<tr>
                                         <th>예약 문자발송(알림톡)</th>
-                                        <td colspan="3">
+                                        <td>
                                          <button type="button" class="btn btn-primary" style="width: unset;" onclick="allimtalk('<?=$order_no?>','TY_1652');">예약접수</button>
 										 <button type="button" class="btn btn-primary" style="width: unset;" onclick="allimtalk('<?=$order_no?>','TY_1652');">예약가능</button>
 										 <button type="button" class="btn btn-primary" style="width: unset;" onclick="allimtalk('<?=$order_no?>','TY_1655');">예약불가능</button>
@@ -411,7 +411,7 @@
 										 <button type="button" class="btn btn-primary" style="width: unset;" onclick="allimtalk('<?=$order_no?>','TY_1657');">예약취소</button>
                                         </td>
 										
-										<!--th>바우쳐  금액</th>
+										<th>바우쳐 금액</th>
 										<td>
 											<input type="text" id="voucher_price_bath" name="voucher_price_bath"
 												   value="<?= number_format($voucher_price_bath)?>" class="input_txt price"
@@ -423,7 +423,7 @@
                                             <a href="#!" class="btn btn-default" id="voucher_update" >
 										    <span class="glyphicon glyphicon-cog"></span><span class="txt">금액수정</span></a><br>
 										    <span style="color:red;" >* 바트를 넣으면 원화가 계산됩니다.</span>
-									 	</td-->
+									 	</td>
                                   </tr>
                                 
                                 </tbody>
