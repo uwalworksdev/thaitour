19ec342c04cd8d9fcff3a168e75114453a889336
diana tgkim <diana001@naver.com>
2025-04-18 00:07:25 +0900

    modified:   app/Views/layouts/popup_layout.php

--- app/Views/layouts/popup_layout.php
+++ app/Views/layouts/popup_layout.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <title><?= esc($title ?? '팝업') ?></title>
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+	<link href="<?= base_url('css/mypage/mypage_new.css') ?>" rel="stylesheet" type="text/css"/>
+	<link href="<?= base_url('css/mypage/mypage_reponsive_new.css') ?>" rel="stylesheet" type="text/css"/>
+	<link href="<?= base_url('css/mypage/mypage_reponsive_new02.css') ?>" rel="stylesheet" type="text/css"/>
+	<link href="<?= base_url('css/mypage/mypage.css') ?>" rel="stylesheet" type="text/css"/>
+	<link href="<?= base_url('css/mypage/mypage_reponsive.css') ?>" rel="stylesheet" type="text/css"/>
+	<link href="<?= base_url('css/community/community.css') ?>" rel="stylesheet" type="text/css"/>	
     <style>
         /* 팝업용 간단한 스타일 예시 */
         body {
