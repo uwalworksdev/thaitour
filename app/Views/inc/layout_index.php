7feedecafc521b2729908d948cad44c8a31a151b
diana tgkim <diana001@naver.com>
2025-04-17 15:03:06 +0900

    modified:   app/Views/inc/layout_index.php

--- app/Views/inc/layout_index.php
+++ app/Views/inc/layout_index.php
@@ -125,7 +125,7 @@ try {
 <div class="main_sale_banner flex__c">
     <?php foreach ($banner_ as $row) { ?>
         <div class="time_sale_banner flex__c">
-           <a href="/time_sale/list"><img src="/data/bbs/20250417141826026.png" alt="main_sale_img"></a>
+           <a href="/time_sale/list"><img src="/data/bbs/<?=$row['ufile5']?>" alt="main_sale_img"></a>
         </div>
 	<?php } ?>	
 </div>
