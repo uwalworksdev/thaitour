aad2afb143e105d11dacd87e46694d50eddeb66e
diana tgkim <diana001@naver.com>
2025-04-18 10:37:50 +0900

    modified:   app/Views/voucher/voucher_hotel.php

--- app/Views/voucher/voucher_hotel.php
+++ app/Views/voucher/voucher_hotel.php
@@ -71,7 +71,8 @@ $setting = homeSetInfo();
                         <tr>
                             <th>Date</th>
                             <td style="color : red" colspan="3">
-							    <?=$result->start_date?>(<?=date('D', strtotime($result->start_date))?>) <?=$result->end_date?>(<?=date('D', strtotime($result->end_date))?>) /<?=$result->order_day_cnt?> night
+							    <?=date('d-M-Y(D)', strtotime($result->start_date))?>
+								<?=$result->end_date?>(<?=date('D', strtotime($result->end_date))?>) /<?=$result->order_day_cnt?> night
 							</td>
 
                         </tr>
