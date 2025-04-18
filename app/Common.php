c054be0bdf0b1653e591b61ca4db4ed1e5ae43e6
diana tgkim <diana001@naver.com>
2025-04-19 02:13:58 +0900

    modified:   app/Common.php

--- app/Common.php
+++ app/Common.php
@@ -1976,18 +1976,19 @@ function order_info($order_gubun, $order_no, $order_idx)
 		$order_info = $result['option_name'] . "| 라운딩인원 ". $result['option_cnt'] ."명";
 	}
 
-	if($order_gubun == "spa") {
-		$query      = $connect->query("SELECT * FROM tbl_order_option WHERE order_idx = '". $order_idx ."' "); 
-        $result     = $query->getResultArray();
-		$order_info = "";
-		foreach ($result as $row):
-			if($order_info == "") {
-		       $order_info  = $row['option_name'] . " ". $row['option_cnt'] ."명";
-		    } else {
-		       $order_info .= "|". $row['option_name'] . " ". $row['option_cnt'] ."명 ";
-		    }   
-		endforeach;
-	}
+if ($order_gubun == "spa") {
+    $query = $connect->query("SELECT * FROM tbl_order_option WHERE order_idx = '" . addslashes($order_idx) . "'"); 
+    $result = $query->getResultArray();
+
+    $order_info_parts = [];
+
+    foreach ($result as $row) {
+        $order_info_parts[] = $row['option_name'] . " " . $row['option_cnt'] . "명";
+    }
+
+    $order_info = implode(" | ", $order_info_parts);
+}
+
 	
 	return $order_info;
 }
