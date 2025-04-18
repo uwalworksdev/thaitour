efeb1b651ae4d1d4a9350c6fab61e34a798d4789
diana tgkim <diana001@naver.com>
2025-04-18 10:14:44 +0900

    modified:   app/Controllers/VoucherController.php

--- app/Controllers/VoucherController.php
+++ app/Controllers/VoucherController.php
@@ -27,26 +27,31 @@ class VoucherController extends BaseController
     public function hotel($idx)
     {
 		
-		$private_key = private_key();
-		
-		$db      = db_connect(); // DB 연결
-		$builder = $db->table('tbl_order_mst'); // 테이블 지정
-		$builder->select(" *,
-			AES_DECRYPT(UNHEX(order_user_name), '$private_key') AS order_user_name,
-			AES_DECRYPT(UNHEX(order_user_email), '$private_key') AS order_user_email,
-			AES_DECRYPT(UNHEX(order_user_first_name_en), '$private_key') AS order_user_first_name_en,
-			AES_DECRYPT(UNHEX(order_user_last_name_en), '$private_key') AS order_user_last_name_en,
-			AES_DECRYPT(UNHEX(order_user_mobile), '$private_key') AS order_user_mobile,
-			AES_DECRYPT(UNHEX(local_phone), '$private_key') AS local_phone,
-			AES_DECRYPT(UNHEX(order_zip), '$private_key') AS order_zip,
-			AES_DECRYPT(UNHEX(order_addr1), '$private_key') AS order_addr1,
-			AES_DECRYPT(UNHEX(order_addr2), '$private_key') AS order_addr2,
-			AES_DECRYPT(UNHEX(manager_name), '$private_key') AS manager_name
-		");		
-		$query   = $builder->where('order_idx', $idx)->get(); // 조건 추가 후 실행
-        //write_log("last query- ". $db->getLastQuery());
+		$private_key = private_key(); // 복호화 키
 
-        $result = $query->getRow(); // 단일 객체
+		$db = db_connect();
+		$builder = $db->table('tbl_order_mst a');
+
+		$builder->select("
+					a.*, b.*, c.*,
+					AES_DECRYPT(UNHEX(a.order_user_name), '$private_key') AS order_user_name,
+					AES_DECRYPT(UNHEX(a.order_user_email), '$private_key') AS order_user_email,
+					AES_DECRYPT(UNHEX(a.order_user_first_name_en), '$private_key') AS order_user_first_name_en,
+					AES_DECRYPT(UNHEX(a.order_user_last_name_en), '$private_key') AS order_user_last_name_en,
+					AES_DECRYPT(UNHEX(a.order_user_mobile), '$private_key') AS order_user_mobile,
+					AES_DECRYPT(UNHEX(a.local_phone), '$private_key') AS local_phone,
+					AES_DECRYPT(UNHEX(a.order_zip), '$private_key') AS order_zip,
+					AES_DECRYPT(UNHEX(a.order_addr1), '$private_key') AS order_addr1,
+					AES_DECRYPT(UNHEX(a.order_addr2), '$private_key') AS order_addr2,
+					AES_DECRYPT(UNHEX(a.manager_name), '$private_key') AS manager_name
+		");
+
+		$builder->join('tbl_product_mst b', 'a.product_idx = b.product_idx', 'left');
+		$builder->join('tbl_product_stay c', 'b.stay_idx = c.stay_idx', 'left');
+		$builder->where('a.order_idx', $idx);
+
+		$query = $builder->get();
+		$result = $query->getResult();
 
         return view("voucher/voucher_hotel", [ 'result'  => $result ]);        
     }
