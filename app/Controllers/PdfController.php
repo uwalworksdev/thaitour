53c9bf88714974f9768c5b390af0e1ad8b9ed694
diana tgkim <diana001@naver.com>
2025-04-19 12:59:40 +0900

    modified:   app/Controllers/PdfController.php

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -8,10 +8,45 @@ class PdfController extends BaseController
 {
     public function generateQuotation()
     {
-        $pdf = new Mpdf([
+        // 샘플 데이터 (테스트용)
+        $sum = [
+            ['code_name' => '호텔', 'cnt' => 1, 'total_won' => 50000],
+            ['code_name' => '골프', 'cnt' => 2, 'total_won' => 150000],
+        ];
+
+        $items = [
+            [
+                'code_name' => '호텔',
+                'order_date' => '2025-04-19',
+                'product_name' => '그랜드호텔',
+                'order_gubun' => 'hotel',
+                'order_no' => '1234',
+                'order_idx' => '1',
+                'real_price_won' => 50000,
+                'real_price_bath' => 1300
+            ],
+            [
+                'code_name' => '골프',
+                'order_date' => '2025-04-20',
+                'product_name' => '그린밸리 CC',
+                'order_gubun' => 'golf',
+                'order_no' => '5678',
+                'order_idx' => '2',
+                'real_price_won' => 150000,
+                'real_price_bath' => 3900
+            ],
+        ];
+
+        // 사용자 이름 세션 예시
+        session()->set(['member' => ['name' => '김평진']]);
+
+        $html = view('pdf/quotation_view', compact('sum', 'items'));
+
+        // mPDF 설정
+        $mpdf = new Mpdf([
             'mode' => 'utf-8',
             'format' => 'A4',
-            'fontDir' => [FCPATH . 'ttfonts'], // 폰트 폴더 추가
+            'fontDir' => [FCPATH . 'ttfonts'],
             'default_font' => 'nanumgothic',
             'fontdata' => [
                 'nanumgothic' => [
@@ -21,30 +56,17 @@ class PdfController extends BaseController
             ]
         ]);
 
-        $data = [
-					'quotation_date' => '2025년 03월 14일',
-					'customer_name'  => '김평진',
-					'hotel_count' => '0건',
-					'hotel_price' => '0원',
-					'golf_count'  => '12건',
-					'golf_price'  => '303,175원',
-					'tour_count'  => '1건',
-					'tour_price'  => '39,000원',
-					'car_count'   => '0건',
-					'car_price'   => '0원',
-					'guide_count' => '0건',
-					'guide_price' => '0원',
-					'total_count' => '2건',
-					'total_price' => '342,175원'
-        ];
+        // CSS 불러오기 (절대 경로 사용)
+        $stylesheet = file_get_contents(FCPATH . 'css/mypage/mypage_new.css');
+        $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
 
-        $html = view('pdf/quotation', $data);
+        // HTML 내용 출력
+        $mpdf->WriteHTML($html);
 
-        $pdf->WriteHTML($html);
-		
-        $pdf->Output('quotation.pdf', 'I');
-        exit;
+        // 출력
+        return $mpdf->Output('견적서.pdf', 'I');
     }
 }
+
 
 
