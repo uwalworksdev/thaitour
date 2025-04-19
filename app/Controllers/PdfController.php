091d84be2106a5de2fde93722630c21ac24196bd
diana tgkim <diana001@naver.com>
2025-04-19 12:05:27 +0900

    modified:   app/Controllers/PdfController.php

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -1,32 +1,41 @@
 <?php
-public function generateQuotation()
+
+namespace App\Controllers;
+
+use Mpdf\Mpdf;
+
+class PdfController extends BaseController
 {
-    $pdf = new \Mpdf\Mpdf([
-        'default_font' => 'dejavusans',
-        'tempDir' => WRITEPATH . 'mpdf_tmp'
-    ]);
+    public function generateQuotation()
+    {
+        $pdf = new Mpdf([
+            'default_font' => 'dejavusans',
+            'tempDir' => WRITEPATH . 'mpdf_tmp'
+        ]);
 
-    $data = [
-        'quotation_date' => '2025년 03월 14일',
-        'customer_name'  => '김평진',
-        'hotel_count' => '0건',
-        'hotel_price' => '0원',
-        'golf_count'  => '12건',
-        'golf_price'  => '303,175원',
-        'tour_count'  => '1건',
-        'tour_price'  => '39,000원',
-        'car_count'   => '0건',
-        'car_price'   => '0원',
-        'guide_count' => '0건',
-        'guide_price' => '0원',
-        'total_count' => '2건',
-        'total_price' => '342,175원'
-    ];
+        $data = [
+            'quotation_date' => '2025년 03월 14일',
+            'customer_name'  => '김평진',
+            'hotel_count' => '0건',
+            'hotel_price' => '0원',
+            'golf_count'  => '12건',
+            'golf_price'  => '303,175원',
+            'tour_count'  => '1건',
+            'tour_price'  => '39,000원',
+            'car_count'   => '0건',
+            'car_price'   => '0원',
+            'guide_count' => '0건',
+            'guide_price' => '0원',
+            'total_count' => '2건',
+            'total_price' => '342,175원'
+        ];
 
-    $html = view('pdf/quotation', $data);
+        $html = view('pdf/quotation', $data);
 
-    $pdf->WriteHTML($html);
-    $pdf->Output('quotation.pdf', 'I'); // 브라우저 출력
-    exit;
+        $pdf->WriteHTML($html);
+        $pdf->Output('quotation.pdf', 'I');
+        exit;
+    }
 }
+
 
