9d549481c34af065cca1685302e03796389cbd89
diana tgkim <diana001@naver.com>
2025-04-19 13:24:53 +0900

    modified:   app/Views/admin/_reservation/popup_group_estimate.php

--- app/Views/admin/_reservation/popup_group_estimate.php
+++ app/Views/admin/_reservation/popup_group_estimate.php
@@ -1,5 +1,5 @@
 <link href="<?= base_url('css/mypage/mypage_new.css') ?>" rel="stylesheet" />
-	
+
       <div class="estimate_popup_content custom_popup_content">
          <div class="btn_close_popup">
               <img src="/img/btn/btn_close_black_20x20.png" alt="">
@@ -104,63 +104,4 @@
 			$(document).on('click', '#btn_print', function () {
 				window.print();
 			});
-		</script>
-
-		<script>
-		$(document).on('click', '#btn_select', function() {
-			
-				// 예: 선택된 그룹 값 가져오기
-				let selectedGroup = $('#group_select').val();
-				if(selectedGroup == "") {
-				   alert('이동 그룹을 선택하세요.');
-				   $("#group_select").focus();
-				   return false;
-				}   
-				
-				// 예: 체크된 항목 수
-				let selectedItems = $('input[type="checkbox"][id^="check_b_"]:checked')
-					.map(function () {
-						return $(this).val();
-					})
-					.get()
-					.join('|');
-
-				if(selectedItems == "") {
-				   alert('이동할 에약을 선택하세요.');
-				   return false;
-				}   
-					
-				console.log('선택된 그룹:', selectedGroup);
-				console.log('선택된 예약:', selectedItems);
-				
-				$.ajax({
-
-					url: "/ajax/ajax_group_change",
-					type: "POST",
-					data: {
-
-						"selectedGroup": selectedGroup, 
-						"order_idxs"   : selectedItems 
-
-					},
-					dataType: "json",
-					async: false,
-					cache: false,
-					success: function (data, textStatus) {
-						var message = data.message;
-						alert(message);
-						location.reload();
-					},
-					error: function (request, status, error) {
-						alert("code = " + request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
-					}
-				});				
-		});
-
-		$(document).on('click', '#check_all', function () {
-				const isChecked = $(this).is(':checked');
-				
-				// 모든 개별 체크박스에 체크 상태 설정
-				$('input[type="checkbox"][id^="check_b_"]').prop('checked', isChecked);
-		});
 		</script>
