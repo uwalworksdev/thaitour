f1ee840aa45f0cb01fd68d0dacf188c7c26cbc63
diana tgkim <diana001@naver.com>
2025-04-19 12:27:32 +0900

    modified:   app/Controllers/PdfController.php

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -9,31 +9,32 @@ class PdfController extends BaseController
     public function generateQuotation()
     {
         $pdf = new Mpdf([
-            'mode' => 'utf-8', // UTF-8 모드 사용
-            'format' => 'A4',
-            'default_font' => 'NanumGothic', // 기본 폰트 설정
+							'mode' => 'utf-8', // UTF-8 모드 사용
+							'format' => 'A4',
+							'default_font' => 'NanumGothic', // 기본 폰트 설정
         ]);
 
         $data = [
-            'quotation_date' => '2025년 03월 14일',
-            'customer_name'  => '김평진',
-            'hotel_count' => '0건',
-            'hotel_price' => '0원',
-            'golf_count'  => '12건',
-            'golf_price'  => '303,175원',
-            'tour_count'  => '1건',
-            'tour_price'  => '39,000원',
-            'car_count'   => '0건',
-            'car_price'   => '0원',
-            'guide_count' => '0건',
-            'guide_price' => '0원',
-            'total_count' => '2건',
-            'total_price' => '342,175원'
+					'quotation_date' => '2025년 03월 14일',
+					'customer_name'  => '김평진',
+					'hotel_count' => '0건',
+					'hotel_price' => '0원',
+					'golf_count'  => '12건',
+					'golf_price'  => '303,175원',
+					'tour_count'  => '1건',
+					'tour_price'  => '39,000원',
+					'car_count'   => '0건',
+					'car_price'   => '0원',
+					'guide_count' => '0건',
+					'guide_price' => '0원',
+					'total_count' => '2건',
+					'total_price' => '342,175원'
         ];
 
         $html = view('pdf/quotation', $data);
 
         $pdf->WriteHTML($html);
+		
         $pdf->Output('quotation.pdf', 'I');
         exit;
     }
