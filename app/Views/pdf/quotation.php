d94128e3541003c0c0f77c8fdcebc131b2f3669f
diana tgkim <diana001@naver.com>
2025-04-19 13:15:44 +0900

    modified:   app/Views/pdf/quotation.php

--- app/Views/pdf/quotation.php
+++ app/Views/pdf/quotation.php
@@ -11,77 +11,38 @@
 </head>
 <body>
 
-<div class="estimate_popup_content custom_popup_content">
-          <h1>더투어랩 여행견적서 </h1>
-          <div class="sec1">
-              <div class="left">
-                  <p class="ttl">The Tour Lab Co.,Ltd </p>
-                  <span>Sukhumvit 13 Klongtoei Nuea </span>
-                  <span>Watthana Bangkok 10110 </span>
-                  <span>서비스/여행업 No. 0105565060507 </span>
-                  <p class="day">견적일 : 2025년 04월 19일 </p>
-                  <p class="name">고객명 : 김태균 님 귀하 </p>
-                  <img src="/img/sub/sign-001.jpg" class="img_stem">
-              </div>
-              <div class="right">
-                  <table>
-                      <colgroup>
-                          <col width="110px">
-                          <col width="110px">
-                          <col width="110px">
-                      </colgroup>
-                      <tbody>
-					      	
-					      					      	
-                          <tr>
-                              <th>골프</th>
-                              <td>1건 </td>
-                              <td>4,157원 </td>
-                          </tr>
-						                            <tr>
-                              <th class="total">합계 </th>
-                              <td class="total">1건 </td>
-                              <td class="total">4,157원 </td>
-                          </tr>
-                      </tbody>
-                  </table>
-              </div>
-          </div>
-          <div class="sec2">
-              <table>
-                  <colgroup>
-                      <col width="70px">
-                      <col width="*">
-                      <col width="110px">
-                  </colgroup>
-                  <tbody>
-                      <tr>
-                          <th>품목</th>
-                          <th>상세</th>
-                          <th>금액</th>
-                      </tr>
-					  					                        <tr>
-                          <td>골프</td>
-                          <td>
-                              <p class="time">2025-04-19 01:34:48(토) | 테스트 상품 김평진 </p>
-                              <p>홀수 : 18홀|주간 : 2025-04-19 01:34:48| 티오프시간 : 10:09| 라운딩인원 2명 </p>
-                          </td>
-                          <td>
-                              <p>4,157원 </p>
-                              <p>(98바트) </p>
-                          </td>
-                      </tr>
-					                </tbody></table>
-          </div>
-  
-          <div class="list_desc">
-              <p>- 상기 견적은 고객님께서 직접 선택하신 상품으로 발행된 견적서입니다. </p>
-              <p>- 견적서상 내용은 확정 예약시 상품의 예약가능여부/환을 등에 따라 금액 및 내용에 변동이 있을 수 있습니다. </p>
-              <p>- 한국 : 국민은행 636101-01-301315 (주) 토토부킹 </p>
-              <p>- 태국: Kasikorn Bank 895-2-19850-6 (Totobooking) </p>
-          </div>
-      </div>
+<h1>더투어랩 여행견적서</h1>
+<h3>TOTO Booking Co., Ltd.</h3>
 
+<p>Sukhumvit 101 Bangjak<br>
+Prakhanong Bangkok 10260<br>
+서비스/여행업 No.101-86-79949</p>
+
+<p>견적일: <?= $quotation_date ?><br>
+고객명: <?= $customer_name ?> 님 귀하 
+<img src="/img/sub/sign-001.jpg" width="100" height="auto" alt="logo">
+</p>
+<table>
+    <tr>
+        <th>호텔</th><td><?= $hotel_count ?></td><td><?= $hotel_price ?></td>
+        <th>골프</th><td><?= $golf_count ?></td><td><?= $golf_price ?></td>
+    </tr>
+    <tr>
+        <th>투어</th><td><?= $tour_count ?></td><td><?= $tour_price ?></td>
+        <th>차량</th><td><?= $car_count ?></td><td><?= $car_price ?></td>
+    </tr>
+    <tr>
+        <th>가이드</th><td><?= $guide_count ?></td><td><?= $guide_price ?></td>
+        <th>합계</th><td><?= $total_count ?></td><td><?= $total_price ?></td>
+    </tr>
+</table>
+
+<p style="margin-top: 30px;">
+- 상기 견적은 고객님께서 직접 선택하신 상품으로 발행된 견적서입니다.<br>
+- 견적서상 내용은 항공 및 예약 가능여부/환율 등에 따라 금액 및 내용에 변동이 있을 수 있습니다.<br>
+- 계좌번호: 636101-01-3031315 (주) 토토북킹<br>
+- 태국: Kasikorn Bank 895-2-19850-6 (Totobooking)
+</p>
 
 </body>
 </html>
