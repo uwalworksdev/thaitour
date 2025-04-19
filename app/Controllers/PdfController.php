68cf0c264c29dbda4ee021dd7e396ba3ca1321d9
diana tgkim <diana001@naver.com>
2025-04-19 16:23:45 +0900

    modified:   app/Controllers/PdfController.php

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -21,6 +21,30 @@ class PdfController extends BaseController
             ]
         ]);
 
+		$group_no = $this->request->getPost('group_no');
+
+		$db = \Config\Database::connect();
+
+		// 요약 정보 (상품 코드별 집계)
+		$sql = "SELECT code_name, 
+					   COUNT(order_idx) AS cnt,
+					   SUM(real_price_bath) AS total_bath,
+					   SUM(real_price_won) AS total_won
+				FROM tbl_order_mst
+				WHERE group_no = ?
+				GROUP BY code_name";
+
+		$sum = $db->query($sql, [$group_no])->getResultArray();
+
+		// ① 그룹 해당 예약 목록
+		$items = $db->query("SELECT * FROM tbl_order_mst WHERE m_idx = ? AND group_no = ?", [$m_idx, $group_no])->getResultArray();
+
+		$data = [
+			'group_no' => $group_no,
+			'sum'      => $sum,
+			'items'    => $items
+		];
+		
         $data = [
 					'quotation_date' => '2025년 03월 14일',
 					'customer_name'  => '김태균',
