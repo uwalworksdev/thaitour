a88b6e4037c4e542f89cab81421cc5029b25d18a
diana tgkim <diana001@naver.com>
2025-04-19 12:32:31 +0900

    modified:   app/Controllers/PdfController.php

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -9,9 +9,16 @@ class PdfController extends BaseController
     public function generateQuotation()
     {
         $pdf = new Mpdf([
-							'mode' => 'utf-8', // UTF-8 모드 사용
-							'format' => 'A4',
-							'default_font' => 'NanumGothic', // 기본 폰트 설정
+            'mode' => 'utf-8', // UTF-8 모드 사용
+            'format' => 'A4',
+            'default_font' => 'NanumGothic', // 기본 폰트 설정
+            'fontDir' => [FCPATH . 'public/ttfonts'], // 폰트 디렉토리 설정
+            'fontdata' => [
+                'NanumGothic' => [
+                    'R' => 'NanumGothic.ttf', // Regular 폰트 파일
+                    'B' => 'NanumGothicBold.ttf', // Bold 폰트 파일
+                ]
+            ]
         ]);
 
         $data = [
