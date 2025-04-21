0d20c08e0ac1b309f8d7bf11109eb773d079b67e
diana tgkim <diana001@naver.com>
2025-04-21 21:27:16 +0900

    modified:   app/Views/admin/_reservation/list.php

--- app/Views/admin/_reservation/list.php
+++ app/Views/admin/_reservation/list.php
@@ -244,9 +244,9 @@
 
                         </tbody>
                     </table>
-                </form>
+                <!--/form>
 
-                <form name="search" id="search">
+                <form name="search" id="search"-->
                     <table cellpadding="0" cellspacing="0" summary="" class="listTable01" style="table-layout:fixed;">
                         <colgroup>
                             <col width="100">
