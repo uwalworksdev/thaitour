831901d99cda7a967c168bfc23227fbbd14045f6
diana tgkim <diana001@naver.com>
2025-04-22 14:32:19 +0900

    modified:   app/Views/admin/_reservation/list_payment.php

--- app/Views/admin/_reservation/list_payment.php
+++ app/Views/admin/_reservation/list_payment.php
@@ -104,18 +104,19 @@
                         </tr>
                         <tr>
                             <td class="label">결제방법</td>
-                            <td class="inbox">
-
-                                <?php
-                                foreach ($_pg_Method as $key => $value) {
-                                    ?>
-                                    <p>
-                                        <input name="payment_chker[]" class="state_chker" type="checkbox"
-                                               value="<?= $_pg_Method[$key] ?>" > <?= $value ?>
-                                        &nbsp;&nbsp;
-                                    </p>
-                                <?php } ?>
-                            </td>
+<?php foreach ($_pg_Method as $key => $value): ?>
+    <p>
+        <input
+            name="payment_chker[]"
+            class="state_chker"
+            type="checkbox"
+            value="<?= esc($value) ?>"
+            <?= in_array($value, $selected_payments) ? 'checked' : '' ?>
+        >
+        <?= esc($value) ?>
+        &nbsp;&nbsp;
+    </p>
+<?php endforeach; ?>
                         </tr>
                         <tr>
                             <td class="label">예약상품상태</td>
