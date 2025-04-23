e140854da2db374c424c3eb6f0138ad8b71cde3b
diana tgkim <diana001@naver.com>
2025-04-23 11:38:57 +0900

    modified:   app/Views/admin/_member/list_grade.php

--- app/Views/admin/_member/list_grade.php
+++ app/Views/admin/_member/list_grade.php
@@ -51,9 +51,7 @@
                                         <td><?= esc($row['upd_date']) ?></td>
                                         <td><?= esc($row['reg_date']) ?></td>
                                         <td>
-                                            <!--a href="#!"><img src="/images/admin/common/ico_setting2.png" alt="수정"></a-->
-                                            <a href="javascript:del_it('<?= $row['m_idx'] ?>');"><img
-                                                    src="/images/admin/common/ico_error.png" alt="삭제" /></a>
+                                            <button type="button" id="geade_add">등급수정</button>
                                         </td>
                                     </tr>
                                 <?php } ?>
