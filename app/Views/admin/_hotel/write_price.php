26b6e7d8cb67c1beced1280cbbc945682cd647d2
diana tgkim <diana001@naver.com>
2025-04-25 22:42:52 +0900

    modified:   app/Views/admin/_hotel/write_price.php

--- app/Views/admin/_hotel/write_price.php
+++ app/Views/admin/_hotel/write_price.php
@@ -952,15 +952,13 @@ $(document).ready(function () {
 	$(document).ready(function(){
 		$(".cpyDatePrice").click(function(){
 
-			if (confirm("일자별 생성은 상품가격이 초기화 되므로\n복구가 불가능합니다.\n반드시 침대타입 생성 후 처리해 주세요.........") == false) {
+			if (confirm("일자별 생성은 상품가격이 초기화 되므로\n복구가 불가능합니다.\n반드시 침대타입 생성 후 처리해 주세요.") == false) {
 				return;
 			}
 
 		    let rooms_idx = $(this).val();
 			let	from_date = $("#o_sdate_"+rooms_idx).val();
 			let	to_date   = $("#o_edate_"+rooms_idx).val();		
-			alert(from_date);
-			alert(to_date);
 
 			if(from_date == "") {
 			   alert('가격적용 기간을 입력하세요.');
@@ -1009,9 +1007,23 @@ $(document).ready(function () {
 				return;
 			}
 		
-		    allUpdate();
-/*			
+		    //allUpdate();
+ 			
 		    let rooms_idx = $(this).val();
+			let	from_date = $("#o_sdate_"+rooms_idx).val();
+			let	to_date   = $("#o_edate_"+rooms_idx).val();		
+
+			if(from_date == "") {
+			   alert('가격적용 기간을 입력하세요.');
+			   $("#o_sdate_"+rooms_idx).focus();
+			   return false;
+			}   
+				   
+			if(to_date == "") {
+			   alert('가격적용 기간을 입력하세요.');
+			   $("#o_edate_"+rooms_idx).focus();
+			   return false;
+			}   
 			
 			var message = "";
 			$.ajax({
@@ -1035,7 +1047,7 @@ $(document).ready(function () {
 					alert("code = " + request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
 				}
 			});	
-*/			
+ 			
      	});
 	});
 </script>
