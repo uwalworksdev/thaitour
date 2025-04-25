ad28956fcbe8b097a446b161db99ffd287e9d7b6
thanhthoai <thanhthoai1001@gmail.com>
2025-04-25 14:50:08 +0700

    fix policy

--- app/Views/admin/_cms/policy_write.php
+++ app/Views/admin/_cms/policy_write.php
@@ -61,18 +61,33 @@
                                             </tr>
                                             <tr>
                                                 <td>
-                                                    <textarea name="policy_contents[]" id="policy_contents_<?= $index ?>" ...><?= viewSQ($item['policy_contents']) ?></textarea>
-                                                    
+                                                <textarea name="policy_contents[]" id="policy_contents_<?= $index ?>" rows="10" cols="100"
+                                                    class="input_txt"
+                                                    style="width:100%; height:400px; display:none;"><?= isset($item) ? viewSQ($item['policy_contents']) : '' ?></textarea>
+
                                                     <script type="text/javascript">
                                                         var oEditors<?= $index ?> = [];
+
+                                                        // 추가 글꼴 목록
+                                                        //var aAdditionalFontSet = [["MS UI Gothic", "MS UI Gothic"], ["Comic Sans MS", "Comic Sans MS"],["TEST","TEST"]];
+
                                                         nhn.husky.EZCreator.createInIFrame({
                                                             oAppRef: oEditors<?= $index ?>,
                                                             elPlaceHolder: "policy_contents_<?= $index ?>",
                                                             sSkinURI: "/smarteditor/SmartEditor2Skin.html",
                                                             htParams: {
-                                                                bUseToolbar: true,
-                                                                bUseVerticalResizer: true,
-                                                                bUseModeChanger: true,
+                                                                bUseToolbar: true,				// 툴바 사용 여부 (true:사용/ false:사용하지 않음)
+                                                                bUseVerticalResizer: true,		// 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
+                                                                bUseModeChanger: true,			// 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
+                                                                //aAdditionalFontList : aAdditionalFontSet,		// 추가 글꼴 목록
+                                                                fOnBeforeUnload: function () {
+                                                                    //alert("완료!");
+                                                                }
+                                                            }, //boolean
+                                                            fOnAppLoad: function () {
+                                                                //예제 코드
+                                                                //oEditors.getById["ir1"].exec("PASTE_HTML", ["로딩이 완료된 후에 본문에 삽입되는 text입니다."]);
+
                                                             },
                                                             fCreator: "createSEditor2"
                                                         });
