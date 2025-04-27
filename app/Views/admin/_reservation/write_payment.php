ffe4f86c3cb9b47f05b076c7b88cefa2453c4e81
diana tgkim <diana001@naver.com>
2025-04-27 22:55:03 +0900

    modified:   app/Views/admin/_reservation/write_payment.php 	deleted:    app/Views/admin/_reservation/write_payment.php.tmp

--- app/Views/admin/_reservation/write_payment.php
+++ app/Views/admin/_reservation/write_payment.php
@@ -172,7 +172,7 @@
                                     <th>결제금액 취소</th>
                                         <td>
                                             <?=number_format($payment_row['payment_price'])?>원 &emsp;
-											<a href="javascript:payment_cancel('<?=$payment_row['payment_no']?>')" class="btn btn-default">
+											<a href="javascript:payment_cancel('<?=$payment_row['payment_no']?>','<?=$payment_row['payment_pg']?>)" class="btn btn-default">
 										<span class="glyphicon glyphicon-cog"></span><span class="txt">카드결제 취소</span></a>
 										&emsp;2025-02-08 00:00
                                         </td>
