27a7f65646cfae1f78764f571e9901f4e192c093
diana tgkim <diana001@naver.com>
2025-04-30 01:08:38 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -1378,7 +1378,7 @@ $(document).ready(function() {
                     hour,
                 },
                 success: function (data) {
-					alert(data); 	
+					//alert(data); 	
                     $('#final_option_list').html(data);
                     $("#final_option_list .card-item").eq(0).trigger("click");
                     
