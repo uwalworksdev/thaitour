bed1dc8977c81776cf7da93b7e969341a9fd5df4
diana tgkim <diana001@naver.com>
2025-05-02 01:00:06 +0900

    modified:   app/Config/Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -684,7 +684,6 @@ $routes->group("ajax", static function ($routes) {
 	$routes->post("ajax_estimate_mailsend", "AjaxController::ajax_estimate_mailsend");
 	$routes->post("ajax_nicepay_cancelResult", "AjaxController::ajax_nicepay_cancelResult");
 	$routes->post("ajax_order_del", "AjaxController::ajax_order_del");
-	
 });
 
 $routes->group("api", static function ($routes) {
@@ -1012,6 +1011,7 @@ $routes->get('/payment/request', 'PaymentController::requestPayment');
 $routes->get('/payment/complete', 'PaymentController::completePayment');
 $routes->post('/payment/nicepay_result', 'PaymentController::nicepay_result');
 $routes->post('nicepay_refund', 'PaymentController::nicepay_refund');
+$routes->post('nicepay_partial_refund', 'PaymentController::nicepay_partial_refund');
 
 $routes->get('fake-login', 'FakeLogin::index');  // 가상 로그인
 $routes->get('fake-logout', 'FakeLogin::logout'); // 로그아웃
