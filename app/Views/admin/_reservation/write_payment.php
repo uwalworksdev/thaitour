dcaa7c60e37fdf710cb8b6a015f45164329fe92d
diana tgkim <diana001@naver.com>
2025-05-01 23:15:48 +0900

    modified:   app/Views/admin/_reservation/write_payment.php

--- app/Views/admin/_reservation/write_payment.php
+++ app/Views/admin/_reservation/write_payment.php
@@ -78,15 +78,15 @@
                                 </caption>
                                 <colgroup>
                                     <col width="10%"/>
-                                    <col width="10%"/>
-                                    <col width="10%"/>
-                                    <col width="10%"/>
+                                    <col width="8%"/>
+                                    <col width="8%"/>
+                                    <col width="8%"/>
                                     <col width="*"/>
 									<!--col width="10%"/-->
-                                    <col width="10%"/>
-                                    <col width="10%"/>
-                                    <col width="10%"/>
-                                    <col width="10%"/>
+                                    <col width="8%"/>
+                                    <col width="8%"/>
+                                    <col width="8%"/>
+                                    <col width="8%"/>
                                     <col width="5%"/>
                                     <col width="10%"/>
                                 </colgroup>
