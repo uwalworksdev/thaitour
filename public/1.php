e2e6b46a2376012a426220b93f34a7f5cccb7134
diana tgkim <diana001@naver.com>
2025-05-02 10:49:17 +0900

    modified:   public/1.php

--- public/1.php
+++ public/1.php
@@ -4,7 +4,7 @@ $add_mileage = 50;
 $payment_price = 2524; 
 $cancelAmt = 1000;
 
-$rate = ($payment_price - $cancelAmt) / $payment_price;
+$rate = (($payment_price - $cancelAmt) / $payment_price) * 100;
 echo $rate ."<br>";
 $mileage = ($add_mileage * (($payment_price - $cancelAmt) / $payment_price)) / 100;
 
