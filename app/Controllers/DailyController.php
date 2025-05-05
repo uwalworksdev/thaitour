495e9f24795d0ec17ccf3dfe8ec16d9c588472c0
diana tgkim <diana001@naver.com>
2025-05-06 01:49:05 +0900

    modified:   app/Controllers/DailyController.php

--- app/Controllers/DailyController.php
+++ app/Controllers/DailyController.php
@@ -19,17 +19,27 @@ class DailyController extends BaseController {
     public function service_end() {
 
 		// 이용완료 : 사용날짜가 지나면 자동으로 이용완료 UPDATE
+		
+		$db     =  \Config\Database::connect();
+		$today  =  date('Y-m-d'); // 오늘 날짜
+		$sql    = "UPDATE tbl_order_mst SET order_status = 'E' WHERE order_day < ? AND order_status != 'E'";
+		$result = $db->query($sql, [$today]);
 
-		$this->db = db_connect();
+		if ($result) {
+			$msg = $today . " 이용완료 처리완료";
+		} else {
+			$msg = $today . " 이용완료 오류";
+		} 
+		
+		write_log($msg);
 
-		write_log("service_end");
     }
 	
     public function service_cancel() {
         
 		// 취소는 예약확인 상태에서 10일동안 결제가 없을시 자동으로 취소 
 		
-		$this->db = db_connect();
+	    $db = \Config\Database::connect(); // 데이터베이스 연결
 		
 		write_log("service_cancel");
     }
