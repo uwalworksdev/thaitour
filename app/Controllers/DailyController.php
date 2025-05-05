9e711fde961181ee9e2663fb68ca5d318e7ab96a
diana tgkim <diana001@naver.com>
2025-05-06 01:27:57 +0900

    modified:   app/Controllers/DailyController.php

--- app/Controllers/DailyController.php
+++ app/Controllers/DailyController.php
@@ -17,18 +17,19 @@ class DailyController extends BaseController {
     }
 	
     public function service_end() {
-        $this->db           = db_connect();
-        $this->productModel = model("ProductModel");
-        $this->roomImg      = model("RoomImg");
-        $this->CodeModel    = model("Code");
+
+		// 이용완료 : 사용날짜가 지나면 자동으로 이용완료 UPDATE
+
+		$this->db = db_connect();
+
 		write_log("service_end");
     }
 	
     public function service_cancel() {
-        $this->db           = db_connect();
-        $this->productModel = model("ProductModel");
-        $this->roomImg      = model("RoomImg");
-        $this->CodeModel    = model("Code");
+        
+		// 취소는 예약확인 상태에서 10일동안 결제가 없을시 자동으로 취소 
+		
+		$this->db = db_connect();
 		
 		write_log("service_cancel");
     }
