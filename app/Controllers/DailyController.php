a5b16d3bfcf3eb8a9994750d1d4d2567ed70ef92
diana tgkim <diana001@naver.com>
2025-05-06 02:04:41 +0900

    modified:   app/Controllers/DailyController.php

--- app/Controllers/DailyController.php
+++ app/Controllers/DailyController.php
@@ -24,9 +24,10 @@ class DailyController extends BaseController {
 		$today  =  date('Y-m-d'); // 오늘 날짜
 		$sql    = "UPDATE tbl_order_mst SET order_status = 'E' WHERE order_day < ? AND order_status != 'E'";
 		$result = $db->query($sql, [$today]);
+        $affectedRows = $db->affectedRows();
 
 		if ($result) {
-			$msg = $today . " 이용완료 처리완료";
+			$msg = $today . " 이용완료(". $affectedRows .")건 처리완료";
 		} else {
 			$msg = $today . " 이용완료 오류";
 		} 
@@ -48,10 +49,11 @@ class DailyController extends BaseController {
               AND order_r_date < DATE_SUB(NOW(), INTERVAL 10 DAY)
         ";
 
-        $result = $db->query($sql);
+        $result       = $db->query($sql);
+        $affectedRows = $db->affectedRows();
 
         if ($result) {
-            $msg = "10일 지난 예약건 취소 처리 완료.";
+            $msg = "10일 지난 예약(". $affectedRows .")건 취소 처리 완료.";
         } else {
             $msg = "예약건 취소 처리 중 오류 발생.";
         }
