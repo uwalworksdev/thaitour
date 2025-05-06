daac8dd32ee8f498b4097e6ddd77386242043c41
diana tgkim <diana001@naver.com>
2025-05-07 00:55:41 +0900

    modified:   app/Views/mypage/reservation_list.php

--- app/Views/mypage/reservation_list.php
+++ app/Views/mypage/reservation_list.php
@@ -3,13 +3,6 @@
         <strong>그룹번호: <?= esc($group->group_no) ?></strong> /
         <span>합계: <?= number_format($group->group_total) ?>원</span>
 
-        <ul>
-            <?php foreach ($groupedOrders[$group->group_no] as $order): ?>
-                <li>
-                    예약번호: <?= esc($order->order_no) ?> /
-                    금액: <?= number_format($order->order_amount) ?>원
-                </li>
-            <?php endforeach; ?>
-        </ul>
+
     </div>
 <?php endforeach; ?>
