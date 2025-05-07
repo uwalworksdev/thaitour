885dbfe5c48ee1e3f30ce1af82a17249a31d5e65
truong <vuxuantruong080920@gmail.com>
2025-05-07 15:48:41 +0700

    update

--- app/Views/center/center_term.php
+++ app/Views/center/center_term.php
@@ -9,7 +9,7 @@
                 <li class="<?= $tab3 ?>"><a href="./terms">이용약관</a></li>
                 <li class="<?= $tab4 ?>"><a href="./privacy">개인정보처리방침</a></li>
                 <li class="<?= $tab5 ?>"><a href="./reservation">예약변경 및 취소 정책</a></li>
-                <li class="<?= $tab2 ?>"><a href="./tourterms">여행약관</a></li>
+                <!-- <li class="<?= $tab2 ?>"><a href="./tourterms">여행약관</a></li> -->
             </ul>
         </nav>
     </div>
