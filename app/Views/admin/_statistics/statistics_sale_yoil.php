b7ba2ebed14294155a3a023ab2fa65097db500a8
diana tgkim <diana001@naver.com>
2025-05-09 15:41:28 +0900

    modified:   app/Views/admin/_statistics/statistics_sale_yoil.php

--- app/Views/admin/_statistics/statistics_sale_yoil.php
+++ app/Views/admin/_statistics/statistics_sale_yoil.php
@@ -200,7 +200,7 @@
                         <tbody class="statistics">
                             <tr>
                                 <td>
-                                    <div class="nums">0</div>
+                                    <div class="nums"><?=number_format(array_sum($pc_price_arr));?></div>
                                     <div>
                                         <span><i class="xi-desktop masterTooltip" title="PC"></i> 0</span><span><i class="xi-tablet masterTooltip" title="모바일"></i> 0</span>
                                     </div>
