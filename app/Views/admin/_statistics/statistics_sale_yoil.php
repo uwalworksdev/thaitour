e24e4ef4c26e1e55ccc0bf125f4dfd83e437c6d6
diana tgkim <diana001@naver.com>
2025-05-09 15:54:21 +0900

    modified:   app/Views/admin/_statistics/statistics_sale_yoil.php

--- app/Views/admin/_statistics/statistics_sale_yoil.php
+++ app/Views/admin/_statistics/statistics_sale_yoil.php
@@ -209,8 +209,7 @@
                                 <td>
                                     <div class="nums"><?=number_format(array_sum($pc_cnt_arr) + array_sum($mobile_cnt_arr));?>(개)</div>
                                     <div>
-                                        <span><i class="xi-desktop masterTooltip" title="PC"></i> <?=number_format(array_sum($pc_cnt_arr))?></span>
-										<span><i class="xi-tablet masterTooltip" title="모바일"></i> <?=number_format(array_sum($mobile_cnt_arr))?></span>
+                                        <span><i class="xi-desktop masterTooltip" title="PC"></i> <?=number_format(array_sum($pc_cnt_arr))?></span><span><i class="xi-tablet masterTooltip" title="모바일"></i> <?=number_format(array_sum($mobile_cnt_arr))?></span>
                                     </div>
                                 </td>
                                 <!-- <td>
