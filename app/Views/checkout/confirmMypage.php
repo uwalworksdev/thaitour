5c43e8dcebb10a1550fb8e89464d90eb158e25b8
diana tgkim <diana001@naver.com>
2025-05-10 18:58:31 +0900

    modified:   app/Views/checkout/confirmMypage.php

--- app/Views/checkout/confirmMypage.php
+++ app/Views/checkout/confirmMypage.php
@@ -306,14 +306,14 @@
                         <div class="item-info-r">
                             <span>포인트</span>
                             <span>
-                                -<span class="textPrice_ " id="minus_point">-0</span> 원
+                                -<span class="textPrice_ " id="minus_point">0</span> 원
                             </span>
                         </div>
 
                         <div class="item-info-r item-info-r-border-b">
                             <span>쿠폰할인</span>
                             <span>
-                                -<span class="textPrice_ " id="minus_coupon">-0</span> 원
+                                -<span class="textPrice_ " id="minus_coupon">0</span> 원
                             </span>
                         </div>
 
