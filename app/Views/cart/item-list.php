7567283a04609fdb0a1e4c633fac50e59906d966
diana tgkim <diana001@naver.com>
2025-05-10 22:48:44 +0900

    modified:   app/Views/cart/item-list.php

--- app/Views/cart/item-list.php
+++ app/Views/cart/item-list.php
@@ -237,11 +237,11 @@
 												if (!empty($item['date_price'])) {
 													$options = explode('|', $item['date_price']);
 													foreach ($options as $option) {
-														$option_r  = explode(",", esc($option));
+														$option_r  = explode(":", esc($option));
 														$basic_bed = (int)(($option_r[2]+$option_r[3]) * $option_r[4]);
 														$extra_bed = (int)($option_r[5] * $option_r[4]);
-														$day_info  = $option_r[0] ." ". number_format($basic_bed) ."(원).. ";
-														if($extra_bed > 0) $day_info .= number_format($basic_bed) ."(원).. ";
+														$day_info  = $option_r[0] ." ". number_format($basic_bed) ."(원) ";
+														if($extra_bed > 0) $day_info .= number_format($basic_bed) ."(원) ";
 														
 														echo $day_info."<br>";
 													}
