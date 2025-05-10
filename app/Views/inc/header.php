c7ab245d08cd71a29b770abdaec040f15a8d5f2a
diana tgkim <diana001@naver.com>
2025-05-10 09:57:42 +0900

    modified:   app/Views/inc/header.php

--- app/Views/inc/header.php
+++ app/Views/inc/header.php
@@ -513,7 +513,7 @@
         </div>
         <div class="nav-item" onclick="location.href='/magazines/list'">
             <img class="nav-pic" src="/images/ico/quick-header-footer_4.png" alt="quick-header-footer_4">
-            <span class="nav-text text-grey">일정표</span>
+            <span class="nav-text text-grey">예약확인</span>
         </div>
         <div class="nav-item" onclick="location.href='/mypage/fav_list'">
             <img class="nav-pic" src="/images/ico/quick-header-footer_5.png" alt="quick-header-footer_5">
