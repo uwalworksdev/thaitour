355565bedb670236780c5bcbc916de79f5225e36
diana tgkim <diana001@naver.com>
2025-05-11 13:10:43 +0900

    modified:   app/Views/admin/_statistics/statistics_sale_year.php

--- app/Views/admin/_statistics/statistics_sale_year.php
+++ app/Views/admin/_statistics/statistics_sale_year.php
@@ -135,7 +135,7 @@
                                 <td>
                                     <div class="nums"><?=number_format($top_banner1_arr['P'] + $top_banner1_arr['M'])?></div>
                                     <div>
-                                        <span><i class="xi-desktop masterTooltip" title="PC"></i> 0</span><span><i class="xi-tablet masterTooltip" title="모바일"></i> 0</span>
+                                        <span><i class="xi-desktop masterTooltip" title="PC"></i> <?=number_format($top_banner1_arr['P'])?></span><span><i class="xi-tablet masterTooltip" title="모바일"></i> <?=number_format($top_banner1_arr['M'])?></span>
                                     </div>
                                 </td>
 
