7df7411fd285ea31cfa368cf067c190c021f5000
diana tgkim <diana001@naver.com>
2025-05-12 00:00:47 +0900

    modified:   app/Controllers/Admin/AdminStatisticsController.php

--- app/Controllers/Admin/AdminStatisticsController.php
+++ app/Controllers/Admin/AdminStatisticsController.php
@@ -421,8 +421,10 @@ public function statistics_sale_month()
 		$month          = sprintf("%s-%02d", $years, $m);
 		$price_arr[$m]  = ($pc_price_arr[$month] ?? 0) + ($mobile_price_arr[$month] ?? 0);
 		$cnt_arr[$m]    = ($pc_cnt_arr[$month] ?? 0) + ($mobile_cnt_arr[$month] ?? 0);
-		$point_arr[$m]  = ($pc_point_arr[$month] ?? 0) + ($mobile_point_arr[$month] ?? 0);
-		$coupon_arr[$m] = ($pc_coupon_arr[$month] ?? 0) + ($mobile_coupon_arr[$month] ?? 0);
+		$pc_point_arr[$m]  = $pc_point_arr[$month] ?? 0;
+		$mobile_point_arr[$m]  = $mobile_point_arr[$month] ?? 0;
+		$pc_coupon_arr[$m] = ($pc_coupon_arr[$month] ?? 0;
+		$mobile_coupon_arr[$m] = $mobile_coupon_arr[$month] ?? 0;
 		$cp_arr[$m]     = 0; // 예시용, CP 수수료 값도 추가했다면 이 부분 채우세요
 	}
 
