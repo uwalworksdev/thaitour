ba71ff604d403371c4a2d330289d5f4e1a08e012
diana tgkim <diana001@naver.com>
2025-05-14 19:18:45 +0900

    modified:   app/Common.php

--- app/Common.php
+++ app/Common.php
@@ -1809,7 +1809,7 @@ function alimTalk_depisit_send($payment_idx)
 
 	$order_user_name   = $row_d['order_user_name'];
 	$order_user_mobile = $row_d['order_user_mobile'];	
-	
+	write_log("alimTalk_depisit_send- ". $order_user_name .. $order_user_mobile);
 	$bank_no = $row['VbankBankName_1'] . $row['VbankNum_1'];
 	$allim_replace = [
 						"#{고객명}"   => $order_user_name,
