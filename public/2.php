1cd031d3cf7a3e0c70f1c5762191f88225004138
diana tgkim <diana001@naver.com>
2025-05-14 19:29:33 +0900

    modified:   public/2.php

--- public/2.php
+++ public/2.php
@@ -3,12 +3,12 @@
 // Common.php 파일 경로 지정 및 포함
 
 require_once  '/home/thaitour/www/app/Common.php';
-write_log("ccc- ");
+log_message('info', 'ccc-');
  
 // 사용할 변수
 $payment_idx = "2097";
 
 // 함수 호출
-//alimTalk_depisit_send($payment_idx);
+alimTalk_depisit_send($payment_idx);
  
 ?>
