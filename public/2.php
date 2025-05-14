4baf79a9dc95a2fd9e41b73f194cb7e37a810843
diana tgkim <diana001@naver.com>
2025-05-14 19:06:39 +0900

    modified:   public/2.php

--- public/2.php
+++ public/2.php
@@ -1,6 +1,10 @@
 <?php
 
-$payment_idx = "2097";
-alimTalk_depisit_send($payment_idx); 
+// Common.php 파일 경로 지정 및 포함
+require_once __DIR__ . '/../app/Common.php';
 
-?>
+// 사용할 변수
+$payment_idx = "2097";
+
+// 함수 호출
+alimTalk_depisit_send($payment_idx);
