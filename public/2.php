5eab8a24205df209387219d05030def6d4402786
diana tgkim <diana001@naver.com>
2025-05-14 19:32:12 +0900

    modified:   public/2.php

--- public/2.php
+++ public/2.php
@@ -1,14 +1,12 @@
 <?php
+// Common.php 불러오기
+require_once realpath(__DIR__ . '/../app/Common.php');
 
-// Common.php 파일 경로 지정 및 포함
-
-require_once  '/home/thaitour/www/app/Common.php';
-log_message('info', 'ccc-');
- 
-// 사용할 변수
-$payment_idx = "2097";
+// 로그 찍기
+write_log('ccc-');  // CI4 방식 로그
 
 // 함수 호출
+$payment_idx = "2097";
 alimTalk_depisit_send($payment_idx);
  
 ?>
