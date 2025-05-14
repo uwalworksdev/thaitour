31764314872a95146c956ebbb096ab329e8097ff
diana tgkim <diana001@naver.com>
2025-05-14 19:09:29 +0900

    modified:   public/2.php

--- public/2.php
+++ public/2.php
@@ -1,12 +1,14 @@
 <?php
 
 // Common.php 파일 경로 지정 및 포함
-require_once __DIR__ . '/app/Common.php';
 
+echo __DIR__;
+require_once __DIR__ . '/app/Common.php';
+/*
 // 사용할 변수
 $payment_idx = "2097";
 
 // 함수 호출
 alimTalk_depisit_send($payment_idx);
-
+*/
 ?>
