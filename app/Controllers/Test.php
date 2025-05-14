84778cbf7a2f6e5ca6948013e6cc096331b3d658
diana tgkim <diana001@naver.com>
2025-05-15 01:46:50 +0900

    modified:   app/Controllers/Test.php

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -15,6 +15,41 @@ class Test extends BaseController
         $payment_idx = "2097";
         alimTalk_deposit_send($payment_idx);
 
+        $sql             = "	select AES_DECRYPT(UNHEX(pay_name),  '$private_key') AS user_name
+                                 	 , AES_DECRYPT(UNHEX(pay_email), '$private_key') AS user_email
+									 , Amt_1 
+									 , payment_no 
+									 , product_name
+									 , payment_price
+									 , payment_pg
+									 , TID_1
+									 , paydate
+									 , payment_method
+									 , payment_status
+									 , used_coupon_money
+									 , used_point
+									 , payment_m_date
+									 , payment_c_date
+									from tbl_payment_mst
+									where payment_idx = '" . $payment_idx . "'";
+		write_log($sql);				
+        $result = $db->connect->query($sql);
+        $row    = $result->getRowArray();
+		
+        $code      = "A17";
+        $user_mail = $row['user_email'];
+		
+        $_tmp_fir_array = [
+            'RECEIVE_NAME'   => $row['user_name'],
+            'PROD_NAME'      => $row['product_name'],
+            'ORDER_NO'       => $row['payment_no'],
+            'ORDER_DAY'      => $row['paydate'],
+            'ORDER_PRICE'    => $row['Amt_1'],
+			'PAYMENT_METHOD' => $row['payment_method'],
+            'ORDER_DATE'     => $row['paydate']
+        ];
+        autoEmail($code, $user_mail, $_tmp_fir_array);
+		
         echo "ajax_temp end";
     }
 }
