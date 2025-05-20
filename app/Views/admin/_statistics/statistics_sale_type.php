1b5ba8f7981a93e6a238ba749935ca2f48225aa2
diana tgkim <diana001@naver.com>
2025-05-20 15:46:43 +0900

    modified:   app/Views/admin/_statistics/statistics_sale_type.php

--- app/Views/admin/_statistics/statistics_sale_type.php
+++ app/Views/admin/_statistics/statistics_sale_type.php
@@ -32,11 +32,29 @@
 
     $price_arr = array();
 
-    $price_arr['Card'] = 0;
-    $price_arr['VBank'] = 0;
-    $price_arr['DBank'] = 0;
-    $price_arr['MBank'] = 0;
+    //$price_arr['Card'] = 0;
+    //$price_arr['VBank'] = 0;
+    //$price_arr['DBank'] = 0;
+    //$price_arr['MBank'] = 0;
 ?>
+
+<table>
+    <thead>
+        <tr>
+            <th>결제수단</th>
+            <th>매출합계</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($price_arr as $method => $amount): ?>
+            <tr>
+                <td><?= esc($method) ?></td>
+                <td><?= number_format($amount) ?> 원</td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
 
 <div id="container">
     <span id="print_this">
