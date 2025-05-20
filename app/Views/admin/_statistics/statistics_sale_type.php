799ab606e99ea62a1012fb23059c50e6d044e9c8
diana tgkim <diana001@naver.com>
2025-05-20 16:49:14 +0900

    modified:   app/Views/admin/_statistics/statistics_sale_type.php

--- app/Views/admin/_statistics/statistics_sale_type.php
+++ app/Views/admin/_statistics/statistics_sale_type.php
@@ -32,17 +32,9 @@
 
     $price_arr = array();
 
-                                    foreach ($ordered_methods as $method) {
-                                    if (isset($price_arr[$method])) {
-                                        $sorted_price_arr[$method] = $price_arr[$method];
-                                    }
-                                    foreach ($ordered_methods as $method) {
-                                }
     foreach ($converted_result as $row) {
 			 if($row['method'] == "Card")  $price_arr['Card']  = $row['total'];
 			 if($row['method'] == "VBank") $price_arr['VBank'] = $row['total'];
-			$price_arr['DBank'] = $row['total'];
-			$price_arr['MBank'] = $row['total'];
     }		
 ?>
 
