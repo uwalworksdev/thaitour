c2e1b0f9e7bbc58b39391255d7144e57ed729992
diana tgkim <diana001@naver.com>
2025-05-21 11:49:43 +0900

    modified:   app/Views/product/hotel/hotel-details.php

--- app/Views/product/hotel/hotel-details.php
+++ app/Views/product/hotel/hotel-details.php
@@ -1437,9 +1437,7 @@
                                             <img src="/images/sub/user-iconn.png" alt="">
                                             <p>성인 : <?=$room['adult']?>명</p>
                                             <p>아동 : <?=$room['kids']?>명</p>
-											<?php if($room['r_contents2']) { ?>
-                                            <a href="javascript:viewBenefitPopup(<?=$room['rooms_idx']?>);" style="color : #104aa8">혜택보기 &gt;</a> 
-											<?php } ?>
+                                            <a href="javascript:viewBenefitPopup(<?=$room['rooms_idx']?>);" style="color : #104aa8">혜택보기xxxx &gt;</a> 
                                         </div>
                                     </td>
                                     
