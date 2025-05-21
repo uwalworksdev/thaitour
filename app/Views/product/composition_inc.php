77267d7e30bf6e24d6da9a211ec48fa3610f175f
datdo <datvbhp123@gmail.com>
2025-05-21 15:18:02 +0700

    update-spas-order

--- app/Views/product/composition_inc.php
+++ app/Views/product/composition_inc.php
@@ -241,6 +241,37 @@
         });
     });
 
+    $(".btn_add_cart").on("click", function () {
+        // if ($("#extra-requests").val() === "") {
+        //     alert("여기에 요청 사항을 입력하세요!");
+        //     $("#extra-requests").focus();
+        //     return false;
+        // }
+        
+        $("#order_memo").val($("#extra-requests").val());
+
+        let formData = new FormData($('#frm')[0]);
+
+        let url = `<?= route_to('api.spa_.handleBooking') ?>`;
+
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (data, textStatus) {
+                alert(data.message);
+                if(data.result){
+                    window.location.href = "/product-spa/completed-cart";
+                }
+            },
+            error: function (request, status, error) {
+                console.log(request);
+                alert("code = " + request.status + " message = " + request.responseText + " error = " + error);
+            }
+        });
+    });
     
 
     function order_it(status) {
