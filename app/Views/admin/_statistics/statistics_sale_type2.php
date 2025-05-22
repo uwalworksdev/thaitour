3567d67b4e288926dd4d299b08ec2d39211dfa59
diana tgkim <diana001@naver.com>
2025-05-22 12:23:54 +0900

    modified:   app/Controllers/Admin/AdminStatisticsController.php 	modified:   app/Views/admin/_statistics/statistics_sale_type2.php

--- app/Views/admin/_statistics/statistics_sale_type2.php
+++ app/Views/admin/_statistics/statistics_sale_type2.php
@@ -220,7 +220,8 @@
                 </form>
             </div>
 
-            <?php echo ipageListing(1, 1, 10, $_SERVER["PHP_SELF"] . "?s_date=" . $s_date . "&e_date=" . $e_date . "&pg=") ?>
+            <?php echo $page ?>
+            <?php //echo ipageListing(1, 1, 10, $_SERVER["PHP_SELF"] . "?s_date=" . $s_date . "&e_date=" . $e_date . "&pg=") ?>
         </div>
     </span>
 </div>
