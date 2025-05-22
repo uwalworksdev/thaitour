d0235852fea823db38a02f0bd5d922760c734638
diana tgkim <diana001@naver.com>
2025-05-22 11:00:32 +0900

    modified:   app/Views/admin/_statistics/statistics_sale_type.php

--- app/Views/admin/_statistics/statistics_sale_type.php
+++ app/Views/admin/_statistics/statistics_sale_type.php
@@ -144,7 +144,7 @@ document.addEventListener('DOMContentLoaded', function () {
 						</div>
 
 						<!-- 날짜 버튼들 -->
-						<button type="button" class="contact_btn date-range-btn" data-start="<?= date('Y-m-d'); ?>" data-range="today">오늘</button>
+						<button type="button" class="contact_btn date-range-btn <?php if($range == "today") echo "active";?> " data-start="<?= date('Y-m-d'); ?>" data-range="today">오늘</button>
 						<button type="button" class="contact_btn date-range-btn" data-start="<?= date('Y-m-d', strtotime('-3 day')); ?>" data-range="3day">3일</button>
 						<button type="button" class="contact_btn date-range-btn" data-start="<?= date('Y-m-d', strtotime('-7 day')); ?>" data-range="7day">7일</button>
 						<button type="button" class="contact_btn date-range-btn" data-start="<?= date('Y-m-d', strtotime('-1 month')); ?>" data-range="1month">1개월</button>
