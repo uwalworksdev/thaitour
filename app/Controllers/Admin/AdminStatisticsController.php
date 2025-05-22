118cc5abedaf8760592c4272d0a093684ae2136f
diana tgkim <diana001@naver.com>
2025-05-22 20:11:33 +0900

    modified:   app/Controllers/Admin/AdminStatisticsController.php

--- app/Controllers/Admin/AdminStatisticsController.php
+++ app/Controllers/Admin/AdminStatisticsController.php
@@ -1052,8 +1052,9 @@ public function statistics_sale_type2()
 		$payin  = $this->request->getGet('payin'); // 'P' or 'M'
 
 		$sql = "
-			SELECT pm.payment_method, SUM(pm.Amt_1) AS total
+			SELECT cd.code_name, SUM(pm.Amt_1) AS total
 			FROM tbl_payment_mst pm
+			JOIN tbl_code cd ON om.product_code_2 = cd.code_no
 			JOIN tbl_order_mst om ON pm.payment_no = om.payment_no
 			WHERE pm.paydate BETWEEN ? AND ?
 			  AND pm.payment_method IS NOT NULL
