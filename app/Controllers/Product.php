062f2ddbe3b12c38d3a2e39cd44f44bc0987e061
datdo <datvbhp123@gmail.com>
2025-05-23 17:18:56 +0700

    update-tours-detail

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -3328,6 +3328,7 @@ write_log("golfList- ". $this->productModel->db->getLastQuery());
                 'tours_idx' => $row['tours_idx'],
                 'tours_subject' => $row['tours_subject'],
                 'tours_subject_eng' => $row['tours_subject_eng'],
+                'tours_desc' => $row['tours_desc'],
                 'tour_price' => $row['tour_price'],
                 'tour_price_kids' => $row['tour_price_kids'],
                 'tour_price_baby' => $row['tour_price_baby'],
