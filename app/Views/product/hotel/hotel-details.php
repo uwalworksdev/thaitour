1fa8c5d00618df9b1ad88ee55399d380bdd52f87
truong <vuxuantruong080920@gmail.com>
2025-05-26 16:23:53 +0700

    update

--- app/Views/product/hotel/hotel-details.php
+++ app/Views/product/hotel/hotel-details.php
@@ -2048,8 +2048,8 @@
                                     <span class="qa-tag <?php if($qna_status == "N"){ echo "normal-style"; }?>"><?=$qna_text?></span>
                                     <div class="con-cus-mo-qa">
                                         <p class="qa-text"><?=$qna["title"]?></p>
-                                        <div class="qa-meta text-gray only_mo"><?=$qna["r_date"]?></div>
                                     </div>
+                                    <div class="qa-meta text-gray only_mo"><?=$qna["r_date"]?></div>
                                 </div>
                                 <div class="qa-meta text-gray only_web"><?=$qna["r_date"]?></div>
                             </div>
