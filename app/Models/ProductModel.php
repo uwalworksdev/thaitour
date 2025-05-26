ff1af15eaf4b83f002c89bbeacc0422bc2bae42e
diana tgkim <diana001@naver.com>
2025-05-26 20:14:28 +0900

    modified:   app/Models/ProductModel.php

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -1242,6 +1242,7 @@ class ProductModel extends Model
         $builder->where('h.o_edate IS NOT NULL');
         $builder->where('h.o_sdate <>', '');
         $builder->where('h.o_edate <>', '');
+        $builder->where('h.option_type', 'M');
 */		
         if ($where['day_start'] && $where['day_start'] != "") {
             $builder->where('h.o_sdate <=', $where['day_start']); 
@@ -1250,9 +1251,6 @@ class ProductModel extends Model
         if ($where['day_end'] && $where['day_end'] != "") {
             $builder->where('h.o_edate >=', $where['day_end']);
         }
-
-		
-        //$builder->where('h.option_type', 'M');
 
         if ($where['product_code_1'] != "") {
             $builder->where('product_code_1', $where['product_code_1']);
