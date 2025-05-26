190e9d512cdac88603949251c7a03bb00fee0b0f
diana tgkim <diana001@naver.com>
2025-05-26 20:05:48 +0900

    modified:   app/Models/ProductModel.php

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -1237,7 +1237,7 @@ class ProductModel extends Model
         $builder = $this->db->table('tbl_product_mst AS p');
         $builder->select('p.*, MIN(STR_TO_DATE(h.o_sdate, "%Y-%m-%d")) AS oldest_date, MAX(STR_TO_DATE(o_edate, "%Y-%m-%d")) AS latest_date');
         $builder->join('tbl_hotel_rooms AS h', 'p.product_idx = h.goods_code', 'left');
-
+/*
         $builder->where('h.o_sdate IS NOT NULL');
         $builder->where('h.o_edate IS NOT NULL');
         $builder->where('h.o_sdate <>', '');
@@ -1250,7 +1250,7 @@ class ProductModel extends Model
         if ($where['day_end'] && $where['day_end'] != "") {
             $builder->where('h.o_edate >=', $where['day_end']);
         }
-
+*/
 		
         //$builder->where('h.option_type', 'M');
 
