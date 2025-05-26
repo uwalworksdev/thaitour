df245595a608b52c5f0fbe0b205da2bca148a704
diana tgkim <diana001@naver.com>
2025-05-26 13:55:17 +0900

    modified:   app/Views/admin/_reservation/write_payment.php

--- app/Views/admin/_reservation/write_payment.php
+++ app/Views/admin/_reservation/write_payment.php
@@ -264,8 +264,8 @@
 								    <tr>
                                         <th>결제 부분취소</th>
                                         <td colspan="3">
-										    기 취소총액<input type="text" name="cancel_tot" id="cancel_tot" class="input_txt" value="<?=$cancel_tot?>" style="width:100px;text-align:right;" readonly>
-										    <input type="text" name="partial_cancel_amt" id="partial_cancel_amt" class="input_txt" value="<?=$payment_row['payment_price']?>" style="width:100px;text-align:right;">
+										    기 취소총액<input type="text" name="cancel_tot" id="cancel_tot" class="input_txt" value="<?=$cancel_tot?>" style="width:100px;text-align:right;" readonly>&nbsp;&nbsp;
+										    부분 취소금액<input type="text" name="partial_cancel_amt" id="partial_cancel_amt" class="input_txt" value="<?=$payment_row['payment_price']?>" style="width:100px;text-align:right;">
 									        <button type="button" class="btn" style="width: unset;" onclick="payment_partial_cancel('<?=$payment_row['payment_no']?>','<?=$payment_row['payment_pg']?>');">부분취소</button>
                                         </td>
                                     </tr>
