4ccb6f439fe4dca9ef7e5b31c588cd8da3822404
diana tgkim <diana001@naver.com>
2025-05-27 22:52:28 +0900

    modified:   app/Views/inc/popup_wraper_header.php 	new file:   app/Views/product/hotel/list-hotel.php.tmp

--- app/Views/inc/popup_wraper_header.php
+++ app/Views/inc/popup_wraper_header.php
@@ -662,7 +662,7 @@
                             </td>
                         </tr>
                         <tr>
-                            <th>이동시간</th>
+                            <th>이동시간......</th>
                             <td>
                                 <div class="list_area list_travel_times">
                                     <p data-code="all">전체</p>
