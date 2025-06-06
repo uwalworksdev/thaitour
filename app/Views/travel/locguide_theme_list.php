0ee50cd736b2f130f21d62e96c0c419b155be538
Trần Thị Trà Gianghp <tranthitragiangkt19@gmail.com>
2025-06-06 13:40:07 +0700

    G

--- app/Views/travel/locguide_theme_list.php
+++ app/Views/travel/locguide_theme_list.php
@@ -4,7 +4,7 @@
 <div class="theme_list">
 <div class="inner">
           <div class="loctheme_all">
-                    <div class="loctheme_top"><span class="loctheme_tit">핫 플레이스</span>
+                    <div class="loctheme_top"><span class="loctheme_tit">추천여행지</span>
                               <ul class="tab_loctheme tabtour_inb">
                                         <li class="on"><a href="/user/localguide/locguide_theme_list.php?city_id=1">전체</a></li>
                                         <li>
@@ -41,7 +41,7 @@
                     </div>
                     <ul class="loctheme_list">
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=42&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                         <div class="loctheme_banner">
                                                   <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/202002/1581396868378936.png?w=492&amp;h=250" alt="카페투어 핫플레이스! 아리역 신규 카페 모음">
                                                   <span class="loctheme_boxinfo">
@@ -52,7 +52,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=43&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/202002/1582704471040267.png?w=492&amp;h=250" alt="통로, 에까마이 지역 Best Bar">
                                                             <span class="loctheme_boxinfo">
@@ -63,7 +63,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=41&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/202002/1580960152296231.png?w=492&amp;h=250" alt="방콕 아속 주변 Cafe 추천">
                                                             <span class="loctheme_boxinfo">
@@ -74,7 +74,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=66&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/202304/1682576081587541.jpg?w=492&amp;h=250" alt="호사스럽고 럭셔리한 호텔 스파">
                                                             <span class="loctheme_boxinfo">
@@ -85,7 +85,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=39&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/202001/1579150210744175.jpg?w=492&amp;h=250" alt="출출할때 생각나는 B.B.Q. 뷔페 Best">
                                                             <span class="loctheme_boxinfo">
@@ -96,7 +96,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=37&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201912/1576137769929737.jpg?w=492&amp;h=250" alt="미슐랭 스타 레스토랑">
                                                             <span class="loctheme_boxinfo">
@@ -107,7 +107,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=32&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201910/1571726276452650.jpg?w=492&amp;h=250" alt="방콕 호텔 뷔페 BEST 5">
                                                             <span class="loctheme_boxinfo">
@@ -118,7 +118,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=36&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201912/1575354381085704.jpg?w=492&amp;h=250" alt="타이 쿠킹 스쿨 리스트">
                                                             <span class="loctheme_boxinfo">
@@ -129,7 +129,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=35&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201911/1573716740546048.jpg?w=492&amp;h=250" alt="꿀잼 보장 어린이 테마 파크 시리즈">
                                                             <span class="loctheme_boxinfo">
@@ -140,7 +140,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=34&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201911/1573105077502893.jpg?w=492&amp;h=250" alt="방콕 어린이 놀이터 총정리">
                                                             <span class="loctheme_boxinfo">
@@ -151,7 +151,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=31&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201910/1571368554471919.jpg?w=492&amp;h=250" alt="방콕 추천 스테이크 레스토랑">
                                                             <span class="loctheme_boxinfo">
@@ -162,7 +162,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=30&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201910/1571282135723508.jpg?w=492&amp;h=250" alt="분위기&amp;맛 둘다 잡는 태국 레스토랑">
                                                             <span class="loctheme_boxinfo">
@@ -173,7 +173,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=29&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201910/1570594154339864.jpg?w=492&amp;h=250" alt="씨푸드 레스토랑 추천 모음">
                                                             <span class="loctheme_boxinfo">
@@ -184,7 +184,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=27&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/202206/1656554981203926.jpg?w=492&amp;h=250" alt="태국 로컬시장 완벽 탐구!">
                                                             <span class="loctheme_boxinfo">
@@ -195,7 +195,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=38&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/202001/1578293887145500.jpg?w=492&amp;h=250" alt="방콕 차이나타운 추천 먹거리">
                                                             <span class="loctheme_boxinfo">
@@ -206,7 +206,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=24&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201909/1568107130235588.jpg?w=492&amp;h=250" alt="방콕 여행 선물 고민 끝~">
                                                             <span class="loctheme_boxinfo">
@@ -217,7 +217,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=18&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201909/1568109208122557.jpg?w=492&amp;h=250" alt="루프탑바 완벽 가이드 ">
                                                             <span class="loctheme_boxinfo">
@@ -228,7 +228,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=22&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201909/1568106739441513.jpg?w=492&amp;h=250" alt="방콕 쌀국수 맛집 리스트">
                                                             <span class="loctheme_boxinfo">
@@ -239,7 +239,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=23&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201909/1568106596232488.jpg?w=492&amp;h=250" alt="여행하다 한식이 생각날 땐">
                                                             <span class="loctheme_boxinfo">
@@ -250,7 +250,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=25&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201909/1568109752726146.jpg?w=492&amp;h=250" alt="고급스러운 로컬 마사지&amp;스파">
                                                             <span class="loctheme_boxinfo">
@@ -261,7 +261,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=26&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201909/1568109537847359.jpg?w=492&amp;h=250" alt="방콕 클러빙 가이드!">
                                                             <span class="loctheme_boxinfo">
@@ -272,7 +272,7 @@
                                         </a>
                               </li>
                               <li class="theme_list_li">
-                                        <a href="/user/localguide/locguide_theme_detail.php?theme_list_id=28&amp;city_id=1">
+                                        <a href="/travel-tips/hot-place">
                                                   <div class="loctheme_banner">
                                                             <img src="https://thai.monkeytravel.com/attach/theme_list/MONKEY/201909/1568109107576944.jpg?w=492&amp;h=250" alt="가볼만한 아름다운 태국 사원">
                                                             <span class="loctheme_boxinfo">
