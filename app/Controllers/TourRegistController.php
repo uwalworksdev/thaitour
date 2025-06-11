6dc56cc2d4aad5fd3ab3136b79730f73050dff73
diana tgkim <diana001@naver.com>
2025-06-11 12:15:34 +0900

    modified:   app/Controllers/TourRegistController.php

--- app/Controllers/TourRegistController.php
+++ app/Controllers/TourRegistController.php
@@ -1550,16 +1550,32 @@ class TourRegistController extends BaseController
         if ($s_date) $o_sdate = $s_date; 
         if ($e_date) $o_edate = $e_date;
 
-        if ($s_date && $e_date) {
-            $sql = "SELECT a.*, b.bed_idx, b.bed_type, b.bed_seq FROM tbl_room_price a
-			                        LEFT JOIN tbl_room_beds b ON a.bed_idx = b.bed_idx 
-									WHERE a.product_idx = '" . $product_idx . "' $search1 AND a.bed_idx != '' AND a.bed_idx != '0' AND a.goods_date BETWEEN '$s_date' AND '$e_date' ";
-        } else {
-            $sql = "SELECT a.*, b.bed_idx, b.bed_type, b.bed_seq FROM tbl_room_price a
-			                        LEFT JOIN tbl_room_beds b ON a.bed_idx = b.bed_idx 
-			                        WHERE goods_date >= '". $today ."' AND a.bed_idx != ''  AND a.bed_idx != '0' AND product_idx = '" . $product_idx . "' $search1 ";
-        }
-        $result = $this->connect->query($sql);
+$searchBedCondition = "AND a.bed_idx IS NOT NULL AND a.bed_idx != 0";
+
+if ($s_date && $e_date) {
+    $sql = "
+        SELECT a.*, b.bed_idx, b.bed_type, b.bed_seq
+        FROM tbl_room_price a
+        LEFT JOIN tbl_room_beds b ON a.bed_idx = b.bed_idx
+        WHERE a.product_idx = ?
+        $search1
+        $searchBedCondition
+        AND a.goods_date BETWEEN ? AND ?
+    ";
+    $result = $this->connect->query($sql, [$product_idx, $s_date, $e_date]);
+} else {
+    $sql = "
+        SELECT a.*, b.bed_idx, b.bed_type, b.bed_seq
+        FROM tbl_room_price a
+        LEFT JOIN tbl_room_beds b ON a.bed_idx = b.bed_idx
+        WHERE a.product_idx = ?
+        $search1
+        $searchBedCondition
+        AND a.goods_date >= ?
+    ";
+    $result = $this->connect->query($sql, [$product_idx, $today]);
+}
+
         $nTotalCount = $result->getNumRows();
 
         $nPage = ceil($nTotalCount / $g_list_rows);
