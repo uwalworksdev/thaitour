b3bd2de03fb7cf91d0beffdc01314f2331efbf80
ThuyAn <tannv6@gmail.com>
2025-06-17 14:47:17 +0700

    add xin chao

--- app/Views/admin/_tourRegist/write_spas.php
+++ app/Views/admin/_tourRegist/write_spas.php
@@ -311,9 +311,9 @@
                                     </td>
                                     <th>담당자 담당자</th>
                                     <td>
-                                        이름: <input type="text" id="stay_user_name" name="stay_user_name"  value="<?= $stay_item['stay_user_name'] ?>" class="input_txt" placeholder="" style="width:150px"/>
-										&ensp;이메일: <input id="phone" name="phone" class="input_txt" type="text" value="<?= $phone ?? '' ?>"   style="width:150px"/>
-                                        &ensp;연락처: <input id="email" name="email" class="input_txt"  type="text" value="<?= $email ?? '' ?>"   style="width:150px"/>
+                                        이름: <input type="text" id="product_manager_2" name="product_manager_2"  value="<?= $stay_item['product_manager_2'] ?>" class="input_txt" placeholder="" style="width:150px"/>
+										&ensp;연락처: <input id="phone_2" name="phone_2" class="input_txt" type="text" value="<?= $phone_2 ?? '' ?>"   style="width:150px"/>
+                                        &ensp;이메일: <input id="email_2" name="email_2" class="input_txt"  type="text" value="<?= $email_2 ?? '' ?>"   style="width:150px"/>
                                     </td>
                                 </tr>
 
