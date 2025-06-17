697aece4b9c98d6607c7a566d3beec28dcb8cd1c
diana tgkim <diana001@naver.com>
2025-06-18 03:51:48 +0900

    modified:   app/Common.php

--- app/Common.php
+++ app/Common.php
@@ -1664,18 +1664,8 @@ function alimTalkSend($tmpCode, $allim_replace, $order_link, $voucher_link) {
 					
 					// 버튼 배열 구성
 					$buttons = [
-						(object) [
-							"ordering"     => 3,
-							"name"         => "더투어랩",
-							"linkType"     => "WL",
-							"linkTypeName" => "웹링크",
-							"linkMo"       => "https://thetourlab.com",
-							"linkPc"       => "https://thetourlab.com",
-							"linkIos"      => "",
-							"linkAnd"      => ""
-						],
 						(object)[
-							"ordering"     => 4,
+							"ordering"     => 3,
 							"name"         => "견적서 확인하기",
 							"linkType"     => "WL",
 							"linkTypeName" => "웹링크",
@@ -1685,7 +1675,7 @@ function alimTalkSend($tmpCode, $allim_replace, $order_link, $voucher_link) {
 							"linkAnd"      => ""
 						],
 						(object)[
-							"ordering"     => 5,
+							"ordering"     => 4,
 							"name"         => "나의 예약현황 바로가기",
 							"linkType"     => "WL",
 							"linkTypeName" => "웹링크",
