f6a0690d3e10e3ddc12a14031dffee180160c3f1
datdo <datvbhp123@gmail.com>
2025-06-23 16:23:33 +0700

    update-spa-invoice

--- app/Views/pdf/invoice_ticket.php
+++ app/Views/pdf/invoice_ticket.php
@@ -381,9 +381,8 @@
                         </tr>
                     </tbody>
                 </table>
-                <div class="">
-                    <h2 class="tit_top">예약내역</h2>
-                </div>
+      
+                <h2 class="tit_top">예약내역</h2>
                 <table class="invoice_tbl">
                     <tbody>
                         <tr>
@@ -392,34 +391,21 @@
                             <th style="width: 20%;">여행자 이름</th>
                             <td style="width: 80%;"><?=$row->order_user_name?></td>
                         </tr>
-                        <!-- <tr>
-                            <th style="width: 20%">날짜</th>
-                            <td style="width: 30%"><?= $option->option_date ?>(<?=dateToYoil($option->option_date)?>)</td>
-                            <th style="width: 20%">여행자 이름</th>
-                            <td style="width: 30%"><?=$row->order_user_name?></td>
-                        </tr> -->
+
                         <tr>
                             <th style="width: 20%;">고객 연락처</th>
                             <td style="width: 30%;"><?=$row->order_user_mobile?></td>
                             <th style="width: 20%;">고객 이메일</th>
                             <td style="width: 30%;"><?=$row->order_user_email?></td>
                         </tr>
-                        <!-- <tr>
-                            <th style="width: 20%">예약 선택상품</th>
-                            <td style="width: 80%" colspan="3"><?=$option->option_name?></td>
-                        </tr> -->
+
                         <tr>
                             <th style="width: 20%;">예약시간</th>
                             <td style="width: 30%;"><?=$row->order_day?>(<?=dateToYoil($row->order_day)?>) <?=$row->time_line?></td>
                             <th style="width: 20%;">총인원</th>
                             <td style="width: 30%;">성인 : <?= $row->people_adult_cnt ?>명 / 아동 : <?= $row->people_kids_cnt ?>명</td>
                         </tr>
-                        <!-- <tr>
-                            <th style="width: 20%">픽업포함여부</th>
-                            <td style="width: 30%">불포함</td>
-                            <th style="width: 20%">미팅 장소</th>
-                            <td style="width: 30%">개별이동</td>
-                        </tr> -->
+
                     </tbody>
                 </table>
                 <h2 class="tit_top">금액내역</h2>
