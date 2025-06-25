df54ede44b1daa74429c88c22dca99eb99ad2bf4
datdo <datvbhp123@gmail.com>
2025-06-25 17:06:16 +0700

    update-coupon-admin

--- app/Views/admin/_operator/coupon_write.php
+++ app/Views/admin/_operator/coupon_write.php
@@ -93,7 +93,7 @@ $titleStr = "생성";
                             <tr id="direct">
                                 <th>발행매수</th>
                                 <td>
-                                    <input type="text" name="coupon_cnt" id="coupon_cnt" class="onlynum"
+                                    <input type="text" name="coupon_cnt" id="coupon_cnt" class="onlynum" oninput="this.value = this.value.replace(/[^0-9]/g, '')"
                                            style="width:100px;text-align:right;" value="" maxlength="3"/>
                                     <a href="#!" onclick="send_it();" class="btn btn-default"><span
                                                 class="glyphicon glyphicon-cog"></span><span class="txt">등록</span></a>
