c35d849c3b761421546e96a4ba7339b83c9f3c77
datdo <datvbhp123@gmail.com>
2025-06-25 18:15:37 +0700

    update-coupon-mypage

--- app/Views/mypage/discount.php
+++ app/Views/mypage/discount.php
@@ -47,10 +47,11 @@ $nTotalCount = $connect->query($total_sql)->getNumRows();
             ?>
             <div class="content">
                 <h1 class="ttl_table_discount">쿠폰함</h1>
-                <div class="slide_tab discount flex">
+                <div class="slide_tab spe discount flex">
                     <a class="slide_tab_btn active" href="../mypage/discount">사용 가능한 쿠폰</a>
                     <a class="slide_tab_btn" href="../mypage/discount_owned">지난 쿠폰</a>
                     <!-- <a class="slide_tab_btn" href="../mypage/discount_download">쿠폰 다운로드</a> -->
+                    <div></div>
                     <div class="search_coupon_mypage">
                         <input type="text" class="keyword" id="c_keyword" placeholder="쿠폰인증 글씨를 넣어주세요">
                         <button type="button" class="btn_send_coupon">쿠폰입력</button>
