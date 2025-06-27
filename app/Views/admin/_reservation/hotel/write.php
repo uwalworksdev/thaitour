4c5d050ab472cf79bda08c8570a7a424d7173b20
datdo <datvbhp123@gmail.com>
2025-06-27 13:26:28 +0700

    update-hotel-order

--- app/Views/admin/_reservation/hotel/write.php
+++ app/Views/admin/_reservation/hotel/write.php
@@ -403,6 +403,7 @@
                                             $used_coupon_money_bath = (int) round($used_coupon_money / $bath_thai_price);
                                             $used_mileage_money_bath = (int) round($used_mileage_money / $bath_thai_price);
                                             $extra_cost_bath = (int) round($extra_cost / $bath_thai_price);
+                                            $order_price_bath = (int) round($order_price / $bath_thai_price);
                                         ?>
 										바트계산 : <?=number_format($order_price_bath)?>  TH - <?=number_format($used_coupon_money_bath)?> TH(할인쿠폰) 
                                                 - <?=number_format($used_mileage_money_bath)?> TH(마일리지사용) + <?=$extra_cost_bath?> TH 
@@ -410,6 +411,8 @@
                                     </td>
                                     <th>실 결제금액</th>
                                     <td>
+                                        <input type="hidden" id="order_price_bath" name="order_price_bath"
+                                               value="<?= number_format($order_price_bath)?>"/>
 										<input type="text" id="real_price_bath" name="real_price_bath"
                                                value="<?= number_format($real_price_bath)?>" class="input_txt price"
                                                style="width:150px;text-align:right;" <?php if($order_status != "W") echo "readonly";?> /> TH
