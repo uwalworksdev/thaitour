c20897df9a0f834246d1c72d44a012164a3829eb
datdo <datvbhp123@gmail.com>
2025-07-02 14:54:03 +0700

    update-hotel-detail

--- app/Controllers/AjaxController.php
+++ app/Controllers/AjaxController.php
@@ -971,10 +971,10 @@ public function get_golf_option() {
 																<span class="price_content"><i class="hotel_price_percent">'. $room['discount_rate'] .'</i>%할인</span>
 																</div>';
 													}  
-													$msg .= '</div>';
+													$msg .= '</div></div>';
 												}
 												
-													
+		
 												//write_log($room['goods_code']."-".$room['g_idx']."-".$room['rooms_idx']."-".$date_check_in."-".$days);		
 												$result    = detailPrice($db, $room['goods_code'], $room['g_idx'], $room['rooms_idx'], $date_check_in, $days);
 											    //write_log("11111111- ". $result);
@@ -1050,24 +1050,21 @@ public function get_golf_option() {
 														   </div>';
 
 												if($price_won > 0) {  
-												$msg .=	'<div class="wrap_btn_book">
+													$msg .=	'<div class="wrap_btn_book">
 																<div class="flex__c btn_re">
 																	<button type="button" id="reserv_'. $room['rooms_idx'] .'" data-yes="Y" data-idx="'. $room['rooms_idx'] .'" class="reservation book-button book_btn_217" >예약하기</button>
 																	<button type="button" data-idx="'. $room['rooms_idx'] .'" class="reservationx book-add-cart">장바구니</button>
-                                                    				<button type="button" id="contact_'. $room['rooms_idx'] .'" class="reservationx contact-button default-button">문의하기</button>
 																</div>
 																<p class="wrap_btn_book_note">세금서비스비용 포함</p>
 															</div>
-															</div>';
+															';
 												} else {
 													$msg .=	'<div class="wrap_btn_book">
 																<button type="button" id="reserv_'. $room['rooms_idx'] .'" data-yes="N" data-idx="'. $room['rooms_idx'] .'" class="reservation book-button disabled" >문의하기</button>
-                                                    			<button type="button" id="contact_'. $room['rooms_idx'] .'" class="reservationx contact-button default-button">문의하기</button>
 																<p class="wrap_btn_book_note">세금서비스비용 포함</p>
-															</div>
 															</div>';
-												}		
-												$msg .=	'</td>
+												}			   
+												$msg .=		   '</td>
 														   </tr>';
                              			endforeach; 
 
