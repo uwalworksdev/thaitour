038e4dac2dba1518eee3364f94a55178a47469d3
datdo <datvbhp123@gmail.com>
2025-07-02 16:37:17 +0700

    update-tour-detail

--- app/Views/product/composition_inc.php
+++ app/Views/product/composition_inc.php
@@ -152,7 +152,7 @@
                     </div>
                     <div class="flex">
                         <button type="button" class="btn-default"
-                                onclick="location='/qna/write'">상담 문의하기
+                                onclick="location='/contact/write'">상담 문의하기
                         </button>
                     </div>
                 </div>
