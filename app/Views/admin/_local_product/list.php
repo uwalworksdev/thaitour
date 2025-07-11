55b6c565e1b4e5f3fc34b3630db4409f3ba90a19
datdo <datvbhp123@gmail.com>
2025-07-11 11:33:28 +0700

    update-local-guide

--- app/Views/admin/_local_product/list.php
+++ app/Views/admin/_local_product/list.php
@@ -204,10 +204,10 @@
                                             <?= $row["r_date"] ?>
                                         </td>
                                         <td>
-                                            <a href="write?city_code=<?= $city_code ?>&category_code=<?= $category_code ?>&search_category=<?= $search_category ?>&search_txt=<?= $search_txt ?>&pg=<?= $pg ?>&idx=<?= $row["idx"] ?>"><img
-                                                    src="/images/admin/common/ico_setting2.png"></a>&nbsp;
                                             <a href="javascript:del_it('<?= $row['idx'] ?>');"><img
                                                     src="/images/admin/common/ico_error.png" alt="삭제" /></a>
+                                            <a href="write?city_code=<?= $city_code ?>&category_code=<?= $category_code ?>&search_category=<?= $search_category ?>&search_txt=<?= $search_txt ?>&pg=<?= $pg ?>&idx=<?= $row["idx"] ?>"><img
+                                                    src="/images/admin/common/ico_setting2.png"></a>&nbsp;
                                         </td>
                                     </tr>
                                 <?php endforeach; ?>
