766c77ac601cd659772942a0d3343cd7990f5509
datdo <datvbhp123@gmail.com>
2025-07-11 11:43:58 +0700

    update-local-guide

--- app/Views/admin/_local_product/list.php
+++ app/Views/admin/_local_product/list.php
@@ -195,19 +195,19 @@
                                                     style="max-width:150px;max-height:100px"></a>
                                         </td>
                                         <td class="tac">
+                                             <a href="write?city_code=<?= $city_code ?>&category_code=<?= $category_code ?>&search_category=<?= $search_category ?>&search_txt=<?= $search_txt ?>&pg=<?= $pg ?>&idx=<?= $row["idx"] ?>">
+                                                <?= viewSQ($row["title"]) ?>
                                             <a href="/product-hotel/hotel-detail/<?= $row["idx"] ?>"
                                                     class="product_view" target="_blank">[<span>상품상세</span>]</a>
-                                            <a href="write?city_code=<?= $city_code ?>&category_code=<?= $category_code ?>&search_category=<?= $search_category ?>&search_txt=<?= $search_txt ?>&pg=<?= $pg ?>&idx=<?= $row["idx"] ?>">
-                                                <?= viewSQ($row["title"]) ?>
                                         </td>
                                         <td>
                                             <?= $row["r_date"] ?>
                                         </td>
                                         <td>
-                                            <a href="javascript:del_it('<?= $row['idx'] ?>');"><img
-                                                    src="/images/admin/common/ico_error.png" alt="삭제" /></a>
                                             <a href="write?city_code=<?= $city_code ?>&category_code=<?= $category_code ?>&search_category=<?= $search_category ?>&search_txt=<?= $search_txt ?>&pg=<?= $pg ?>&idx=<?= $row["idx"] ?>"><img
                                                     src="/images/admin/common/ico_setting2.png"></a>&nbsp;
+                                            <a href="javascript:del_it('<?= $row['idx'] ?>');"><img
+                                                    src="/images/admin/common/ico_error.png" alt="삭제" /></a>
                                         </td>
                                     </tr>
                                 <?php endforeach; ?>
