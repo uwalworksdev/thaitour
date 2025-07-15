57ad8ccf885227e5c7091a0daf1fd61c50082743
datdo <datvbhp123@gmail.com>
2025-07-15 13:28:36 +0700

    update-tours-detail

--- app/Views/tours/tour-details.php
+++ app/Views/tours/tour-details.php
@@ -1754,13 +1754,17 @@
             .attr("data-price-baht", Number(dayData?.goods_price1 ?? 0));
 
         quantityContainer.find(".quantity-container.child .price").text("0원")
+            .text(number_format(Number(dayData?.goods_price2_won ?? 0)) + "원")
             .attr("data-price", Number(dayData?.goods_price2_won ?? 0));
-        quantityContainer.find(".quantity-container.child .currency").text("0 바트")
+        quantityContainer.find(".quantity-container.child .currency")
+            .text(number_format(Number(dayData?.goods_price2 ?? 0)) + " 바트")
             .attr("data-price-baht", Number(dayData?.goods_price2 ?? 0));
 
         quantityContainer.find(".quantity-container.baby .price").text("0원")
+            .text(number_format(Number(dayData?.goods_price3_won ?? 0)) + "원")
             .attr("data-price", Number(dayData?.goods_price3_won ?? 0));
-        quantityContainer.find(".quantity-container.baby .currency").text("0 바트")
+        quantityContainer.find(".quantity-container.baby .currency")
+            .text(number_format(Number(dayData?.goods_price3 ?? 0)) + "원")
             .attr("data-price-baht", Number(dayData?.goods_price3 ?? 0));
 
         adultQuantity = 1; childQuantity = 0; babyQuantity = 0;
