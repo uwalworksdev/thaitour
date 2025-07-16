6027ee8e399dc6968c7300de8c15b1cc0d14f170
datdo <datvbhp123@gmail.com>
2025-07-16 15:19:29 +0700

    update-hotel-search

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -1240,8 +1240,8 @@ class ProductModel extends Model
         $baht_thai = (float)($setting['baht_thai'] ?? 0);
 
         $builder = $this->db->table('tbl_product_mst AS p');
-        $builder->select('p.*, MIN(STR_TO_DATE(h.o_sdate, "%Y-%m-%d")) AS oldest_date, MAX(STR_TO_DATE(o_edate, "%Y-%m-%d")) AS latest_date');
-        $builder->join('tbl_hotel_rooms AS h', 'p.product_idx = h.goods_code', 'left');
+        // $builder->select('p.*, MIN(STR_TO_DATE(h.o_sdate, "%Y-%m-%d")) AS oldest_date, MAX(STR_TO_DATE(o_edate, "%Y-%m-%d")) AS latest_date');
+        // $builder->join('tbl_hotel_rooms AS h', 'p.product_idx = h.goods_code', 'left');
 /*
         $builder->where('h.o_sdate IS NOT NULL');
         $builder->where('h.o_edate IS NOT NULL');
