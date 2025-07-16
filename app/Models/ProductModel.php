287b7955630f8631c7a990ce99e21c4992318747
datdo <datvbhp123@gmail.com>
2025-07-16 14:14:16 +0700

    update-hotel-search

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -1240,22 +1240,22 @@ class ProductModel extends Model
         $baht_thai = (float)($setting['baht_thai'] ?? 0);
 
         $builder = $this->db->table('tbl_product_mst AS p');
-        // $builder->select('p.*, MIN(STR_TO_DATE(h.o_sdate, "%Y-%m-%d")) AS oldest_date, MAX(STR_TO_DATE(o_edate, "%Y-%m-%d")) AS latest_date');
-        // $builder->join('tbl_hotel_rooms AS h', 'p.product_idx = h.goods_code', 'left');
-/*
-        $builder->where('h.o_sdate IS NOT NULL');
-        $builder->where('h.o_edate IS NOT NULL');
-        $builder->where('h.o_sdate <>', '');
-        $builder->where('h.o_edate <>', '');
-        $builder->where('h.option_type', 'M');
-*/		
-        if ($where['day_start'] && $where['day_start'] != "") {
-            $builder->where('h.o_sdate <=', $where['day_start']); 
-        }
+//         $builder->select('p.*, MIN(STR_TO_DATE(h.o_sdate, "%Y-%m-%d")) AS oldest_date, MAX(STR_TO_DATE(o_edate, "%Y-%m-%d")) AS latest_date');
+//         $builder->join('tbl_hotel_rooms AS h', 'p.product_idx = h.goods_code', 'left');
+// /*
+//         $builder->where('h.o_sdate IS NOT NULL');
+//         $builder->where('h.o_edate IS NOT NULL');
+//         $builder->where('h.o_sdate <>', '');
+//         $builder->where('h.o_edate <>', '');
+//         $builder->where('h.option_type', 'M');
+// */		
+//         if ($where['day_start'] && $where['day_start'] != "") {
+//             $builder->where('h.o_sdate <=', $where['day_start']); 
+//         }
 
-        if ($where['day_end'] && $where['day_end'] != "") {
-            $builder->where('h.o_edate >=', $where['day_end']);
-        }
+//         if ($where['day_end'] && $where['day_end'] != "") {
+//             $builder->where('h.o_edate >=', $where['day_end']);
+//         }
 
         if ($where['product_code_1'] != "") {
             $builder->where('product_code_1', $where['product_code_1']);
