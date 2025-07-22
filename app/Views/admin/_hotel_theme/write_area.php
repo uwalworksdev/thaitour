ea6529f40fdea09cb7b14b8a220fe064fe264b6e
datdo <datvbhp123@gmail.com>
2025-07-22 14:49:29 +0700

    update-hotel-theme

--- app/Views/admin/_hotel_theme/write_area.php
+++ app/Views/admin/_hotel_theme/write_area.php
@@ -476,7 +476,7 @@
             cache: false,
             async: false,
             success: function (data, textStatus) {
-                console.log($('.s_category_code').length);
+                console.log($('.s_category_code[value="'+ code +'"]').closest("table").find(".product_area tbody"));
                 $('.s_category_code[value="'+ code +'"]').closest("table").find(".product_area tbody").append(data);
                 $(".pick_item_pop02").hide();
             },
