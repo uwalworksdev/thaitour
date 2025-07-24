918bce0f6f8ba75df9a30c6991752c5ee56f79ec
datdo <datvbhp123@gmail.com>
2025-07-24 09:22:40 +0700

    update-theme-travel

--- app/Views/admin/_hotel_theme/write_area.php
+++ app/Views/admin/_hotel_theme/write_area.php
@@ -252,8 +252,8 @@ $links = "list";
                                                                 <input type="hidden" name="step[<?= $count ?>][]" class="step_index" value="<?= $prow['step'] ?>">
 
                                                                 <th style="text-align: center;">
-                                                                    <div class="flex_c_c" style="margin-top: 5px;">
-                                                                        <span style="font-weight: bold;">TOP <?= $prow['step'] ?></span>
+                                                                    <div class="flex_c_c" style="margin-top: 5px; flex-direction: column;">
+                                                                        <p style="font-size: 20px;">TOP <?= $prow['step'] ?></p>
                                                                         <button type="button" onclick="del_product(this, '<?= $prow['s_idx'] ?>');" class="btn btn-danger">삭제</button>
                                                                     </div>
                                                                 </th>
