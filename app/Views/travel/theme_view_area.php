46821e477e59e434b20b1c3c463b191184e9df09
datdo <datvbhp123@gmail.com>
2025-07-24 14:26:52 +0700

    update-theme-travel

--- app/Views/travel/theme_view_area.php
+++ app/Views/travel/theme_view_area.php
@@ -228,6 +228,9 @@
                                     </a>
                                 </div>
                             </div>
+                            <div class="swiper-button-next"></div>
+                            <div class="swiper-button-prev"></div>
+                            <div class="swiper-pagination"></div>
                         </div>
                     </div>
                 </div>
