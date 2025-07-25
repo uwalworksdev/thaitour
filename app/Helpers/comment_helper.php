19aaf38945ae7b5d9e60467c4244c5b173f714d5
datdo <datvbhp123@gmail.com>
2025-07-25 08:30:45 +0700

    update-theme-travel

--- app/Helpers/comment_helper.php
+++ app/Helpers/comment_helper.php
@@ -66,7 +66,7 @@ function displayComments($list, $r_code, $r_idx, $parentCommentId = 0, $level = 
         }
         $html .= '</div>';
         $html .= '<div class="comment_user-operation">';
-        if ((session('member.idx') == $comment['r_m_idx']) || session('member.id') == "admin" || session('member.level') <= 2) {
+        if (session('member.id') == "admin" || session('member.level') <= 2) {
             $html .= '<button type="button" onclick="handleCmtEdit(' . $comment['r_cmt_idx'] . ')">수정</button>';
             $html .= '<button type="button" onclick="handleCmtDelete(' . $comment['r_cmt_idx'] . ')">삭제</button>';
         }
