b374f894f438379bf1504cee163feeac37703aa7
datdo <datvbhp123@gmail.com>
2025-07-29 15:38:25 +0700

    update-promotion-admin

--- app/Config/AdminMenus.php
+++ app/Config/AdminMenus.php
@@ -144,13 +144,13 @@ class AdminMenus extends BaseConfig
                     'alias' => ['AdminPromotionController::list_product', 'AdminPromotionController::write_product']
                 ],
                 [
-                    'name' => '투어',
+                    'name' => '골프',
                     'code' => 'L3 ',
                     'url' => ['/AdmMaster/_promotion/list_product?type=golf'],
                     'alias' => ['AdminPromotionController::list_product', 'AdminPromotionController::write_product']
                 ],
                 [
-                    'name' => '골프',
+                    'name' => '투어',
                     'code' => 'L4',
                     'url' => ['/AdmMaster/_promotion/list_product?type=tour'],
                     'alias' => ['AdminPromotionController::list_product', 'AdminPromotionController::write_product']
