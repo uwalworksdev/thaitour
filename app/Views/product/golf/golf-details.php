dbcffc4b9afc1dd1224ba95c3f1ac181d315a27a
diana tgkim <diana001@naver.com>
2025-07-31 18:10:39 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -274,7 +274,7 @@ $(document).ready(function() {
                     </div>
                 </div>
 
-                <h3 id="pickup" class="title-size-24 text-parent">상품 예약<span>날짜 · 홀수 ·인원 ·시간대를 먼저 선택해 주세요.</span></h3>
+                <h3 id="pickup" class="title-size-24 text-parent">상품 예약<span>날짜 · 홀수 ·인원 ·시간대를 먼저 선택해 주세요..</span></h3>
                 <div class="calendar">
                     <div class="year">
                         <div class="btn_year_new">
