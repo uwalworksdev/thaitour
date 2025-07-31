e38484d0bba87456abed9634c1aad43c80c22082
diana tgkim <diana001@naver.com>
2025-07-31 19:31:44 +0900

    modified:   app/Views/product/golf/golf-details.php

--- app/Views/product/golf/golf-details.php
+++ app/Views/product/golf/golf-details.php
@@ -1567,7 +1567,7 @@ $(document).ready(function() {
 
         function getOptions() {
             const golf_date = $("#order_date").val();
-			if(golf_date == "") {
+			if(golf_date == "NaN-NaN-NaN") {
 			   golf_date = "<?=date('y-m-d')?>";	
 			}   
             const hole_cnt  = $('.tag-js.active').data('tab') + '홀';
