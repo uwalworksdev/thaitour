ab9164d1defb6bac32018f97c5ce1b506f64d00c
datdo <datvbhp123@gmail.com>
2025-08-15 16:22:36 +0700

    update-copy-product-admin

--- app/Controllers/TourRegistController.php
+++ app/Controllers/TourRegistController.php
@@ -2906,10 +2906,11 @@ public function list_room_pricex()
 
         $info = $result['info'];
 
-        var_dump($info['product_code_1']);
-        die();
+
 
         if($info['product_code_1'] == 1303) {
+                    var_dump("dat");
+        die();
             $this->productPlace->copyProductPlace($product_idx, $newProductIdx);
             $this->roomsModel->copyRooms($stay_idx, $stay_new_idx);
         }
