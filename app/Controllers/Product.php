1cfa8e4163a70dd80f053be4fdbc424a661fa00b
datdo <datvbhp123@gmail.com>
2025-08-19 17:13:11 +0700

    update-vehicle

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -4093,6 +4093,7 @@ class Product extends BaseController
                                             ->where("code_idx !=", $code_idx)
                                             ->where("is_apply =", 'Y')
                                             ->get()->getRowArray();
+                echo $this->db->getLastQuery();
                 $departure_list[$key]["contents_list"] = $this->codeContents->where("code_idx", $value["code_idx"])->get()->getResultArray();
 
                 if(count($departure_list[$key]["contents_list"]) < 0 && !empty($row_code)) {
