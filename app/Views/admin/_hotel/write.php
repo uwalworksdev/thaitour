61e21d7c7a7fada1d56107da2ad6c870a8522dc5
datdo <datvbhp123@gmail.com>
2025-08-21 17:09:56 +0700

    update-editor

--- app/Views/admin/_hotel/write.php
+++ app/Views/admin/_hotel/write.php
@@ -1555,6 +1555,8 @@ $links = "list";
                                                 fOnAppLoad: function () {
                                                     //예제 코드
                                                     //oEditors.getById["ir1"].exec("PASTE_HTML", ["로딩이 완료된 후에 본문에 삽입되는 text입니다."]);
+                                                    var editor = oEditors3.getById["product_notes"];
+                                                    editor.setIR("");  
                                                 },
                                                 fCreator: "createSEditor2"
                                             });
