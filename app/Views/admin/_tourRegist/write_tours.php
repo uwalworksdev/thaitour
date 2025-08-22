2496d4270ef59b44965f8b022e21d7396e0c9e44
datdo <datvbhp123@gmail.com>
2025-08-22 10:14:59 +0700

    update-editor

--- app/Views/admin/_tourRegist/write_tours.php
+++ app/Views/admin/_tourRegist/write_tours.php
@@ -924,6 +924,8 @@
                                                 fOnAppLoad: function () {
                                                     //예제 코드
                                                     //oEditors2.getById["ir1"].exec("PASTE_HTML", ["로딩이 완료된 후에 본문에 삽입되는 text입니다."]);
+                                                    let editor = oEditors21.getById["minimun_reservation"];
+                                                    editor.setIR("");  
                                                 },
                                                 fCreator: "createSEditor2"
                                             });
