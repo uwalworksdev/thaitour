532b08372a9879ead40a84f70c8f2c0c5fb4297c
datdo <datvbhp123@gmail.com>
2025-08-22 10:19:30 +0700

    update-editor

--- app/Views/admin/_tourRegist/write_tours.php
+++ app/Views/admin/_tourRegist/write_tours.php
@@ -924,9 +924,15 @@
                                                 fOnAppLoad: function () {
                                                     //예제 코드
                                                     //oEditors2.getById["ir1"].exec("PASTE_HTML", ["로딩이 완료된 후에 본문에 삽입되는 text입니다."]);
+
                                                     let editor = oEditors21.getById["minimun_reservation"];
-                                                    editor.setIR("");  
-                                                },
+
+                                                    let initContent = $("#minimun_reservation").val().trim();
+
+                                                    if (initContent === "" || initContent === "<p><br></p>" || initContent === "<p>&nbsp;</p>") {
+                                                        editor.setIR("");
+                                                    }
+                                            },
                                                 fCreator: "createSEditor2"
                                             });
                                         </script>
