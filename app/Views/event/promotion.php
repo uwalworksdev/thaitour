43c87a73266048dacdee24df15f8942aeaa5bc79
datdo <datvbhp123@gmail.com>
2025-08-25 10:02:36 +0700

    update-promotion-img

--- app/Views/event/promotion.php
+++ app/Views/event/promotion.php
@@ -60,11 +60,14 @@
                             $img_banner_mo = "/data/promotion/" . $banner['ufile'];
                         }
                 ?>
-                        <div class="slide absolute inset-0 w-full h-full opacity-0 transition-opacity duration-1000">
-                            <img src="<?= $img_banner ?>" alt="<?= $banner['rfile'] ?>" class="w-full h-full object-cover only_web">
-                            <img src="<?= $img_banner_mo ?>" alt="<?= $banner['rfile'] ?>" class="w-full h-full object-cover only_mo">
-                            <div class="absolute inset-0 bg-transparent"></div>
-                        </div>
+                    <div class="slide absolute inset-0 w-full h-full opacity-0 transition-opacity duration-1000 only_web">
+                        <img src="<?= $img_banner ?>" alt="<?= $banner['rfile'] ?>" class="w-full h-full object-cover ">
+                        <div class="absolute inset-0 bg-transparent"></div>
+                    </div>
+                    <div class="slide absolute inset-0 w-full h-full opacity-0 transition-opacity duration-1000 only_mo">
+                        <img src="<?= $img_banner_mo ?>" alt="<?= $banner['rfile'] ?>" class="w-full h-full object-cover">
+                        <div class="absolute inset-0 bg-transparent"></div>
+                    </div>
                 <?php
                     }
                 }
